5177c29ead1bdaaea58f83f7c6d02e89ac967d00
clu <clu@c60d6122-3b2e-0410-912d-af40fea696fe>
2003-04-02 15:27:35 +0000

    generated peptide fasta files from bsml doc

---
+++ src/perl/make_pep_bsml.pl
@@ -0,0 +1,196 @@
+#!/usr/local/bin/perl
+
+
+use lib("/usr/local/annotation/PNEUMO/clu_dir/BSML/ANNOTATION/bsml/src");
+use strict;
+use Getopt::Long qw(:config no_ignore_case no_auto_abbrev);
+use BsmlReader;
+use BsmlParserTwig;
+
+
+my %options = ();
+my $results = GetOptions (\%options, 'bsml_dir|b=s', 'asmbl_ids|a=s', 'simple_header|s',
+                                     'output_dir|o=s', 'DEBUG', 'help|h', 'each_file|e', 'each_genome|g' );
+
+###-------------PROCESSING COMMAND LINE OPTIONS-------------###
+
+my $ASMBL_IDS       = $options{'asmbl_ids'};
+my $simple_header   = $options{'simple_header'};
+my $each_file       = $options{'each_file'} || 0;
+my $each_genome     = $options{'each_genome'} || 0;
+my $output_dir      = $options{'output_dir'};
+$output_dir =~ s/\/+$//;       #remove terminating '/'s
+my $BSML_dir        = $options{'bsml_dir'};
+$BSML_dir =~ s/\/+$//;         #remove terminating '/'s
+
+if($each_file and $each_genome) {
+    print STDERR "--each_genome(-g) and --each_file(-e) CANNOT be invoked at the same time.\n";
+    exit 1;
+}
+
+if(!defined($ASMBL_IDS) or !$output_dir or !$BSML_dir or exists($options{'help'})) {
+    &print_usage();
+}
+
+###-------------------------------------------------------###
+if(! -d $output_dir ) {
+    mkdir $output_dir;
+}
+chmod 0777, $output_dir;
+
+my @asm_ids;
+if($ASMBL_IDS =~ /all/i) {
+    my @files = <$BSML_dir/*.bsml>;
+    foreach (@files) {
+	if(/asmbl_(\d+)\.bsml/) {
+	    push(@asm_ids, $1);
+        }
+    }
+} else {
+    @asm_ids = split(/,/, $ASMBL_IDS);
+}
+
+my $parser = new BsmlParserTwig;
+
+if($each_genome) {
+    #$output_dir = $ENV{'PNEUMO_QUERY_DIR'} if(!$output_dir);
+    make_fasta_for_each_genome(\@asm_ids);
+    print "Finished making fasta pep files for EACH genome\n";
+}elsif($each_file) {
+    make_fasta_for_each_gene(\@asm_ids);
+    print "Finished making fasta pep file for each gene\n";
+}else {
+    #$output_dir = $ENV{'PNEUMO_DB_DIR'} if(!$output_dir);
+    make_PNEUMO_pep_for_ALL_genomes(\@asm_ids);
+    print "Finished making PNEUMO.pep file\n";
+}
+
+sub make_fasta_for_each_gene  {
+
+    my $assembly_ids = shift;
+
+    foreach my $asmbl_id (@$assembly_ids) {
+	next if($asmbl_id !~ /\d+/);
+	my $final_output_dir = "$output_dir/asmbl_id_".$asmbl_id;
+	#create the directory that will hold all the individual peptide files if it doesn't exist
+	if(! -d $final_output_dir ) {
+	    mkdir $final_output_dir;
+	}else {         #delete old crap if that directory already exists
+	    unlink glob("$final_output_dir/*");
+	}
+	chmod 0777, $final_output_dir;
+	my $bsml_file = "$BSML_dir/asmbl_${asmbl_id}.bsml";
+	if (-s $bsml_file) {
+	    my $reader = BsmlReader->new();
+	    $parser->parse( \$reader, $bsml_file );
+	    my $proteins = $reader->get_all_protein_aa("PNEUMO_${asmbl_id}");
+	    while (my ($identifier, $seq) = each %$proteins) {
+		$identifier =~ s/_\d$//;
+		my $pep_file = "$final_output_dir/$identifier.fsa";
+		open(FILE, ">$pep_file") || die "Unable to write to $pep_file due to $!";
+		my $prot_name = $identifier;
+		my $fastaout = &fasta_out($prot_name, $seq);
+		print FILE $fastaout;
+		close FILE;
+		chmod 0777, $pep_file;
+	    }
+	} else {
+	    print STDERR "The $bsml_file does not exist!\n";
+	    exit 5;
+	}
+
+    }
+
+}
+
+sub make_fasta_for_each_genome {
+
+    my $assembly_ids = shift;
+
+    foreach my $asmbl_id (@$assembly_ids) {
+	my $pep_file = "$output_dir/asmbl_id_".$asmbl_id.".pep";
+	open(FILE, ">$pep_file") || die "Unable to write to $pep_file due to $!";
+	my $bsml_file = "$BSML_dir/asmbl_${asmbl_id}.bsml";
+	if (-s $bsml_file) {
+	    my $reader = BsmlReader->new();
+	    $parser->parse( \$reader, $bsml_file );
+	    my $proteins = $reader->get_all_protein_aa("PNEUMO_${asmbl_id}");
+	    while (my ($identifier, $seq) = each %$proteins) {
+		$identifier =~ s/_\d$//;	
+		my $prot_name = $identifier;
+		my $fastaout = &fasta_out($prot_name, $seq);
+                print FILE $fastaout;
+            }
+	    close FILE;
+	    chmod 0777, $pep_file;
+	}else {
+	    print STDERR "$bsml_file NOT found!!!! Aborting...\n";
+	    exit 5;
+	}
+    }
+
+}
+
+
+sub make_PNEUMO_pep_for_ALL_genomes {
+
+    my $assembly_ids = shift;
+
+    my $pep_file = "$output_dir/PNEUMO.pep";
+    open(FILE, ">$pep_file") || die "Cant open $pep_file due to $!";
+    foreach my $asmbl_id (@$assembly_ids) {
+	#my $result = $CGC->fetch_protein_seq_info_from_asmbl_id($asmbl_id);
+	my $bsml_file = "$BSML_dir/asmbl_${asmbl_id}.bsml";
+	if (-s $bsml_file) {
+	    my $reader = BsmlReader->new();
+	    $parser->parse( \$reader, $bsml_file );
+	    my $proteins = $reader->get_all_protein_aa("PNEUMO_${asmbl_id}");
+	    while (my ($identifier, $seq) = each %$proteins) {
+		$identifier =~ s/_\d$//;
+		my $prot_name = $simple_header ? "$identifier asmbl_id_${asmbl_id}" : "asmbl_id_${asmbl_id}:$identifier";
+		my $fastaout = &fasta_out($prot_name, $seq);
+                print FILE $fastaout;
+            }
+	} else {
+	    print STDERR "$bsml_file NOT found!!!! Aborting...\n";
+        }
+    }
+    close FILE;
+    chmod 0777, $pep_file;
+    qx(setdb $pep_file);
+    #chmod 0777 <$pep_file.*>;
+}
+
+
+sub fasta_out {
+#This subroutine takes a sequence name and its sequence and
+#outputs a correctly formatted single fasta entry (including newlines).
+
+    my $seq_name = shift;
+    my $seq = shift;
+
+    my $fasta=">"."$seq_name"."\n";
+    for(my $i=0; $i < length($seq); $i+=60){
+	my $seq_fragment = substr($seq, $i, 60);
+	$fasta .= "$seq_fragment"."\n";
+    }
+    return $fasta;
+
+}
+
+
+
+sub print_usage {
+
+
+    print STDERR "SAMPLE USAGE:  make_pep_bsml.pl -b bsml_dir -o output_dir -a 19\n";
+    print STDERR "  --bsml_dir    = dir containing BSML doc\n";
+    print STDERR "  --output_dir  = dir to save output to\n";
+    print STDERR "  --asmbl_ids  (only get sequences  belong to particular asmbl_ids)\n";
+    print STDERR "  --each_genome = save fasta file individually for each genome\n";
+    print STDERR "  --each_file   = save fasta file individually for each gene\n";
+    print STDERR " NOTE* --each_genome and --each_file cannot be invoked concurrently\n";
+    print STDERR "  --help = This help message.\n";
+    exit 1;
+
+}
