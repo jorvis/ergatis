6582cc4a99c1d92d37e2e3b18b3d6af0861a1774
angiuoli <angiuoli@c60d6122-3b2e-0410-912d-af40fea696fe>
2003-04-24 14:37:04 +0000

    filtered output

--- src/perl/pe2bsml.pl
+++ src/perl/pe2bsml.pl
@@ -35,11 +35,14 @@ if(!$pe_file or !$output or exists($options{'help'})) {
 
 read_pe_output($pe_file);
 
+
 sub read_pe_output {
 
     my $pe_out = shift;
 
     my $doc = BSML::BsmlBuilder->new();
+
+    my $seq_pair = {};
 
     my ($ClusterGeneCount, $ClusterGapCount, $ClusterScore, $ClusterId);
     open (PE, "$pe_out") or die "Unable to open \"$pe_out\" due to $!";
@@ -53,10 +56,11 @@ sub read_pe_output {
 	    $ClusterScore = $pe[2];
 	    $ClusterId = $pe[4];
 	} elsif(@pe == 4) {
-	    my $aln = $doc->createAndAddSequencePairAlignment( 'query_name'        => $pe[2],
-							       'dbmatch_accession' => $pe[3] 
-							       );
-	    my $s = $doc->createAndAddSequencePairRun( 'alignment_pair' => $aln,
+	    if(! (exists $seq_pair->{$pe[2]}->{$pe[3]})){
+		$seq_pair->{$pe[2]}->{$pe[3]} = [];
+	    }
+	    else{
+		push @{$seq_pair->{$pe[2]}->{$pe[3]}}, {
 						       'start_query'    => $pe[0],
 						       'runlength'      => 0,
 						       'start_hit'      => $pe[1],
@@ -64,11 +68,43 @@ sub read_pe_output {
 						       'PEffect_Cluster_Id' => $ClusterId,
 						       'PEffect_Cluster_Gap_Count' => $ClusterGapCount,
 						       'PEffect_Cluster_Gene_Count' => $ClusterGeneCount 
-						       );
+						       };
+	    }
 	}
     }
+
     close PE;
+
+    print STDERR "Created lookup with ",scalar(keys %$seq_pair)," matches\n";
+
+    foreach my $query_name (keys %$seq_pair){
+	foreach my $dbmatch_accession (keys %{$seq_pair->{$query_name}}){
+	    my $aln = $doc->createAndAddSequencePairAlignment( 'query_name'        => $query_name,
+							       'dbmatch_accession' => $dbmatch_accession 
+							       );
+	    my $runs = $seq_pair->{$query_name}->{$dbmatch_accession};
+	    my $hitnum = 0;
+	    foreach my $run (sort {$a->{'runscore'} <=> $b->{'runscore'}} (@$runs)){
+		if($hitnum <3){
+		    my $s = $doc->createAndAddSequencePairRun( 'alignment_pair' => $aln,
+							       'start_query'    => $run->{'start_query'},
+							       'runlength'      => $run->{'runlength'},
+							       'start_hit'      => $run->{'start_hit'},
+							       'runscore'       => $run->{'runscore'},
+							       'PEffect_Cluster_Id' => $run->{'PEffect_Cluster_Id'},
+							       'PEffect_Cluster_Gap_Count' => $run->{'PEffect_Cluster_Gap_Count'},
+							       'PEffect_Cluster_Gene_Count' => $run->{'PEffect_Cluster_Gene_Count'}
+							       );
+		}
+		$hitnum++;
+	    }
+	}
+    }
+
+    print STDERR "Writing output\n";
     
     $doc->write($output);
+
+    print STDERR "Writing done\n";
 }
 
