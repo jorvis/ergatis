56a6d203ca6a73e9cc0b3c05f7d0a2a9d9875614
clu <clu@c60d6122-3b2e-0410-912d-af40fea696fe>
2003-04-24 20:57:15 +0000

    addes bit score percent to bsml-blastp doc

---
+++ src/perl/add_bit_score.pl
@@ -0,0 +1,62 @@
+#!/usr/local/bin/perl
+
+
+use strict;
+use Getopt::Long qw(:config no_ignore_case no_auto_abbrev);
+use BSML::BsmlReader;
+use BSML::BsmlParserTwig;
+use File::Basename;
+use BSML::BsmlBuilder;
+
+
+my %options = ();
+my $results = GetOptions (\%options, 'bsml_file|f=s', 'bsml_dir|b=s', 'output|o=s', 'verbose|v', 'help|h',);
+
+
+###-------------PROCESSING COMMAND LINE OPTIONS-------------###
+my $bsml_file   = $options{'bsml_file'};
+my $output_file = $options{'output'} || $bsml_file;
+my $BSML_dir   = $options{'bsml_dir'};
+$BSML_dir =~ s/\/+$//;         #remove terminating '/'s
+my $verbose    = $options{'verbose'};
+
+if(!$bsml_file or !$output_file  or exists($options{'help'})) {
+    #$logger->fatal("Not all of the required options have been defined.  Exiting...");
+    &print_usage();
+}
+
+###-------------------------------------------------------###
+
+my $reader = BSML::BsmlReader->new();
+my $parser = BSML::BsmlParserTwig->new();
+
+$parser->parse( \$reader, $bsml_file );
+
+foreach my $seqobj (@{$reader->returnAllSequences()}) {
+    my $seqId = $seqobj->returnattr('id');
+    # return the alignment object corresponding to the query sequences verses itself.
+    my $alignment_list = $reader->fetch_all_alignmentPairs( $seqId, $seqId );
+    if( $alignment_list->[0] ) {
+	print STDERR "found base match\n";
+	my $base_alignment = $reader->readSeqPairAlignment( $alignment_list->[0] );
+	my $base_score = $base_alignment->{'seqPairRuns'}->[0]->{'runscore'};
+	# get all the alignment objects having $seqId as the query sequence
+	$alignment_list = $reader->fetch_all_alignmentPairs( $seqId );
+	# foreach alignment, calculate the percent bit score for each seq pair run
+	foreach my $align (@{$alignment_list}) {
+	    my $alignmentDat = $reader->readSeqPairAlignment( $align );
+	    foreach my $run ( @{$alignmentDat->{'seqPairRuns'}} ) {
+		my $score = $run->{'runscore'};
+		my $PercentBitScore = sprintf('%.3f', $score/$base_score);
+		# add a bsml attribute to the document for percent bit score
+		print STDERR "adding percent bit score $PercentBitScore\n";
+		$run->{'bsmlRef'}->addBsmlAttr( 'percent_bit_score', $PercentBitScore );
+	    }
+	}
+    } else {
+	print STDERR "NO BASE MATCH found\n";
+    }
+}
+
+# write the altered file to disk
+$reader->write( $output_file );
