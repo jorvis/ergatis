c487ee632df927446b4207d11659706ae0b9b6d7
angiuoli <angiuoli@c60d6122-3b2e-0410-912d-af40fea696fe>
2003-04-26 21:22:39 +0000

    updated

--- workflow/set_assembly.sh
+++ workflow/set_assembly.sh
@@ -26,3 +26,5 @@ cat all_vs_all_template.xml | sed "s/$KEY/$1/" > $1/all_vs_all_template.xml
 
 echo "Start workflow with cd $1;"
 echo "RunWorkflow -t all_vs_all_template.xml -c all_vs_all.ini -i allvsall.$1.xml -l allvsall.$1.log"
+echo "RunWorkflow -t pe_template.xml -c pe.ini -i pe.$1.xml -l pe.$1.log"
+echo "RunWorkflow -t bit_score_template.xml -c bit_score.ini -i bit_score.$1.xml -l bit_score.$1.log"
