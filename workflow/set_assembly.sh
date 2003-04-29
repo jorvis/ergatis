c88521e2475941c8231b7313c09af85c28b77c30
clu <clu@c60d6122-3b2e-0410-912d-af40fea696fe>
2003-04-29 20:20:53 +0000

    sets moaf.ini

--- workflow/set_assembly.sh
+++ workflow/set_assembly.sh
@@ -25,6 +25,8 @@ cat pe.ini | sed "s/$KEY/$1/g" | sed "s/$DB/$2/g" > $1/pe.ini
 echo "Saving $1/all_vs_all.ini"
 cat all_vs_all.ini | sed "s/$KEY/$1/g" | sed "s/$DB/$2/g" > $1/all_vs_all.ini
 
+echo "Saving $1/moaf.ini"
+cat moaf.ini | sed "s/$KEY/$1/g" | sed "s/$DB/$2/g" > $1/moaf.ini
 
 echo "Saving $1/bit_score_template.xml"
 cat bit_score_template.xml | sed "s/$KEY/$1/g" > $1/bit_score_template.xml
@@ -33,8 +35,12 @@ cat pe_template.xml | sed "s/$KEY/$1/g" > $1/pe_template.xml
 echo "Saving $1/all_vs_all_template.xml"
 cat all_vs_all_template.xml | sed "s/$KEY/$1/g" > $1/all_vs_all_template.xml
 
+echo "Saving $1/moaf_template.xml"
+cat moaf_template.xml | sed "s/$KEY/$1/g" > $1/moaf_template.xml
+
 
 echo "Start workflow with cd $1;"
+echo "RunWorkflow -t moaf_template.xml -c moaf.ini -i moaf.$1.xml -l moaf.$1.log"
 echo "RunWorkflow -t all_vs_all_template.xml -c all_vs_all.ini -i allvsall.$1.xml -l allvsall.$1.log"
 echo "RunWorkflow -t pe_template.xml -c pe.ini -i pe.$1.xml -l pe.$1.log"
 echo "RunWorkflow -t bit_score_template.xml -c bit_score.ini -i bit_score.$1.xml -l bit_score.$1.log"
