a5bb9069654d314cc5430cd297cc632b227c8b02
clu <clu@c60d6122-3b2e-0410-912d-af40fea696fe>
2003-05-08 16:21:22 +0000

    added ability to encode promer outputs

--- src/perl/mummer2bsml.pl
+++ src/perl/mummer2bsml.pl
@@ -36,10 +36,65 @@ if(!$mummer_file or !$mummer_type or !$output  or exists($options{'help'})) {
 my $doc = BSML::BsmlBuilder->new();
 if($mummer_type == 1) {
     parse_nucmer_coords($mummer_file);
+}elsif($mummer_type == 2) {
+    parse_promer_coords($mummer_file);
+}else {
+    print STDERR "Bad mummer_type.  Aborting...\n";
 }
 
 $doc->write($output);
 chmod 0666, $output;
+
+
+sub parse_promer_coords {
+
+    my $coords_file = shift;
+
+    my @promer;
+    open (PROMER, $coords_file) or die "Unable to open \"$coords_file\" due to $!";
+    while (my $line = <PROMER>) {
+	chomp($line);
+	@promer = split("\t", $line);	
+	#In Promer ref seq refers to query
+	my $ref_name    = $promer[15];
+        my $qry_name    = $promer[16];
+	my $ref_start   = $promer[0];
+	my $ref_end     = $promer[1];
+	my $ref_length  = $promer[4];
+	my $qry_start   = $promer[2];
+        my $qry_end     = $promer[3];
+	my $qry_length  = $promer[5];
+	my $percent_id  = $promer[6];
+        my $percent_sim = $promer[7];
+	my $percent_stop = $promer[8];
+	my $qry_asmbl_length = $promer[10];
+        my $ref_asmbl_length = $promer[9];
+	my $frame_ref = $promer[13];
+        my $frame_qry = $promer[14];
+
+	my $aln = $doc->createAndAddSequencePairAlignment( 'refseq'        => $ref_name,
+							   'compseq'       => $qry_name,
+                                                           'complength'    => $qry_asmbl_length,
+                                                           'reflength'     => $ref_asmbl_length
+							 ); 
+	my $s = $doc->createAndAddSequencePairRun( 'alignment_pair'   => $aln,
+						   'refpos'           => $ref_start,
+						   'runlength'        => $ref_length,
+						   'comppos'          => $qry_start,
+						   'comprunlength'    => $qry_length,
+                                                   'percent_identity' => $percent_id,
+						   'percent_similarity' => $percent_sim
+                                                  );
+	#additional attributes
+	$s->addBsmlAttr( 'percent_stop_codon',  $percent_stop);
+	$s->addBsmlAttr( 'refframe', $frame_ref );
+	$s->addBsmlAttr( 'compframe', $frame_qry );
+
+
+
+    }
+
+}
 
 
 sub parse_nucmer_coords {
