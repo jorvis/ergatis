c4a624f857519b347da0c8710f6d60f89e17ff24
angiuoli <angiuoli@c60d6122-3b2e-0410-912d-af40fea696fe>
2003-06-11 20:55:42 +0000

    added back condor_submit. don't know why this was deleted

--- src/perl/run_pe.pl
+++ src/perl/run_pe.pl
@@ -151,6 +151,7 @@ ENDCONFIG
     
     $logger->debug("Running 'condor_submit /usr/local/scratch/pe$$.condor.config'");
     unlink "$outputfile.done" if(-e "$outputfile.done");
+    print `condor_submit /usr/local/scratch/pe$$.condor.config`;
     my $counter=0;
     while (! (-e "$outputfile.done")){
 	if($counter>6){
