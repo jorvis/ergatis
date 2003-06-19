5584ddbf0e43994318ed831e12300826a9dbe64c
chauser <chauser@c60d6122-3b2e-0410-912d-af40fea696fe>
2003-06-19 19:21:26 +0000

    control for cogs workflow

---
+++ workflow/run_cogs.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+RUNPATH=.
+
+#process command options
+
+while getopts d:s:h opt
+do case "$opt" in
+      d) database=$OPTARG;;
+      s) search=$OPTARG;;
+      h) echo "Usage: `basename $0` -d dbname";
+	  exit;;
+      esac
+done
+
+#Set up workflow vars
+source $RUNPATH/wfenv_bash.sh
+
+project="$database$$"
+program="cogs"
+
+    $RUNPATH/set_runtime_vars.sh -d $database -p $project -r $program -a $search
+
+/usr/local/devel/ANNOTATION/workflow/CreateWorkflow -t $project/${program}_template.xml -c $project/$program.ini -i $project/$program.xml -l $project/$program.$$.log
+/usr/local/devel/ANNOTATION/workflow/RunWorkflow -i $project/$program.xml -l $project/$program.$$.log
