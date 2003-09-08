d8fb64b31d92a5062e9d37d77f09a4c82ec83754
angiuoli <angiuoli@c60d6122-3b2e-0410-912d-af40fea696fe>
2003-09-08 16:43:37 +0000

    changes for new wf v2

--- workflow/wfenv_bash.sh
+++ workflow/wfenv_bash.sh
@@ -1,15 +1,4 @@
-WF_WORK=/usr/local/devel/ANNOTATION/workflow
-export WF_WORK
+WF_ROOT=/usr/local/devel/ANNOTATION/workflow
+export WF_ROOT
 
-WF_TEMPLATE=$WF_ROOT/templates
-export WF_TEMPLATE
-
-WF_JARS=$WF_ROOT/jars
-export WF_JARS
-
-cp=$WF_JARS/wf.jar:$WF_JARS/castor-0.9.4.1-xml.jar:$WF_JARS/castor-0.9.4.1.jar:$WF_JARS/xerces.jar:$WF_JARS/jconn2.jar
-export cp
-
-PATH=${WF_ROOT}:${WF_ROOT}/bin:${WF_ROOT}/add-ons/bin:/usr/local/java/1.4.1/bin/:$PATH
-export PATH
-
+PATH=${WF_ROOT}/bin:${WF_ROOT}/add-ons/bin:${WF_ROOT}:${PATH}
