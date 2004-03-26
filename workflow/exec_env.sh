9b40c36408c704c35b46adc9da3ce2932c33c3e0
sundaram <sundaram@c60d6122-3b2e-0410-912d-af40fea696fe>
2004-03-26 19:59:51 +0000

    pointing to workflow not workflow-2.1

--- workflow/exec_env.sh
+++ workflow/exec_env.sh
@@ -7,13 +7,13 @@
 # Source this file before using any workflow tools. 
 
 # Set WF_ROOT to the installation directory of workflow.
-WF_ROOT=/usr/local/devel/ANNOTATION/workflow-2.1
+WF_ROOT=/usr/local/devel/ANNOTATION/workflow
 export WF_ROOT
 
-WF_TEMPLATE=/usr/local/devel/ANNOTATION/workflow-2.1/templates
+WF_TEMPLATE=/usr/local/devel/ANNOTATION/workflow/templates
 export WF_TEMPLATE
 
-WF_ROOT_INSTALL=/usr/local/devel/ANNOTATION/workflow-2.1
+WF_ROOT_INSTALL=/usr/local/devel/ANNOTATION/workflow
 export WF_ROOT_INSTALL
 
 #Set PATH
