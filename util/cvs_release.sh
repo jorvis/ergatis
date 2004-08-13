df906a798ec08cf34d0d5a5f0b3d5ee642ebc0e1
angiuoli <angiuoli@c60d6122-3b2e-0410-912d-af40fea696fe>
2004-08-13 19:53:45 +0000

    new merge/branch utils

---
+++ util/cvs_release.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# $Id$
+
+# Copyright (c) 2002 The Institute for Genomic Research. All Rights Reserved.
+
+# This script generates a release tag in cvs
+while getopts r:d:t opt 2>/dev/null
+do case "$opt" in
+    r)    RELEASE_TAG=$OPTARG;;
+    d)    DIRECTORY=$OPTARG;;
+    t)    TEST="-n";;
+    \?)   echo "Usage: `basename $0` -r(release tag) [-d(irectory for output)] [-t(est)]";
+          echo;
+          echo "eg. `basename $0` -r papyrus-v1r8b1";
+          exit 1;;
+  esac
+done
+
+if [ -z $RELEASE_TAG ]; then
+    echo "You must specify a release tag with -r"
+    exit 1;
+fi
+
+if [ -z $DIRECTORY ]; then
+#    echo "Using /tmp as output directory"
+    DIRECTORY=/tmp
+fi
+
+
+cd $DIRECTORY
+if [ -d bug_release ]; then
+    rm -rf bug_release
+fi
+mkdir bug_release
+if [ "$?" -ne 0 ]; then
+    echo "Unable to create bug_release directory"
+    exit 1
+fi
+cd bug_release
+cvs -Q co papyrus_all
+cvs -Q $TEST tag $RELEASE_TAG
+echo "Added tag $RELEASE_TAG in papyrus_all"
