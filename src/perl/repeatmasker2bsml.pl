1b6d2990641891f94743250aed821f69473a5eac
jorvis <jorvis@c60d6122-3b2e-0410-912d-af40fea696fe>
2005-08-23 14:15:11 +0000

    fixed header and perl path

--- src/perl/repeatmasker2bsml.pl
+++ src/perl/repeatmasker2bsml.pl
@@ -1,7 +1,4 @@
-#!/usr/local/devel/ANNOTATION/perl/bin/perl
-
-eval 'exec /usr/local/devel/ANNOTATION/perl/bin/perl  -S $0 ${1+"$@"}'
-    if 0; # not running under some shell
+#!/usr/local/bin/perl
 
 =head1  NAME 
 
