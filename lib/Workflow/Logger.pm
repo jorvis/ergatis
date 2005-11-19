3b9af02d521d4a1bb0f547e9fe557b3e514be1df
jorvis <jorvis@c60d6122-3b2e-0410-912d-af40fea696fe>
2005-11-19 23:55:06 +0000

    temporarily crippling all methods using generic autoload method.  this is to test its relief on the netapp.

--- lib/Workflow/Logger.pm
+++ lib/Workflow/Logger.pm
@@ -34,8 +34,8 @@ based upon a "debug" level will be provided.
 use strict;
 use File::Basename;
 use Data::Dumper;
-use base qw(Log::Log4perl);
-use Log::Log4perl::Level;
+#use base qw(Log::Log4perl);
+#use Log::Log4perl::Level;
 
 
 my $_DEFAULT_LOG_CATEGORY = "Logger";
@@ -83,7 +83,7 @@ B<Returns:> None.
 
 sub _init {
     my $self = shift;
-    $self->{_DEFAULT_LOG_LEVEL} = $WARN;
+#    $self->{_DEFAULT_LOG_LEVEL} = $WARN;
     $self->{_LOGGER_CONF} = $ENV{LOG4PERL_CONF};
     $self->{_LOG_LEVEL} = 0; #use this parameter to inc/dec log level from default
     $self->{_MODE} = "clobber"; #set to 'append' to append to file
@@ -93,7 +93,7 @@ sub _init {
     foreach my $key (keys %arg) {
         $self->{"_$key"} = $arg{$key}
     }
-    $self->_init_class_loggers();
+#    $self->_init_class_loggers();
 }
 
 =item $obj->_init_class_loggers($loglevel)
@@ -154,17 +154,23 @@ sub _init_class_loggers {
 
 sub get_logger {
     my $name = shift;
-    if(defined $name){
-	return Log::Log4perl->get_logger($_DEFAULT_LOG_CATEGORY."::".$name);
-    }
-    else{
-	return Log::Log4perl->get_logger($_DEFAULT_LOG_CATEGORY);
-    }
+#   if(defined $name){
+#	return Log::Log4perl->get_logger($_DEFAULT_LOG_CATEGORY."::".$name);
+#   }
+#   else{
+#	return Log::Log4perl->get_logger($_DEFAULT_LOG_CATEGORY);
+#   }
+    return $name;
 }
 
 sub get_default_logfilename{
     #return "/tmp/".basename($0).".$$.log";
     return '/dev/null';
+}
+
+sub AUTOLOAD {
+    my ($self, @args) = @_;
+    return 0;
 }
 
 1;
