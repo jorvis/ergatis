d7388991ef2a5d97538dc7a73f7773b31440ecb8
angiuoli <angiuoli@c60d6122-3b2e-0410-912d-af40fea696fe>
2005-12-19 14:45:51 +0000

    added -print option to print releases

--- util/cvs_release.sh
+++ util/cvs_release.sh
@@ -5,17 +5,23 @@
 # Copyright (c) 2002 The Institute for Genomic Research. All Rights Reserved.
 
 # This script generates a release tag in cvs
-while getopts r:d:t opt 2>/dev/null
+while getopts r:d:pt opt 2>/dev/null
 do case "$opt" in
     r)    RELEASE_TAG=$OPTARG;;
     d)    DIRECTORY=$OPTARG;;
+    p)    PRINT="yes";;
     t)    TEST="-n";;
-    \?)   echo "Usage: `basename $0` -r(release tag) [-d(irectory for output)] [-t(est)]";
+    \?)   echo "Usage: `basename $0` -r(release tag) [-d(irectory for output)] [-t(est)] [-p(rint latest release]";
           echo;
           echo "eg. `basename $0` -r ergatis-v1r8b1";
           exit 1;;
   esac
 done
+
+if [ $PRINT ]; then
+    cvs log cvs_release.sh | perl -e 'while(my $line=<STDIN>){if($line=~/symbolic/){$retrieve=1;}if($retrieve==1){($tag) = ($line =~ /(ergatis-v\d+r\d+b\d+)\:/);if($tag){print $tag,"\n";exit}}}'
+    exit 1;
+fi
 
 if [ -z $RELEASE_TAG ]; then
     echo "You must specify a release tag with -r"
