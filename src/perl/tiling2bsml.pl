feefb742aab4539ef975133af4b747be60fca8e0
agussman <agussman@c60d6122-3b2e-0410-912d-af40fea696fe>
2006-01-05 15:56:04 +0000

    All sequences are included in bsml document and given analysis links

--- src/perl/tiling2bsml.pl
+++ src/perl/tiling2bsml.pl
@@ -6,6 +6,9 @@ eval 'exec /usr/local/packages/perl-5.8.5/bin/perl  -S $0 ${1+"$@"}'
 eval 'exec /usr/local/packages/perl-5.8.5/bin/perl  -S $0 ${1+"$@"}'
     if 0; # not running under some shell
 
+eval 'exec /usr/local/packages/perl-5.8.5/bin/perl  -S $0 ${1+"$@"}'
+    if 0; # not running under some shell
+
 # this is temporary for development
 # use lib "/export/CVS/bsml/src/";
 
@@ -73,11 +76,13 @@ BEGIN {
 my %options = ();
 GetOptions( \%options, 
 	    'tilingPath|t=s', 
-	    'referencePath|r=s', 
+	    'referencePath|r=s',
+	    'queryPath|q=s',
 	    'outFile|o=s',
 	    'reference_class=s',
 	    'query_class=s',
-	    'outFile|o=s',
+#	    'outFile|o=s',
+	    'analysis_id|a=s',
 	    'help|h' 
 	    ) || pod2usage();
 
@@ -90,8 +95,11 @@ if( !( $options{'tilingPath'} ) ) {
     pod2usage( {-exitval=>1, -verbose => 2, -output => \*STDOUT} );
 }
 
-#path to original reference mfsa required for SeqDataImport in bsml
+#path to original reference and query mfsa required for SeqDataImport in bsml
 if( !( $options{'referencePath'} ) ) {
+    pod2usage( {-exitval=>1, -verbose => 2, -output => \*STDOUT} );
+}
+if( !( $options{'queryPath'} ) ) {
     pod2usage( {-exitval=>1, -verbose => 2, -output => \*STDOUT} );
 }
 
@@ -107,6 +115,11 @@ if( !( $options{'query_class'} ) ) {
     pod2usage( {-exitval=>1, -verbose => 2, -output => \*STDOUT} );
 }
 
+## handle some defaults
+if (! $options{analysis_id}) {
+    $options{analysis_id} = 'unknown_analysis';
+}
+
 # The output of show_tiling is tab deliminted with the following fields
 # 0 - start in ref
 # 1 - end in ref
@@ -117,15 +130,18 @@ if( !( $options{'query_class'} ) ) {
 # 6 - orientation
 # 7 - id
 
-#my $repositoryPath = $options{'bsml_repository_path'};
-
-open( TILINGS, $options{'tilingPath'} ) or die "Unable to open $options{'tilingPath'}";
 my $doc = new BSML::BsmlBuilder;
 my $ref_id = "";
 my $ref_length = -1;
 my $reference_class = $options{'reference_class'};
 my $query_class = $options{'query_class'};
 my %unique_contigs; #track that each contig is only tiled once, otherwise it's ambiguous
+
+#predefine sequences w/ analysis element to track unused sequences
+predefineSequences($doc, $options{'referencePath'}, $reference_class);   #reference
+predefineSequences($doc, $options{'queryPath'}, $query_class);#query
+
+open( TILINGS, $options{'tilingPath'} ) or die "Unable to open $options{'tilingPath'}";
 while (my $line = <TILINGS>) {
     chomp $line;
     #Add in a reference sequence
@@ -133,21 +149,25 @@ while (my $line = <TILINGS>) {
 	$ref_id = $1;
 	$ref_length = $2;
 
-	my $ref_sequence = $doc->createAndAddSequence(
-				    $ref_id, #id
-				    undef, #title
-				    $ref_length, #length
-				    'dna', #molecule
-				    $reference_class #class
-				    );
-	#Seq-data-import/@identifier must equal the fasta header up to the first space	    
-	my $ref_sequence_data = $doc->createAndAddSeqDataImport(
- 				    $ref_sequence,              # Sequence element object reference
- 				    'fasta',                    # //Seq-data-import/@format
- 				    $options{'referencePath'},               # //Seq-data-import/@source
- 				    undef,                      # //Seq-data-import/@id
- 				    $ref_id                     # //Seq-data-import/@identifier
- 				    );
+	if( !( $doc->returnBsmlSequenceByIDR( $ref_id )) ){
+	    die "Reference sequence $ref_id missing from predefined sequences";
+	}
+
+# 	my $ref_sequence = $doc->createAndAddSequence(
+# 				    $ref_id, #id
+# 				    undef, #title
+# 				    $ref_length, #length
+# 				    'dna', #molecule
+# 				    $reference_class #class
+# 				    );
+# 	#Seq-data-import/@identifier must equal the fasta header up to the first space	    
+# 	my $ref_sequence_data = $doc->createAndAddSeqDataImport(
+#  				    $ref_sequence,              # Sequence element object reference
+#  				    'fasta',                    # //Seq-data-import/@format
+#  				    $options{'referencePath'},               # //Seq-data-import/@source
+#  				    undef,                      # //Seq-data-import/@id
+#  				    $ref_id                     # //Seq-data-import/@identifier
+#  				    );
     }
     #Add in a tile
     else {
@@ -176,33 +196,148 @@ while (my $line = <TILINGS>) {
 	    die "Length of scaffold $ref_id ($ref_length), less than tile ($tile_id) ($tile_length)";
 	}
 
-	#check that the coordinates are positive
-	if ($tile_start < 0 || $tile_length < 0) {
-	    die "Invalid tile_start ($tile_start) or tile_length ($tile_length) for $tile_id in $options{'tilingPath'}";
-	}
-	
+	#negative coords are okay, they occur with -c and tiling across origin
+	#if ($tile_start < 0 || $tile_length < 0) {
+	#    die "Invalid tile_start ($tile_start) or tile_length ($tile_length) for $tile_id in $options{'tilingPath'}";
+	#}
 
-	my $tile_sequence = $doc->createAndAddSequence(
-				    $tile_id, #id
-				    undef, #title
-				    $tile_length, #length
-				    'dna', #molecule
-				    $query_class #class
-				    );
-	#Seq-data-import/@identifier must equal the fasta header up to the first space	    
-	my $tile_sequence_data = $doc->createAndAddSeqDataImport(
- 				    $tile_sequence,              # Sequence element object reference
- 				    'fasta',                    # //Seq-data-import/@format
- 				    $options{'tilingPath'},               # //Seq-data-import/@source
- 				    undef,                      # //Seq-data-import/@id
- 				    $tile_id                     # //Seq-data-import/@identifier
- 				    );
+	if( !( $doc->returnBsmlSequenceByIDR( $tile_id )) ){
+	    die "Query sequence $tile_id missing from predefined sequences";
+	}
+	else {
+	    my $tile_sequence = $doc->returnBsmlSequenceByIDR( $tile_id );
+
+# 	my $tile_sequence = $doc->createAndAddSequence(
+# 				    $tile_id, #id
+# 				    undef, #title
+# 				    $tile_length, #length
+# 				    'dna', #molecule
+# 				    $query_class #class
+# 				    );
+# 	#Seq-data-import/@identifier must equal the fasta header up to the first space	    
+# 	my $tile_sequence_data = $doc->createAndAddSeqDataImport(
+#  				    $tile_sequence,              # Sequence element object reference
+#  				    'fasta',                    # //Seq-data-import/@format
+#  				    $options{'queryPath'},     # //Seq-data-import/@source
+#  				    undef,                      # //Seq-data-import/@id
+#  				    $tile_id                     # //Seq-data-import/@identifier
+#  				    );
 	$doc->createAndAddNumbering( seq => $tile_sequence,
  				     seqref => $ref_id,
  				     refnum => $tile_start,
- 				     ascending => $tile_ascending );	
+ 				     ascending => $tile_ascending );
+	}	
     }
 }
 close(TILINGS);
 
 $doc->write( $options{'outFile'} );
+
+
+sub predefineSequences {
+    my $doc = shift;
+    my $seqfile = shift;
+    my $class = shift;
+
+    my %seqs = loadMultiSequence( $seqfile );
+
+    for my $seqid ( sort {$a<=>$b} keys %seqs ) {
+        ## capture the first element of the header up to the first whitespace
+        my $id;
+        if ($seqs{$seqid}{h} =~ /^(\S+)/) {
+            $id = $1;
+        } else {
+            die("unrecognized header format: $seqs{$seqid}{h}");
+        }
+
+	my $seqstub = $doc->createAndAddSequence(
+						 $id, #id
+						 undef, #title
+						 length($seqs{$seqid}{s}), #length
+						 'dna', #molecule
+						 $class #class
+						 );
+	#Seq-data-import/@identifier must equal the fasta header up to the first space	    
+	my $dataimport = $doc->createAndAddSeqDataImport(
+ 				    $seqstub,              # Sequence element object reference
+ 				    'fasta',               # //Seq-data-import/@format
+ 				    $seqfile,              # //Seq-data-import/@source
+ 				    undef,                 # //Seq-data-import/@id
+ 				    $id                    # //Seq-data-import/@identifier
+ 				    );
+	$seqstub->addBsmlLink('analysis', '#' . $options{analysis_id}, 'input_of');
+    }
+}
+
+#taken from fasta2bsml.pl
+sub loadMultiSequence {
+    #  USAGE:   loadMultiSequence($filepath)
+    #  RETURNS: hash
+    #
+    #  takes a file or path as an argument.  that file should be a multiple-
+    #  sequence FASTA file.  It returns a hash with a structure like:
+    #      $db{id}{'h'} = header
+    #             {'s'} = sequence without whitespace
+    #
+    #  where id is an incrementing integer that represents that sequence's
+    #  order in the file.
+    #
+    #########################################################################
+    my ($file) = @_;
+    
+    my $seqid = 0;
+    my $seq = '';
+    my $header;
+    my %db;
+    
+    ## load the sequence file
+#    open (my $sfh, "<$file") || $logger->logdie("can't open $file because $!");
+    open (my $sfh, "<$file") || die("can't open $file because $!");
+
+    for (<$sfh>) {
+        ## if we find a header line ...
+        if (/^\>(.*)/) {
+
+            $header = $1;
+
+            ## don't do anything if this is the first sequence
+            if ($seqid == 0) {
+                $seqid++;
+                $db{$seqid}{'h'} = $header;
+                next;
+            } 
+
+            ## remove whitespace
+            $seq =~ s/\s//g;
+ 
+            ## record the previous sequence before starting the new one
+            $db{$seqid}{'s'} = $seq;
+
+            ## increment the id counter
+            $seqid++;
+
+            ## record the new header
+            $db{$seqid}{'h'} = $header;
+
+            ## reset the sequence
+            $seq = '';
+
+        ## else we've found a sequence line
+        } else {
+            ## skip it if it is just whitespace
+            next if (/^\s*$/);
+
+            ## record this portion of the sequence
+            $seq .= $_;
+        }
+    }
+    
+    ## don't forget the last sequence
+    $seq =~ s/\s//g;
+    $db{$seqid}{'s'} = $seq;
+
+    ## close the sequence file
+    close $sfh;
+    
+    return %db;
+}
