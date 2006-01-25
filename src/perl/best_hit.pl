dcd716b7ce064190e91900315a8254ad57ca6bc2
crabtree <crabtree@c60d6122-3b2e-0410-912d-af40fea696fe>
2006-01-25 14:21:21 +0000

    The search_cogs() method was doing an O(n) scan of all the partial COGs' proteins, which was incredibly inefficient.  Replacing the scan with an O(1) hash lookup reduces the run time on the strep data set from 5.5 hours to 27 seconds, but adds less than 2% to the peak memory usage.  Also removed a number of command-line options that weren't used anywhere in the script and updated the documentation to (hopefully) more accurately reflect the script's current usage.  Finally, note that the cluster sizes printed in the output do not include the Jaccard-clustered proteins (if any).  This should be fixed once it's been determined that none of the downstream analyses rely on the (incorrect) current values.

--- src/perl/best_hit.pl
+++ src/perl/best_hit.pl
@@ -1,56 +1,52 @@
 #!/usr/local/bin/perl
 
-# this program accepts a stream of skim results of the best
-# hits among a number of genomes and generates clusters of
-# proteins connected by best hits. The core of each group is
-# a set of proteins connected by BIDIRECTIONAL BEST HITS.
-# In the special case of two genomes, it finds exactly all
-# pairs of bidirectional best hits.
+# This program takes as input a single BTAB file containing
+# 1-way best hits, typically between the proteins in 2 or more 
+# distinct (and preferably complete) genomes.  It generates 
+# clusters of proteins that are connected by bidirectional 
+# best hits.  In the special case of two genomes it will find 
+# clusters that correspond exactly to all pairs of bidirectional
+# best hits.
 
-# PROGRAMMING STRATEGY:
-# Load all best hits into hashes. Run through a first time and
-# calculate "cog pieces" and build a hash of them.  Merge cog pieces
-# as necessary.  Optionally, in a final run, attach "friends".
-
-
-# This version being redone as "structures"
+# A special 22nd column in the BTAB input may be used to store
+# the ids of proteins that have already been clustered with the
+# protein named in the first column.  This is commonly used 
+# to incorporate a set of within-genome Jaccard cluster analyses
+# into the best hit analysis.  When a protein in one of these
+# input clusters is placed into an output cluster, all of the
+# other proteins in that input cluster will be added to the 
+# output also.
 
 require "getopts.pl";
 $usage = <<_MESSAGE_;
-Usage:  fish.pl feat_name[s]
+Usage:
 best_hit.pl [options] > output_file
-    -D     DEBUG              default = off        Show diagnostic messages
-    -h     help               print this message
-    -c     cutoff             default = 0.0001
-    -i      input file
-    -s     secondary cutoff   see -u option
-    -u     unidirectional     add unidirectional hits better than secondary cutoff   
-    -n     minimum            default = 2      minimum set of transitively related.
+    -D     DEBUG              default = off        show diagnostic messages
+    -h     help                                    print this message
+    -c     cutoff             default = 0.0001     best hits with an e-value above this cutoff will be ignored
+    -i     input file                              input btab file
 _MESSAGE_
 
-&Getopts('Dhui:c:s:n:');
+&Getopts('Dhi:c:');
 die "$usage\n" if ($opt_h == 1);
 
 $DEBUG = $opt_D;
 
 $cutoff = (length($opt_c) == 0) ? 0.0001 : $opt_c;
 
-
-$quorum = ($opt_n < 2) ? 2 : $opt_n;  
-
-$DEBUG && print ("DEBUG is ON, cog minimum is $quorum, cutoff is $cutoff\n");
+$DEBUG && print ("DEBUG is ON, cutoff is $cutoff\n");
 
 my %jaccard;
 
-## open the input file
+## open the input btab file
 open(my $ifh, "<$opt_i") || die "can't open input file: $!";
 
-# first load all the hits data
+# first load all the best hits data
 while ($btab = <$ifh>) {
     chomp $btab;
     @data = split (/\t/,$btab);
     if ($data[20] > $cutoff)  {next;}    # discard if a weak hit by E-value.        
-    if ($data[20] < 0)        {next;}    # a little quality control
+    if ($data[20] < 0)        {next;}    # a little quality control; E-value should always be positive.
     if ($data[5] eq "")       {next;}    # a little quality control
     $data[0] =~ s/\-/:/g;
     ($from_tag, $nothing, $from_gi) = split(/:/,$data[0]);
@@ -87,12 +83,12 @@ while ($btab = <$ifh>) {
 
 local (%cogs) = ();
 local ($next_cog) = 1;
-
+local (%prot2cog) = ();
 
 foreach $bidirect (keys(%all_best_hits))        {
   if ($all_best_hits{$bidirect}->{'to_score'} && $all_best_hits{$bidirect}->{'from_score'})     {
       ($front, $back) = split (/\.\./,$bidirect);
-      ($found1, $found2) = &search_cogs(\%cogs, $front, $back);
+      ($found1, $found2) = &search_cogs(\%cogs, \%prot2cog, $front, $back);
     if ($found2 > $found1) {       # make sure $found2 < $found1
         ($found1, $found2) = ($found2, $found1);
         ($front, $back) = ($back, $front);
@@ -101,21 +97,20 @@ foreach $bidirect (keys(%all_best_hits))        {
 
     if ($found1 == -1 && $found2 == -1)      {
         $DEBUG && print("Starting new COG, members $front and $back\n");
-        &new_cog(\%cogs, $next_cog++, $front, $back);
+        &new_cog(\%cogs, \%prot2cog, $next_cog++, $front, $back);
     }    
     elsif ($found2 == -1)                    {
         #add $back to COG that contains $front
         $DEBUG && print("WILL ATTEMPT TO ADD  $back TO COG $found1\n");
-        &add_to_cog(\%cogs, $found1, $back);
+        &add_to_cog(\%cogs, \%prot2cog, $found1, $back);
     }
     elsif ($found1 == $found2)               {
         $DEBUG && print("CONGRATULATIONS: a confirmation of cluster $found1\n");
         $cogs{$found1}->{'connectivity'} += 1;
-
     }
     else                                     {
         $DEBUG && print("MERGING current cogs $found1 and $found2\n");
-        &merge_cogs(\%cogs, $found1, $found2);
+        &merge_cogs(\%cogs, \%prot2cog, $found1, $found2);
     }
   }
 }
@@ -125,6 +120,8 @@ $DEBUG && print ("============================\n");
 foreach $key (keys %cogs)      {
     $n =  $cogs{$key}->{'size'};
     $perfect = $n * ($n - 1) / 2;
+    # JC: note that the reported size does NOT include any Jaccard members that get printed below.
+    # Don't want to change this without first checking that no downstream analysis relies on the incorrect counts.
     print("COG = $key, size $n, connections = $cogs{$key}->{'connectivity'}, perfect = $perfect;\n");
     *return_array = $cogs{$key}->{'listPtr'};
     foreach $term (sort @return_array)    {
@@ -147,53 +144,48 @@ foreach $key (keys %cogs)      {
 # The bidirectional best hit is two proteins.
 # Search both for membership in up to one cluster each.
 sub search_cogs{
-    local ($cogs, $front, $back) = @_;
-    local ($found1, $found2) = (-1, -1);
-    local (*temp_array);
-    $current_cog_count = 0;
-    foreach $key (keys %$cogs)                      {
-        $current_cog_count++;
-	*temp_array = $cogs->{$key}->{'listPtr'};
-        for ($k=0; $k < @temp_array; $k++)     {
-	    $found1 = $key if ($temp_array[$k] eq $front);
-            $found2 = $key if ($temp_array[$k] eq $back);
-        }
+    local ($cogs, $p2c, $front, $back) = @_;
+    my $found1 = $p2c->{$front} || -1;
+    my $found2 = $p2c->{$back} || -1;
+    if ($DEBUG) {
+	my $current_cog_count = scalar(keys %$cogs);
+	print ("Current COG count = $current_cog_count\n");
+	print ("RESEARCH  $front, $back, $found1, $found2 \n");
     }
-    $DEBUG && print ("Current COG count = $current_cog_count\n");
-    $DEBUG && print ("RESEARCH  $front, $back, $found1, $found2 \n");
     return ($found1, $found2);
 }
 
 # The bidirectional best hit is two proteins.
 # Create a new cluster with exactly those two members
 sub new_cog {
-    local ($cogs, $key, $front, $back) = @_;
+    local ($cogs, $p2c, $key, $front, $back) = @_;
     local (@array) = ($front, $back);
     $cogs->{$key}->{'listPtr'} = \@array;
     $cogs->{$key}->{'size'} = 2;
     $cogs->{$key}->{'connectivity'} = 1;     
+    $p2c->{$front} = $key;
+    $p2c->{$back} = $key;
 }
-
 
 # The bidirectional best hit is two proteins.
 # One was already a member of a cluster. Now add in the other.
 sub add_to_cog {
-    local ($cogs, $key, $back) = @_; 
+    local ($cogs, $p2c, $key, $back) = @_; 
     local (*temp_array);
     *temp_array = $cogs->{$key}->{'listPtr'};
     push (@temp_array, $back);
     $cogs->{$key}->{'listPtr'} = \@temp_array;
     $cogs->{$key}->{'size'} += 1;
     $cogs->{$key}->{'connectivity'} += 1;  
+    $p2c->{$back} = $key;
 }
-
 
 # The bidirectional best hit is two proteins.
 # Since these were in two different clusters, those clusters are linked.
 # Append all the members of the donor cluster to the keep cluster,
 # then delete the donor cluster.
 sub merge_cogs {
-    local ($cogs, $key1, $key2) = @_;
+    local ($cogs, $p2c, $key1, $key2) = @_;
     local (*keep_array, *donor_array);
     $DEBUG && print ("MERGE WORKING! $key1, $key2 \n");
     *keep_array = $cogs->{$key1}->{'listPtr'};
@@ -202,6 +194,9 @@ sub merge_cogs {
     $cogs->{$key1}->{'listPtr'} = \@keep_array;
     $cogs->{$key1}->{'size'}   += $cogs->{$key2}->{'size'};
     $cogs->{$key1}->{'connectivity'} += (1 +  $cogs->{$key2}->{'connectivity'});
+    foreach my $p (@donor_array) {
+	$p2c->{$p} = $key1;
+    }
     delete $cogs->{$key2}->{'listPtr'};
     delete $cogs->{$key2}->{'size'};
     delete $cogs->{$key2}->{'connectivity'};
