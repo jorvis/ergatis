3023b5cf4c685cfcb7b6e81cbb41eb74cff6fd0d
bwhitty <bwhitty@c60d6122-3b2e-0410-912d-af40fea696fe>
2006-01-26 19:26:19 +0000

    *** empty log message ***

---
+++ src/perl/convert_snp_output_to_clusterer_input.pl
@@ -0,0 +1,89 @@
+use strict;
+use warnings;
+use IO::File;
+use Getopt::Long qw(:config no_ignore_case);
+
+use MUMer::SnpDataType;
+
+my $in	= new IO::File->fdopen(fileno(STDIN), "r");
+my $out	= new IO::File->fdopen(fileno(STDOUT), "w");
+
+&parse_options;
+&convert_output;
+
+sub print_usage
+{
+	die << "END";
+usage: convert_snp_output_to_clusterer_input.pl [-input|i <show-snps_output>]
+	[-output|o <output>] [-help|h]
+END
+}
+
+sub parse_options
+{
+	my %opts = ();
+	GetOptions(\%opts, "input|i=s", "output|o=s", "help|h");
+	print_usage if $opts{help};
+	$in->open($opts{input}, "r") or
+		die "Error reading from $opts{input}: $!"
+		if $opts{input};
+	$out->open($opts{output}, "w") or
+		die "Error writing to $opts{output} : $!"
+		if $opts{output};
+}
+
+sub convert_output
+{
+	while (my $line = <$in>) {
+		chomp $line;
+		if ($line =~ /^\[/) {
+			MUMer::SnpDataType::SetHeader($line);
+			next;
+		}
+		my @tokens = split /\t/, $line;
+		next if scalar(@tokens) !=
+			MUMer::SnpDataType::GetNumberOfColumns;
+		my $snp = new MUMer::SnpDataType($line);
+		my $query_id = $snp->GetQueryId;
+		my $subj_id = $snp->GetSubjectId;
+
+		my $query_from;
+		my $query_to;
+		my $subj_from;
+		my $subj_to;
+		my $query_strand;
+		my $subj_strand;
+		&set_loc_data(\$query_from, \$query_to, \$query_strand,
+			      $snp->GetQueryFrame,
+			      $snp->GetQueryPosition,
+			      $snp->GetQuerySubstitution);
+		&set_loc_data(\$subj_from, \$subj_to, \$subj_strand,
+			      $snp->GetSubjectFrame,
+			      $snp->GetSubjectPosition,
+			      $snp->GetSubjectSubstitution);
+		&print_data($query_id, $query_from, $query_to, $query_strand);
+		&print_data($subj_id, $subj_from, $subj_to, $subj_strand);
+		$out->print("//\n");
+	}
+}
+
+sub print_data
+{
+	my ($id, $from, $to, $strand) = @_;
+	$out->printf("%s\t%d\t%d\t%s\n", $id, $from, $to, $strand);
+}
+
+sub set_loc_data
+{
+	my ($from, $to, $strand, $frame, $pos, $subst) = @_;
+	#if ($frame > 0) {
+		$$from = $pos;
+		$$to = $subst  ne "." ?  $pos + 1 : $pos;
+		$$strand = $frame > 0 ? "+" : "-";
+	#}
+	#else {
+	#	$$from = $subst ne "." ?  $pos - 1 : $pos;
+	#	$$to = $pos;
+	#	$$strand = "-";
+	#}
+}
---
+++ src/perl/extract_seq_data_from_snp_output.pl
@@ -0,0 +1,62 @@
+use strict;
+use warnings;
+
+use File::Basename;
+
+use Getopt::Long qw(:config no_ignore_case);
+use IO::File;
+use MUMer::SnpDataType;
+
+my $in		= new IO::File->fdopen(fileno(STDIN), "r");
+my $out		= new IO::File("|sort|uniq>/dev/stdout");
+my $INDEL_CHAR	= '.';
+
+&parse_options;
+&extract_data;
+
+sub print_usage
+{
+	my $progname = basename($0);
+	die << "END";
+usage: $progname [--input|-i <show-snps_output>]
+	[--output|-o <output>] [--help|-h]
+END
+}
+
+sub parse_options
+{
+	my %opts = ();
+	GetOptions(\%opts, "input|i=s", "output|o=s", "help|h");
+	print_usage if $opts{help};
+	$in->open($opts{input}, "r")
+		or die "Error reading from $opts{input}: $!"
+		if $opts{input};
+	$out->open("|sort|uniq>$opts{output}")
+		or die "Error writing to $opts{output}: $!"
+		if $opts{output};
+}
+
+sub extract_data
+{
+	while (my $line = <$in>) {
+		chomp $line;
+		if ($line =~ /^\[/) {
+			MUMer::SnpDataType::SetHeader($line);
+			next;
+		}
+		my @tokens = split /\t/, $line;
+		next if scalar(@tokens) !=
+			MUMer::SnpDataType::GetNumberOfColumns;
+		my $snp = new MUMer::SnpDataType($line);
+		$out->printf("%s\t%d\t%s\t%s\n", $snp->GetQueryId,
+			     $snp->GetQueryPosition,
+			     $snp->GetQueryFrame > 0 ? "+" : "-",
+			     $snp->GetQuerySubstitution)
+			if $snp->GetQuerySubstitution ne $INDEL_CHAR;
+		$out->printf("%s\t%d\t%s\t%s\n", $snp->GetSubjectId,
+			     $snp->GetSubjectPosition,
+			     $snp->GetSubjectFrame > 0 ? "+" : "-",
+			     $snp->GetSubjectSubstitution)
+			if $snp->GetSubjectSubstitution ne $INDEL_CHAR;
+	}
+}
