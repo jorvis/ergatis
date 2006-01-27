c5b1f600193f1115f2b9bba7129890cd20b11cc7
jorvis <jorvis@c60d6122-3b2e-0410-912d-af40fea696fe>
2006-01-27 21:48:28 +0000

    added component_count_hash method

--- cgi-bin/Monitor.pm
+++ cgi-bin/Monitor.pm
@@ -4,6 +4,34 @@ use POSIX;
 
 use strict;
 
+
+sub component_count_hash {
+    my $pipeline_file = shift;
+    my %components;
+    
+    my $t = XML::Twig->new( twig_roots => {
+                                'commandSet/configMapId' => sub {
+                                                                    my ($t, $elt) = @_;
+
+                                                                    if ($elt->text() =~ /^component_(.+?)\./) {
+                                                                        $components{$1}++;
+                                                                        
+                                                                    ## this part is for those older components that didn't
+                                                                    ## yet have output_token portions within the configMapId
+                                                                    ## this will look funny and fail grouping on some of the
+                                                                    ## displays, but it's better than showing no components at all.
+                                                                    } elsif ($elt->text() =~ /^component_(.+)/) {
+                                                                        $components{$1}++;
+                                                                    }
+                                                                },
+                                          },
+                          );
+    $t->parsefile($pipeline_file);
+    
+    return %components;
+}
+
+
 sub process_command {
     my ($twig, $command) = @_;
     
