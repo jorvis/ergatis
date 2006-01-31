df3b9845b27f7eacc34d2e97c5461ff089acfe86
elee <elee@c60d6122-3b2e-0410-912d-af40fea696fe>
2006-01-31 16:08:18 +0000

    Fixed refseq bug in Seq-align-pair attribute

--- src/perl/berbtab2bsml.pl
+++ src/perl/berbtab2bsml.pl
@@ -147,7 +147,7 @@ sub parse_ber_btabs {
 	    next if(!$btab[0] or !$btab[5]);
 	    $btab[5] =~ s/\|//g;   #get rid of trailing |
 	    $query_protein_id = $btab[0];
-	    $btab[0] = $protID_cdsID->{$btab[0]};
+	    $btab[0] = $protID_cdsID->{$btab[0]} if $protID_cdsID->{$btab[0]};
 	    $query_cds_id = $btab[0];	    
 	    $match_name = $btab[5];
 	    splice(@btab, 19, 1);
