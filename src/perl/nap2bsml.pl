0a3a33bc7f56fcbb61e8be4c46d443ab7fd08d90
bwhitty <bwhitty@c60d6122-3b2e-0410-912d-af40fea696fe>
2006-05-30 20:18:11 +0000

    fixed commit wipeout of null input handling

--- src/perl/nap2bsml.pl
+++ src/perl/nap2bsml.pl
@@ -1,7 +1,5 @@
 #!/usr/local/packages/perl-5.8.5/bin/perl
 
-eval 'exec /usr/local/packages/perl-5.8.5/bin/perl  -S $0 ${1+"$@"}'
-    if 0; # not running under some shell
 BEGIN{foreach (@INC) {s/\/usr\/local\/packages/\/local\/platform/}};
 use lib (@INC,$ENV{"PERL_MOD_DIR"});
 no lib "$ENV{PERL_MOD_DIR}/i686-linux";
@@ -24,6 +22,9 @@ B<--input,-i>
 
 B<--query_file_path,-q>
 	Full path to FASTA file containing query sequence.
+
+B<--query_id>
+	ID of query sequence
 	
 B<--debug,-d> 
     Debug level.  Use a large number to turn on verbose debugging. 
@@ -82,6 +83,7 @@ my %options = ();
 my $results = GetOptions (\%options, 
 			  'input|i=s',
 			  'query_file_path|q=s',
+			  'query_id=s',
               'output|o=s',
               'log|l=s',
               'debug=s',
@@ -195,6 +197,14 @@ while (<$ifh>) {
     $doc->createAndAddBsmlAttribute($run, 'segment_number', $segmentID);
 }
 
+## if there were no results this will create a sequence stub
+my $align = &createAndAddNullResult(
+										doc 			=> $doc,
+  	                                    query_name      => $options{'query_id'},
+  	                                    query_length    => '',
+  	                                    class           => 'assembly',
+  	                               );
+  	 
 ## add the analysis element
 my $analysis = $doc->createAndAddAnalysis(
                             id => 'aat_aa_analysis',
@@ -230,4 +240,17 @@ sub min {
     } else {
         return $num2;
     }
+}
+
+##Adds BSML tags for the case where
+##the query sequence returned no hits
+sub createAndAddNullResult {
+	my %args = @_;
+  	my $doc = $args{'doc'};
+  	 
+  	if ( !( $doc->returnBsmlSequenceByIDR( "$args{'query_name'}")) ){
+  		my $seq = $doc->createAndAddSequence( "$args{'query_name'}", "$args{'query_name'}", '', 'na', $args{'class'} );
+  	    $doc->createAndAddSeqDataImport($seq, 'fasta', $options{'query_file_path'}, '', $args{'query_name'});
+  	    $seq->addBsmlLink('analysis', '#' . $options{'analysis_id'}, 'input_of');
+	}
 }
