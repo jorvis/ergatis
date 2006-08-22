0fd6f5af05c4f31ba084280ed7e411f7ea9def4a
elee <elee@c60d6122-3b2e-0410-912d-af40fea696fe>
2006-08-22 17:56:20 +0000

    Made changes to allow for new delimiters "\0\n" and "\0\t"

--- src/c/chado_record_uniq/chado_record_uniq.cpp
+++ src/c/chado_record_uniq/chado_record_uniq.cpp
@@ -21,19 +21,12 @@ void uniq(TFiles &files, FILE *out)
     char buf[BUF_SIZE];
     auto_ptr<string> prevBuf(new string());
     auto_ptr<string> currBuf(new string());
-
-    //int counter = 0;
-
     for (TFiles::iterator i = files.begin(); i != files.end(); ++i) {
-
-        //printf("Processing file %d\n", ++counter);
-
         while (fgets(buf, sizeof(buf), *i)) {
             if (strlen(buf) < EOR_LEN) {
                 currBuf = prevBuf;
             }
             currBuf->append(buf);
-
             if (newRecord) {
                 string cksum = currBuf->substr(0, 32);
                 if (!written.count(cksum)) {
@@ -57,10 +50,24 @@ void uniq(TFiles &files, FILE *out)
     }
 }
 
+string create_read_pipe(const char *file)
+{
+    string pipe = "perl -pe 's/\\0\\n/\?\?\?\?\\n/g; "
+        "s/\\0\\t/\?\?\\t\?\?/g;' ";
+    return pipe + file;
+}
+
+string create_write_pipe(const char *file)
+{
+    string pipe = "perl -pe 's/\\?\\?\\?\\?\\n/\\0\\n/g; "
+        "s/\\?\\?\\t\\?\\?/\\0\\t/g;'";
+    return pipe + " > " + file;
+}
+
 int main(int argc, char **argv)
 {
     TFiles files;
-    FILE *out = stdout;
+    FILE *out = 0;
     bool help = false;
     int opt;
     const char *OPTSTR = "i:o:h";
@@ -68,7 +75,7 @@ int main(int argc, char **argv)
         switch (opt) {
         case 'i':
         {
-            FILE *fp = fopen(optarg, "r");
+            FILE *fp = popen(create_read_pipe(optarg).c_str(), "r");
             if (!fp) {
                 perror(("Error accessing " + string(optarg)).c_str());
             }
@@ -78,7 +85,7 @@ int main(int argc, char **argv)
             break;
         }
         case 'o':
-            out = fopen(optarg, "w");
+            out = popen(create_write_pipe(optarg).c_str(), "w");
             if (!out) {
                 perror(("Error writing " + string(optarg)).c_str());
             }
@@ -95,15 +102,17 @@ int main(int argc, char **argv)
         return 1;
     }
     if (files.empty()) {
-        files.push_back(stdin);
+        FILE *fp = popen(create_read_pipe("/dev/stdin").c_str(), "r");
+        files.push_back(fp);
+    }
+    if (!out) {
+        out = popen(create_write_pipe("/dev/stdout").c_str(), "w");
     }
     uniq(files, out);
     for (TFiles::iterator i = files.begin(); i != files.end(); ++i) {
         if (*i != stdin) {
-            fclose(*i);
+            pclose(*i);
         }
     }
-    if (out && out != stdout) {
-        fclose(out);
-    }
+    pclose(out);
 }
