4a735deb0e1b74189a862777aa4c3d0dc78e47d2
elee <elee@c60d6122-3b2e-0410-912d-af40fea696fe>
2006-08-23 18:52:44 +0000

    Changed implementation to be more efficient and not require external pipes for modifying data

--- src/c/chado_record_uniq/chado_record_uniq.cpp
+++ src/c/chado_record_uniq/chado_record_uniq.cpp
@@ -8,7 +8,6 @@
 using namespace std;
 
 #define BUF_SIZE 4096
-#define EOR_LEN 5
 
 typedef vector<FILE *> TFiles;
 
@@ -18,20 +17,28 @@ void uniq(TFiles &files, FILE *out)
     TWritten written;
     bool newRecord = true;
     bool validRecord = true;
-    char buf[BUF_SIZE];
-    auto_ptr<string> prevBuf(new string());
-    auto_ptr<string> currBuf(new string());
+    size_t buf_size = BUF_SIZE;
+    char *buf = reinterpret_cast<char *>(malloc(sizeof(char) * buf_size));
+    ssize_t num_read = 0;
+    auto_ptr<vector<char> > prevBuf(new vector<char>());
+    auto_ptr<vector<char> > currBuf(new vector<char>());
+    char delim[] = {0, '\n'};
+    if (!buf) {
+        perror("Cannot allocate buffer");
+        exit(1);
+    }
     for (TFiles::iterator i = files.begin(); i != files.end(); ++i) {
-        while (fgets(buf, sizeof(buf), *i)) {
-            if (strlen(buf) < EOR_LEN) {
+        while ((num_read = getline(&buf, &buf_size, *i)) >= 0) {
+            if (num_read < static_cast<ssize_t>(sizeof(delim))) {
                 currBuf = prevBuf;
             }
-            currBuf->append(buf);
+            copy(buf, buf + num_read, back_inserter(*currBuf));
             if (newRecord) {
-                string cksum = currBuf->substr(0, 32);
+                string cksum(currBuf->begin(), currBuf->begin() + 32);
                 if (!written.count(cksum)) {
                     written.insert(cksum);
-                    fprintf(out, "%s", buf);
+                    fwrite(&currBuf->at(0), sizeof(char),
+                           currBuf->size(), out);
                     validRecord = true;
                 }
                 else {
@@ -40,34 +47,23 @@ void uniq(TFiles &files, FILE *out)
             }
             else {
                 if (validRecord) {
-                    fprintf(out, "%s", buf);
+                    fwrite(&currBuf->at(0), sizeof(char),
+                           currBuf->size(), out);
                 }
             }
-            newRecord = currBuf->substr(currBuf->size() - 5) == "????\n";
+            newRecord = memcmp(&*(currBuf->end() - sizeof(delim)),
+                               delim, sizeof(delim)) == 0;
             prevBuf = currBuf;
-            currBuf = auto_ptr<string>(new string());
+            currBuf = auto_ptr<vector<char> >(new vector<char>());
         }
     }
-}
-
-string create_read_pipe(const char *file)
-{
-    string pipe = "perl -pe 's/\\0\\n/\?\?\?\?\\n/g; "
-        "s/\\0\\t/\?\?\\t\?\?/g;' ";
-    return pipe + file;
-}
-
-string create_write_pipe(const char *file)
-{
-    string pipe = "perl -pe 's/\\?\\?\\?\\?\\n/\\0\\n/g; "
-        "s/\\?\\?\\t\\?\\?/\\0\\t/g;'";
-    return pipe + " > " + file;
+    free(buf);
 }
 
 int main(int argc, char **argv)
 {
     TFiles files;
-    FILE *out = 0;
+    FILE *out = stdout;
     bool help = false;
     int opt;
     const char *OPTSTR = "i:o:h";
@@ -75,7 +71,7 @@ int main(int argc, char **argv)
         switch (opt) {
         case 'i':
         {
-            FILE *fp = popen(create_read_pipe(optarg).c_str(), "r");
+            FILE *fp = fopen(optarg, "r");
             if (!fp) {
                 perror(("Error accessing " + string(optarg)).c_str());
             }
@@ -85,7 +81,7 @@ int main(int argc, char **argv)
             break;
         }
         case 'o':
-            out = popen(create_write_pipe(optarg).c_str(), "w");
+            out = fopen(optarg, "w");
             if (!out) {
                 perror(("Error writing " + string(optarg)).c_str());
             }
@@ -102,17 +98,15 @@ int main(int argc, char **argv)
         return 1;
     }
     if (files.empty()) {
-        FILE *fp = popen(create_read_pipe("/dev/stdin").c_str(), "r");
-        files.push_back(fp);
-    }
-    if (!out) {
-        out = popen(create_write_pipe("/dev/stdout").c_str(), "w");
+        files.push_back(stdin);
     }
     uniq(files, out);
     for (TFiles::iterator i = files.begin(); i != files.end(); ++i) {
         if (*i != stdin) {
-            pclose(*i);
+            fclose(*i);
         }
     }
-    pclose(out);
+    if (out && out != stdout) {
+        fclose(out);
+    }
 }
