03482977a8ce4f9588d51913999c3810a295190b
bwhitty <bwhitty@c60d6122-3b2e-0410-912d-af40fea696fe>
2006-08-30 17:58:31 +0000

    new bsml encoding

--- src/perl/signalp2bsml.pl
+++ src/perl/signalp2bsml.pl
@@ -1,4 +1,5 @@
 #!/usr/local/packages/perl-5.8.5/bin/perl
+
 BEGIN{foreach (@INC) {s/\/usr\/local\/packages/\/local\/platform/}};
 use lib (@INC,$ENV{"PERL_MOD_DIR"});
 no lib "$ENV{PERL_MOD_DIR}/i686-linux";
@@ -61,25 +62,26 @@ use Pod::Usage;
 BEGIN {
 use Workflow::Logger;
 use BSML::BsmlRepository;
-use Papyrus::TempIdCreator;
+use Workflow::IdGenerator;
 use BSML::BsmlBuilder;
 use BSML::BsmlParserTwig;
 }
 
 my %options = ();
 my $results = GetOptions (\%options, 
-			  'input|i=s',
+              'input|i=s',
               'output|o=s',
               'debug|d=s',
               'command_id=s',       ## passed by workflow
               'logconf=s',          ## passed by workflow (not used)
               'project|p=s',
+              'id_repository=s',
               'log|l=s',
-			  'help|h') || pod2usage();
+              'help|h') || pod2usage();
 
 my $logfile = $options{'log'} || Workflow::Logger::get_default_logfilename();
 my $logger = new Workflow::Logger('LOG_FILE'=>$logfile,
-				  'LOG_LEVEL'=>$options{'debug'});
+                  'LOG_LEVEL'=>$options{'debug'});
 $logger = $logger->get_logger();
 
 # display documentation
@@ -98,7 +100,7 @@ my $next_id = 1;
 my $doc = new BSML::BsmlBuilder();
 
 ## we're going to generate ids
-my $idcreator = new Papyrus::TempIdCreator();
+my $idcreator = new Workflow::IdGenerator( id_repository => $options{'id_repository'});
 
 ## open the input file for parsing
 open (my $ifh, $options{'input'}) || $logger->logdie("can't open input file for reading");
@@ -112,145 +114,244 @@ while (<$ifh>) {
 
     ##recognize start of results for an individual sequence
     if ($next_seq_flag || /^-{70}/) {
-	$logger->debug("start of sequence results entry found") if($logger->is_debug());  
-	my %nn_results;
-	my %hmm_results;
-	my $sequence_id;
-	if ($next_seq_flag) {
-		$next_seq_flag = 0;
-		$sequence_id = $_;
-	} else {
-		$sequence_id = <$ifh>;
-	}
-	chomp $sequence_id;
-	$sequence_id =~ s/^>//;
-	$logger->debug("sequence id is '$sequence_id'") if($logger->is_debug());  
-	push(@sequence_ids, $sequence_id);
-		
-	while (my $result_line = <$ifh>) {
-		chomp $result_line;
-		if ($result_line =~ /^SignalP-NN result:/) {
-			$logger->debug("parsing signalp nn results") if($logger->is_debug());  
-			$temp = <$ifh>;
-			chomp $temp;
-			$temp =~ /length = (\d+)/ || $logger->error("Couldn't parse sequence length in NN result.") if ($logger->is_error);
-#			$nn_results{'length'} = $1;
-			$temp = <$ifh>; # we will discard the header line
-			$temp = <$ifh>;
-			chomp $temp;
-			$temp =~ /^  max\. C\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)/ || $logger->logdie("Couldn't parse max. C line in NN result");
-			#$temp =~ /^  max\. C[ ]+([^ ]+)[ ]+([^ ]+)[ ]+([^ ]+)[ ]+([^ ]+)/ || $logger->logdie("Couldn't parse max. C line in NN result");
-			$nn_results{'maxc_pos'} = $1;
-			$nn_results{'maxc_value'} = $2;
-			$nn_results{'maxc_cutoff'} = $3;
-			$nn_results{'maxc_signal_peptide'} = $4;
-			
-			$temp = <$ifh>;
-			chomp $temp;
-			$temp =~ /^  max\. Y\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)/ || $logger->logdie("Couldn't parse max. Y line in NN result");
-			$nn_results{'maxy_pos'} = $1;
-			$nn_results{'maxy_value'} = $2;
-			$nn_results{'maxy_cutoff'} = $3;
-			$nn_results{'maxy_signal_peptide'} = $4;
-			
-			$temp = <$ifh>;
-			chomp $temp;
-			$temp =~ /^  max\. S\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)/ || $logger->logdie("Couldn't parse max. S line in NN result");
-			$nn_results{'maxs_pos'} = $1;
-			$nn_results{'maxs_value'} = $2;
-			$nn_results{'maxs_cutoff'} = $3;
-			$nn_results{'maxs_signal_peptide'} = $4;
-			
-			$temp = <$ifh>;
-			chomp $temp;
-			$temp =~ /^  mean S\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)/ || $logger->logdie("Couldn't parse mean S line in NN result");
-			$nn_results{'means_pos'} = $1;
-			$nn_results{'means_value'} = $2;
-			$nn_results{'means_cutoff'} = $3;
-			$nn_results{'means_signal_peptide'} = $4;
-			
-			$temp = <$ifh>;
-			chomp $temp;
-			$temp =~ /^       D\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)/ || $logger->logdie("Couldn't parse D line in NN result");
-			$nn_results{'d_pos'} = $1;
-			$nn_results{'d_value'} = $2;
-			$nn_results{'d_cutoff'} = $3;
-			$nn_results{'d_signal_peptide'} = $4;
-			
-			$temp = <$ifh>;
-			chomp $temp;
-			if ($temp =~ /^# Most likely cleavage site between pos\. (\d+) and (\d+): ([A-Z\-]+)$/) {
-				$result_ref_hash{$sequence_id, 'nn'} = \%nn_results;
-				$nn_results{'cleavage_site_position'} = $1."-".$2;
-			} else {
-				$result_ref_hash{$sequence_id, 'nn'} = \%nn_results;
-			}
+    $logger->debug("start of sequence results entry found") if($logger->is_debug());  
+    my %nn_results;
+    my %hmm_results;
+    my $sequence_id;
+    if ($next_seq_flag) {
+        $next_seq_flag = 0;
+        $sequence_id = $_;
+    } else {
+        $sequence_id = <$ifh>;
+    }
+    chomp $sequence_id;
+    $sequence_id =~ s/^>//;
+    $logger->debug("sequence id is '$sequence_id'") if($logger->is_debug());  
+    push(@sequence_ids, $sequence_id);
+        
+    while (my $result_line = <$ifh>) {
+        chomp $result_line;
+        if ($result_line =~ /^SignalP-NN result:/) {
+            $logger->debug("parsing signalp nn results") if($logger->is_debug());  
+            $temp = <$ifh>;
+            chomp $temp;
+            $temp =~ /length = (\d+)/ || $logger->error("Couldn't parse sequence length in NN result.") if ($logger->is_error);
+#           $nn_results{'length'} = $1;
+            $temp = <$ifh>; # we will discard the header line
+            $temp = <$ifh>;
+            chomp $temp;
+            $temp =~ /^  max\. C\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)/ || $logger->logdie("Couldn't parse max. C line in NN result");
+            #$temp =~ /^  max\. C[ ]+([^ ]+)[ ]+([^ ]+)[ ]+([^ ]+)[ ]+([^ ]+)/ || $logger->logdie("Couldn't parse max. C line in NN result");
+            $nn_results{'maxc_pos'} = $1;
+            $nn_results{'maxc_value'} = $2;
+            $nn_results{'maxc_cutoff'} = $3;
+            $nn_results{'maxc_signal_peptide'} = $4;
+            
+            $temp = <$ifh>;
+            chomp $temp;
+            $temp =~ /^  max\. Y\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)/ || $logger->logdie("Couldn't parse max. Y line in NN result");
+            $nn_results{'maxy_pos'} = $1;
+            $nn_results{'maxy_value'} = $2;
+            $nn_results{'maxy_cutoff'} = $3;
+            $nn_results{'maxy_signal_peptide'} = $4;
+            
+            $temp = <$ifh>;
+            chomp $temp;
+            $temp =~ /^  max\. S\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)/ || $logger->logdie("Couldn't parse max. S line in NN result");
+            $nn_results{'maxs_pos'} = $1;
+            $nn_results{'maxs_value'} = $2;
+            $nn_results{'maxs_cutoff'} = $3;
+            $nn_results{'maxs_signal_peptide'} = $4;
+            
+            $temp = <$ifh>;
+            chomp $temp;
+            $temp =~ /^  mean S\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)/ || $logger->logdie("Couldn't parse mean S line in NN result");
+            $nn_results{'means_pos'} = $1;
+            $nn_results{'means_value'} = $2;
+            $nn_results{'means_cutoff'} = $3;
+            $nn_results{'means_signal_peptide'} = $4;
+            
+            $temp = <$ifh>;
+            chomp $temp;
+            $temp =~ /^       D\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)\s+([^\s]+)/ || $logger->logdie("Couldn't parse D line in NN result");
+            $nn_results{'d_pos'} = $1;
+            $nn_results{'d_value'} = $2;
+            $nn_results{'d_cutoff'} = $3;
+            $nn_results{'d_signal_peptide'} = $4;
+            
+            $temp = <$ifh>;
+            chomp $temp;
+            if ($temp =~ /^# Most likely cleavage site between pos\. (\d+) and (\d+): ([A-Z\-]+)$/) {
+                $result_ref_hash{$sequence_id, 'nn'} = \%nn_results;
+                $nn_results{'cleavage_site_position'} = $1."-".$2;
+            } else {
+                $result_ref_hash{$sequence_id, 'nn'} = \%nn_results;
+            }
 
-		} elsif ($result_line =~ /^SignalP-HMM result:/) {
-			$logger->debug("parsing signalp hmm results") if($logger->is_debug());  
-			$temp = <$ifh>; # we will discard the header line
-			$temp = <$ifh>;
-			chomp $temp;
-			$temp =~ /^Prediction: (.*)$/ || $logger->logdie("Couldn't parse prediction line in HMM result");
-			$hmm_results{'prediction'} = $1;
-			$temp = <$ifh>;
-			chomp $temp;
-			$temp =~ /^Signal peptide probability: (.*)$/ || $logger->logdie("Couldn't parse signal peptide probability line in HMM result");
-			$hmm_results{'signal_peptide_probability'} = $1;
-			$temp = <$ifh>;
-			chomp $temp;
-			if ($temp =~ /^Signal anchor probability: (.*)$/) {
-				$hmm_results{'signal_anchor_probability'} = $1;
-				$temp = <$ifh>;
-				chomp $temp;			
-			}
-			#print $temp."\n";
-			$temp =~ /^Max cleavage site probability: ([0-9]\.[0-9]{3}) between pos\. ([0-9\-]+)\s+and\s+([0-9\-]+)/ || $logger->logdie("Couldn't parse max cleavage site probability line in HMM result");
-			$hmm_results{'max_cleavage_site_probability'} = $1;
-			$hmm_results{'cleavage_site_position'} = $2."-".$3;
+        } elsif ($result_line =~ /^SignalP-HMM result:/) {
+            $logger->debug("parsing signalp hmm results") if($logger->is_debug());  
+            $temp = <$ifh>; # we will discard the header line
+            $temp = <$ifh>;
+            chomp $temp;
+            $temp =~ /^Prediction: (.*)$/ || $logger->logdie("Couldn't parse prediction line in HMM result");
+            $hmm_results{'prediction'} = $1;
+            $temp = <$ifh>;
+            chomp $temp;
+            $temp =~ /^Signal peptide probability: (.*)$/ || $logger->logdie("Couldn't parse signal peptide probability line in HMM result");
+            $hmm_results{'signal_peptide_probability'} = $1;
+            $temp = <$ifh>;
+            chomp $temp;
+            if ($temp =~ /^Signal anchor probability: (.*)$/) {
+                $hmm_results{'signal_anchor_probability'} = $1;
+                $temp = <$ifh>;
+                chomp $temp;            
+            }
+            #print $temp."\n";
+            $temp =~ /^Max cleavage site probability: ([0-9]\.[0-9]{3}) between pos\. ([0-9\-]+)\s+and\s+([0-9\-]+)/ || $logger->logdie("Couldn't parse max cleavage site probability line in HMM result");
+            $hmm_results{'max_cleavage_site_probability'} = $1;
+            $hmm_results{'cleavage_site_position'} = $2."-".$3;
 
-			$result_ref_hash{$sequence_id, 'hmm'} = \%hmm_results;
-			last;
-		} elsif ($result_line =~ /^-{70}/) { ## we'll hit this if result file contains only nn results
-			$logger->debug("found beginning of new results block after nn results\nshould only occur if run method was 'nn' only") if($logger->is_debug());  
-			$next_seq_flag = 1; ## so flag that we've hit the start of next sequence's results
-			last;
-		}
-	}
+            $result_ref_hash{$sequence_id, 'hmm'} = \%hmm_results;
+            last;
+        } elsif ($result_line =~ /^-{70}/) { ## we'll hit this if result file contains only nn results
+            $logger->debug("found beginning of new results block after nn results\nshould only occur if run method was 'nn' only") if($logger->is_debug());  
+            $next_seq_flag = 1; ## so flag that we've hit the start of next sequence's results
+            last;
+        }
+    }
     }
 }
 
-    foreach my $s(@sequence_ids) {
-    	my $seq = $doc->createAndAddSequence($s, undef, '', 'aa', 'polypeptide');
-	foreach my $a(('nn','hmm')) {
-	   	$seq->addBsmlLink('analysis', '#signalp_'.$a.'_analysis', 'input_of');
-	}
-	
-	my $ft;
-	foreach my $a(('nn','hmm')) {
-		if (
-			($a eq 'hmm' && $result_ref_hash{$s,$a}->{'prediction'} eq 'Signal peptide' )
-			|| 
-			($a eq 'nn' && $result_ref_hash{$s,$a}->{'d_signal_peptide'} eq 'YES') ) {
-		if (!$ft){
-        		$ft  = $doc->createAndAddFeatureTable($seq);
-		}
-		my $new_id = $idcreator->new_id( db      => $options{project},
-                                                 so_type => 'signal_peptide',                                           	 prefix  => $options{command_id},
-                                                );
-        		my $signalp = $doc->createAndAddFeature($ft, $new_id, '', 'signal_peptide');
-	   	   	$signalp->addBsmlLink('analysis', '#signalp_'.$a.'_analysis', 'computed_by');
+foreach my $s(@sequence_ids) {
+    my $seq = $doc->createAndAddSequence($s, undef, '', 'aa', 'polypeptide');
 
-			foreach my $k(keys %{$result_ref_hash{$s,$a}}) {
-				$doc->createAndAddBsmlAttribute($signalp, $k, $result_ref_hash{$s,$a}->{$k} );
-			}
-		}
-	}
+    foreach my $a(('nn','hmm')) {
+        $seq->addBsmlLink('analysis', '#signalp_'.$a.'_analysis', 'input_of');
     }
+    
+    my $ft;
+
+    ## create BSML for nn predictions
+    if ($result_ref_hash{$s,'nn'}->{'cleavage_site_position'}) {
+        if (!$ft){
+            $ft  = $doc->createAndAddFeatureTable($seq);
+        }
+        ## parse out cleavage site coords
+        my @coords = parse_position($result_ref_hash{$s,'nn'}->{'cleavage_site_position'});
+        my $site_pos = shift(@coords);
+        
+        ## create signal peptide feature
+        my $signalp_id = $idcreator->next_id( project   => $options{project},
+                                         type       => 'signal_peptide',
+                                       );
+        my $signalp = $doc->createAndAddFeature($ft, $signalp_id, '', 'signal_peptide');
+        $signalp->addBsmlIntervalLoc('0', $site_pos);
+        $signalp->addBsmlLink('analysis', '#signalp_nn_analysis', 'computed_by');
+        foreach my $att((
+                            'maxs_pos',
+                            'maxs_value',
+                            'maxs_cutoff',
+                            'maxs_signal_peptide',
+                            'means_pos',
+                            'means_value',
+                            'means_cutoff',
+                            'means_signal_peptide',
+                            'd_pos',
+                            'd_value',
+                            'd_cutoff',
+                            'd_signal_peptide',
+                        )) {
+            $doc->createAndAddBsmlAttribute(
+                                        $signalp,
+                                        $att, 
+                                        $result_ref_hash{$s,'nn'}->{$att}
+                                       );
+        }
+        
+        ## create cleavage site feature
+        my $csite_id = $idcreator->next_id( project => $options{project},
+                                         type       => 'cleavage_site',
+                                       );
+
+        my $cleavage_site = $doc->createAndAddFeature($ft, $csite_id, '', 'cleavage_site');
+        $cleavage_site->addBsmlLink('analysis', '#signalp_nn_analysis', 'computed_by');
+        $cleavage_site->addBsmlSiteLoc($site_pos);
+        $doc->createAndAddBsmlAttribute(
+                                        $cleavage_site, 
+                                        'max_cleavage_site_probability', 
+                                        $result_ref_hash{$s,'nn'}->{'max_cleavage_site_probability'}
+                                       );
+        foreach my $att((
+                            'maxc_pos',
+                            'maxc_value',
+                            'maxc_cutoff',
+                            'maxc_signal_peptide',
+                            'maxy_pos',
+                            'maxy_value',
+                            'maxy_cutoff',
+                            'maxy_signal_peptide',
+                        )) {
+            $doc->createAndAddBsmlAttribute(
+                                        $cleavage_site,
+                                        $att, 
+                                        $result_ref_hash{$s,'nn'}->{$att}
+                                       );
+        }
+        my $fg = $doc->createAndAddFeatureGroup($seq, '', $signalp_id);
+        $fg->addBsmlFeatureGroupMember($signalp_id, 'signal_peptide');
+        $fg->addBsmlFeatureGroupMember($csite_id, 'cleavage_site');
+    }
+    
+    ## create BSML for hmm predictions
+    if ($result_ref_hash{$s,'hmm'}->{'cleavage_site_position'}) {
+        if (!$ft){
+            $ft  = $doc->createAndAddFeatureTable($seq);
+        }
+        
+        ## parse out cleavage site coords
+        my @coords = parse_position($result_ref_hash{$s,'hmm'}->{'cleavage_site_position'});
+        my $site_pos = shift(@coords);
+            
+        ## create signal peptide feature
+        my $signalp_id = $idcreator->next_id( project   => $options{project},
+                                         type       => 'signal_peptide',
+                                       );
+        my $signalp = $doc->createAndAddFeature($ft, $signalp_id, '', 'signal_peptide');
+        $signalp->addBsmlIntervalLoc('0', $site_pos);
+        $signalp->addBsmlLink('analysis', '#signalp_hmm_analysis', 'computed_by');
+        $doc->createAndAddBsmlAttribute(
+                                        $signalp, 
+                                        'signal_anchor_probability', 
+                                        $result_ref_hash{$s,'hmm'}->{'signal_anchor_probability'}
+                                       );
+        $doc->createAndAddBsmlAttribute(
+                                        $signalp, 
+                                        'signal_peptide_probability', 
+                                        $result_ref_hash{$s,'hmm'}->{'signal_peptide_probability'}
+                                       );
+
+        ## create cleavage site feature
+        my $csite_id = $idcreator->next_id( project => $options{project},
+                                         type       => 'cleavage_site',
+                                       );
+
+        my $cleavage_site = $doc->createAndAddFeature($ft, $csite_id, '', 'cleavage_site');
+        $cleavage_site->addBsmlLink('analysis', '#signalp_hmm_analysis', 'computed_by');
+        $cleavage_site->addBsmlSiteLoc($site_pos);
+        $doc->createAndAddBsmlAttribute(
+                                        $cleavage_site, 
+                                        'max_cleavage_site_probability', 
+                                        $result_ref_hash{$s,'hmm'}->{'max_cleavage_site_probability'}
+                                       );
+        my $fg = $doc->createAndAddFeatureGroup($seq, '', $signalp_id);
+        $fg->addBsmlFeatureGroupMember($signalp_id, 'signal_peptide');
+        $fg->addBsmlFeatureGroupMember($csite_id, 'cleavage_site');
+    }
+}
     
 ## add the analysis element
 foreach my $a(('nn','hmm')) {
-	my $analysis = $doc->createAndAddAnalysis(
+    my $analysis = $doc->createAndAddAnalysis(
                             id => 'signalp_'.$a.'_analysis',
                             sourcename => $options{'output'},
                           );
@@ -278,11 +379,11 @@ sub check_parameters {
 ## parse_position parses the position field to give an array containing
 ## start [and stop] sites
 sub parse_position {
-	my ($position) = @_;
-	if ($position =~ /^\s*([^\s]+)\s*$/) {
-		return split("-", $1);
-	} else {
-		return ();
-	}
+    my ($position) = @_;
+    if ($position =~ /^\s*([^\s]+)\s*$/) {
+        return split("-", $1);
+    } else {
+        return ();
+    }
 }
 
