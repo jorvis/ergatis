26dba9716fe993a84d7fc4b0e798b60aac171866
bwhitty <bwhitty@c60d6122-3b2e-0410-912d-af40fea696fe>
2006-09-05 14:56:01 +0000

    added testing_die_if_empty to check skipping hash

--- test_suite/bin/ini_variable_check.pl
+++ test_suite/bin/ini_variable_check.pl
@@ -8,36 +8,37 @@ use Getopt::Long qw(:config no_ignore_case no_auto_abbrev);
 my %options=();
 GetOptions(\%options,
         "ini_template|i=s",
-		"ergatis_install_root|e=s",
-		"man|m",
+        "ergatis_install_root|e=s",
+        "man|m",
         "help|h",
 );
 
 ## We'll make sure that the ergatis install path ends with a /
 unless ($options{'ergatis_install_root'} =~ /\/$/) {
-	$options{'ergatis_install_root'} .= "/";
+    $options{'ergatis_install_root'} .= "/";
 }
 
 my %exempt_ini_files = (
-							'testing_db_compare' => 1,
-					   );
+                            'testing_db_compare' => 1,
+                            'testing_die_if_empty' => 1,
+                       );
 
 my $ini_file = basename($options{'ini_template'});
 
 unless(-e $options{'ini_template'} && $ini_file =~ /^([^\.]+)/) {
-			die $options{'ini_template'}." is not a valid ini template file.\n";
+            die $options{'ini_template'}." is not a valid ini template file.\n";
 }
 my $component = $1;
 my $component_ini = $component."conf.ini";
 
 if ($exempt_ini_files{$component}) {
-	print STDERR "# Skipping processing of $ini_file\n";
-	print STDERR "# Component is exempt from validation checks!\n";
-	exit(0);
+    print STDERR "# Skipping processing of $ini_file\n";
+    print STDERR "# Component is exempt from validation checks!\n";
+    exit(0);
 }
 
 unless(-e $options{'ergatis_install_root'}."docs/$component_ini") {
-	die $options{'ergatis_install_root'}." is not a valid ergatis install directory.\n";
+    die $options{'ergatis_install_root'}." is not a valid ergatis install directory.\n";
 }
 
 my $component_ini_path = $options{'ergatis_install_root'}."docs/".$component_ini;
@@ -59,23 +60,23 @@ open (IN, $options{'ini_template'}) || die "Couldn't open $ini_file for reading"
 print STDERR "# Parsing $ini_file\n";
 $count = 0;
 while (<IN>) {
-	$count++;
-	chomp;
-	if (/(\$;[A-Z0-9_]+\$[^;])/ || /(\$;[A-Z0-9_]+[^\$];)/ || /([^\$];[A-Z0-9_]+\$;)/ || /(\$[^;]?[A-Z0-9_]+\$;)/ || /(\$[A-Z0-9_]+\$)/ || /(;\$[A-Z0-9_]+;\$)/) {
-		print STDERR "## Malformed variable $1 at line $count\n";
-		$err_flag = 1;
-	}
-	#while (/\$;([^;]+)\$;/g) {
-	while (/\$;([^;]+)\$;\s*=/g) {
-		$template_variables{$1} = 1;
-		push(@template_variables, $1);
-		push(@template_variables_lines, $count." | ".$_);
-		if ($template_variables[$#template_variables] eq 'REVISION') {
-			/Revision:([^\$]+)/i;
-			$template_revision = $1;
-			$template_revision =~ s/\s+//g;
-		}
-	}
+    $count++;
+    chomp;
+    if (/(\$;[A-Z0-9_]+\$[^;])/ || /(\$;[A-Z0-9_]+[^\$];)/ || /([^\$];[A-Z0-9_]+\$;)/ || /(\$[^;]?[A-Z0-9_]+\$;)/ || /(\$[A-Z0-9_]+\$)/ || /(;\$[A-Z0-9_]+;\$)/) {
+        print STDERR "## Malformed variable $1 at line $count\n";
+        $err_flag = 1;
+    }
+    #while (/\$;([^;]+)\$;/g) {
+    while (/\$;([^;]+)\$;\s*=/g) {
+        $template_variables{$1} = 1;
+        push(@template_variables, $1);
+        push(@template_variables_lines, $count." | ".$_);
+        if ($template_variables[$#template_variables] eq 'REVISION') {
+            /Revision:([^\$]+)/i;
+            $template_revision = $1;
+            $template_revision =~ s/\s+//g;
+        }
+    }
 }
 close IN;
 
@@ -84,81 +85,81 @@ open (IN, $component_ini_path) || die "Couldn't open $component_ini for reading"
 print STDERR "# Parsing $component_ini\n";
 $count = 0;
 while (<IN>) {
-	$count++;
-	chomp;
-	if (/(\$;[A-Z0-9_]+\$[^;])/ || /(\$;[A-Z0-9_]+[^\$];)/ || /([^\$];[A-Z0-9_]+\$;)/ || /(\$[^;]?[A-Z0-9_]+\$;)/ || /(\$[A-Z0-9_]+\$)/ || /(;\$[A-Z0-9_]+;\$)/) {
-		print STDERR "## Malformed variable $1 at line $count\n";
-		$err_flag = 1;
-	}
-	#while (/\$;([^;]+)\$;/g) {
-	while (/\$;([^;]+)\$;\s*=/g) {
-		$component_conf_variables{$1} = 1;
-		push(@component_conf_variables, $1);
-		push(@component_conf_variables_lines, $count." | ".$_);
-		if ($component_conf_variables[$#component_conf_variables] eq 'REVISION') {
-			/Revision:([^\$]+)/i;
-			$component_conf_revision = $1;
-			$component_conf_revision =~ s/\s+//g;
-		}
-	}
+    $count++;
+    chomp;
+    if (/(\$;[A-Z0-9_]+\$[^;])/ || /(\$;[A-Z0-9_]+[^\$];)/ || /([^\$];[A-Z0-9_]+\$;)/ || /(\$[^;]?[A-Z0-9_]+\$;)/ || /(\$[A-Z0-9_]+\$)/ || /(;\$[A-Z0-9_]+;\$)/) {
+        print STDERR "## Malformed variable $1 at line $count\n";
+        $err_flag = 1;
+    }
+    #while (/\$;([^;]+)\$;/g) {
+    while (/\$;([^;]+)\$;\s*=/g) {
+        $component_conf_variables{$1} = 1;
+        push(@component_conf_variables, $1);
+        push(@component_conf_variables_lines, $count." | ".$_);
+        if ($component_conf_variables[$#component_conf_variables] eq 'REVISION') {
+            /Revision:([^\$]+)/i;
+            $component_conf_revision = $1;
+            $component_conf_revision =~ s/\s+//g;
+        }
+    }
 }
 close IN;
 
 unless ($template_revision == $component_conf_revision) {
-	print STDERR "## Revision numbers of template and component conf do not match!\n";
-	if ($template_revision eq '') {
-		$template_revision = 'N/A';
-	}
-	if ($component_conf_revision eq '') {
-		$component_conf_revision = 'N/A';
-	}
-	print STDERR "##   Template revision: \t$template_revision\n";
-	print STDERR "##   Component revision:\t$component_conf_revision\n";
-	$err_flag = 1;
+    print STDERR "## Revision numbers of template and component conf do not match!\n";
+    if ($template_revision eq '') {
+        $template_revision = 'N/A';
+    }
+    if ($component_conf_revision eq '') {
+        $component_conf_revision = 'N/A';
+    }
+    print STDERR "##   Template revision: \t$template_revision\n";
+    print STDERR "##   Component revision:\t$component_conf_revision\n";
+    $err_flag = 1;
 }
 
 my $head_flag_a = 0;
 
 foreach my $k(keys(%template_variables)) {
-	unless (defined($component_conf_variables{$k})) {
-		$err_flag = 2;
-		unless ($head_flag_a) {
-			print STDERR "##  Variables unique to $ini_file\n";
-			$head_flag_a = 1;
-		}
-		print STDERR "    [\$;$k\$;]\n";
-		my $var_count = scalar(@template_variables);
-		for (my $i = 0; $i < $var_count; $i++) {
-			if ($template_variables[$i] eq $k) {
-				print STDERR "    ".$template_variables_lines[$i]."\n";
-			}
-		}
-	}
+    unless (defined($component_conf_variables{$k})) {
+        $err_flag = 2;
+        unless ($head_flag_a) {
+            print STDERR "##  Variables unique to $ini_file\n";
+            $head_flag_a = 1;
+        }
+        print STDERR "    [\$;$k\$;]\n";
+        my $var_count = scalar(@template_variables);
+        for (my $i = 0; $i < $var_count; $i++) {
+            if ($template_variables[$i] eq $k) {
+                print STDERR "    ".$template_variables_lines[$i]."\n";
+            }
+        }
+    }
 }
 
 my $head_flag_b = 0;
 foreach my $k(keys(%component_conf_variables)) {
-	unless (defined($template_variables{$k})) {
-		$err_flag = 2;
-		unless ($head_flag_b) {
-			print STDERR "##  Variables unique to $component_ini\n";
-			$head_flag_b = 1;
-		}
-		print STDERR "    [\$;$k\$;]\n";
-		my $var_count = scalar(@component_conf_variables);
-		for (my $i = 0; $i < $var_count; $i++) {
-			if ($component_conf_variables[$i] eq $k) {
-				print STDERR "    ".$component_conf_variables_lines[$i]."\n";
-			}
-		}
-	}
+    unless (defined($template_variables{$k})) {
+        $err_flag = 2;
+        unless ($head_flag_b) {
+            print STDERR "##  Variables unique to $component_ini\n";
+            $head_flag_b = 1;
+        }
+        print STDERR "    [\$;$k\$;]\n";
+        my $var_count = scalar(@component_conf_variables);
+        for (my $i = 0; $i < $var_count; $i++) {
+            if ($component_conf_variables[$i] eq $k) {
+                print STDERR "    ".$component_conf_variables_lines[$i]."\n";
+            }
+        }
+    }
 }
 if ($err_flag > 1) {
-	print STDERR "# Variable mismatch in $ini_file / $component_ini\n";
+    print STDERR "# Variable mismatch in $ini_file / $component_ini\n";
 }
 
 #if ($err_flag == 2) {
-#	system('sed -i -r ');
+#   system('sed -i -r ');
 #}
 
 exit($err_flag);
