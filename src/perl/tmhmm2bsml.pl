5f2b3db34f8266bf3dd24957a437a6cb8c4f40d9
kgalens <kgalens@c60d6122-3b2e-0410-912d-af40fea696fe>
2006-10-30 19:32:07 +0000

    Added compress_bsml_output and fasta_input options (for compressing bsml output and including defline bsml attributes to sequences, respectively).

--- src/perl/tmhmm2bsml.pl
+++ src/perl/tmhmm2bsml.pl
@@ -10,12 +10,23 @@ tmhmm2bsml.pl - convert TMHMM output to BSML
 
 =head1 SYNOPSIS
 
-USAGE: tmhmm2bsml.pl --input=/path/to/tmhmm_file --output=/path/to/output.bsml
+USAGE: tmhmm2bsml.pl --input=/path/to/tmhmm_file --output=/path/to/output.bsml --project=aa1 --fasta_input=/path/to/tmhmm/input.fsa --compress_bsml_output=0
 
 =head1 OPTIONS
 
 B<--input,-i> 
     Input file file from a tmhmm run [long format only!].
+
+B<--project>
+    [OPTIONAL]
+    The project (used for id making).
+    Default: unknown
+
+B<--fasta_input>
+    The fasta file used as input to the tmhmm run.
+
+B<--compress_bsml_output>
+    Will create gzipped output
 
 B<--debug,-d> 
     Debug level.  Use a large number to turn on verbose debugging. 
@@ -40,6 +51,10 @@ script supports parsing single or multiple input result sets.
  
 You define the input file using the --input option.  This file does not need any
 special file extension.
+
+If the file does not exist, the program will automatically search for a gzip'ed version 
+(same filename, with the extension '.gz').  If a '.gz' version exists, will read in the 
+gzip'ed file.
 
 =head1 OUTPUT
 
@@ -70,6 +85,8 @@ my $results = GetOptions (\%options,
               'debug|d=s',
               'command_id=s',       ## passed by workflow
               'logconf=s',          ## passed by workflow (not used)
+              'fasta_input=s',
+              'compress_bsml_output=s',
               'project|p=s',
               'log|l=s',
 			  'help|h') || pod2usage();
@@ -83,6 +100,10 @@ $logger = $logger->get_logger();
 if( $options{'help'} ){
     pod2usage( {-exitval=>0, -verbose => 2, -output => \*STDOUT} );
 }
+
+my $defline;
+my $fastaFile;
+my $gzip = 0;
 
 ## make sure all passed options are peachy
 &check_parameters(\%options);
@@ -110,8 +131,10 @@ my %qualifier = (
 					'POSSIBLE N-term signal sequence' => 'n_term_signal',
 				);
 
-## open the input file for parsing
-open (my $ifh, $options{'input'}) || $logger->logdie("can't open input file for reading");
+## open the input file for parsing (even if it's gziped (bug 2591))
+my $mode = "<";
+$mode .= ":gzip" if($options{'input'} =~ /.gz$/);    
+open (my $ifh, $mode, $options{'input'}) || $logger->logdie("can't open input file for reading");
 
 my @sequence_ids;
 my %result_ref_hash;
@@ -176,7 +199,9 @@ while (my $line = <$ifh>) {
 #die();
 
 foreach my $s(keys(%segment_hash)) {
-   	my $seq = $doc->createAndAddSequence($s, undef, '', 'aa', 'polypeptide');
+   	my $seq = $doc->createAndAddSequence($s, $defline, '', 'aa', 'polypeptide');
+    $doc->createAndAddBsmlAttribute( $seq, 'defline', $defline);
+    $doc->createAndAddSeqDataImport( $seq, 'fasta', $fastaInput, $s, $s );
    	$seq->addBsmlLink('analysis', '#tmhmm_analysis', 'input_of');
 	
 	foreach my $prop_ref(@{$property_hash{$s}}) {
@@ -234,7 +259,7 @@ foreach my $s(keys(%segment_hash)) {
                           );
 
 ## now write the doc
-$doc->write($options{'output'});
+$doc->write($options{'output'},,$gzip);
 
 exit();
 
@@ -243,7 +268,7 @@ sub check_parameters {
 	## input file is required
 	unless ($options{'input'}) { 
 		pod2usage({-message => "No input file specified!"});
-	}
+	} 
 	
 	## output file is required
 	unless ($options{'output'}) { 
@@ -251,14 +276,40 @@ sub check_parameters {
 	}
 	
     ## make sure input file exists
-    if (! -e $options{'input'}) { 
-		pod2usage({-message => "Input file '$options{'input'}' does not exist!"});
-	}
+    if (! -e $options{'input'}) {
+        unless(-e $options{'input'}.".gz") {
+            $options{'input'}.=".gz";
+        } else {
+            pod2usage({-message => "Input file '$options{'input'}' does not exist!"});
+        }
+	} 
     
     ## make sure output file doesn't exist yet
     if (-e $options{'output'}) {
 		pod2usage({-message => "Can't create '$options{'output'}' because it already exists!"});
    	}
+
+    ## make sure the fasta input was provided. (Bug 3781)
+    if($options{'fasta_input'}) {
+        $fastaFile = $options{'fasta_input'};
+        open(IN, "$fastaFile") 
+            or pod2usage({-message => "Could not open fasta_input $fastaFile ($!)"});
+        while(<IN>) {
+            chomp;
+            if(/^(>.*)/) {
+                $defline = $1;
+                last;
+            }
+        }
+        close(IN);
+    } else {
+        pod2usage({-message => "Option fasta_input is required"});
+    }
+
+    ## in case they want to compress the bsml output (bug 2591)
+    if($options{'compress_bsml_output'}) {
+        $gzip = 1;
+    }
     
     $options{'project'}    = 'unknown' unless ($options{'project'});
     $options{'command_id'} = '0' unless ($options{'command_id'});
