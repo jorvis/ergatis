60aed2982c8a35dc758844fe191a9ca71626d20a
jorvis <jorvis@c60d6122-3b2e-0410-912d-af40fea696fe>
2006-11-03 22:09:28 +0000

    first working version

--- lib/Ergatis/Pipeline.pm
+++ lib/Ergatis/Pipeline.pm
@@ -48,6 +48,8 @@ umask(0000);
     my %_attributes = (
                         path  => undef,
                         id    => undef,
+                        debug => 1,
+                        debug_file => '/tmp/ergatis.run.debug',
                       );
 
     ## class variables
@@ -73,6 +75,8 @@ umask(0000);
     }
     
     sub run {
+        my ($self, %args) = @_;
+        
         ## path must be defined and exist to run
         if (! defined $_[0]->{path} ) {
             croak("failed to run pipeline: no path set yet.");
@@ -80,7 +84,93 @@ umask(0000);
             croak("failed to run pipeline: pipeline file $_[0]->{path} doesn't exist");
         }
         
+        ## ergatis cfg is required
+        if (! defined $args{ergatis_cfg} ) { croak("ergatis_cfg is a required option") };
+        my $run_dir = $args{ergatis_cfg}->val('paths', 'workflow_run_dir') || croak "workflow_run_dir not found in ergatis.ini";
         
+        ## create a directory from which to run this pipeline
+        if ( -d $run_dir ) {
+            ## make a subdirectory for this pipelineid
+            $run_dir .= '/' . $self->id;
+            if (! -d $run_dir) {
+                mkdir $run_dir || croak "failed to create workflow_run_dir: $run_dir : $!";
+            }
+        } else {
+            croak "Invalid workflow_run_dir (doesn't exist) in ergatis.ini: $run_dir";
+        }
+        
+        if (! -e $args{ergatis_cfg}->val('paths','workflow_log4j') ) {
+            croak "Invalid workflow_log4j in ergatis.ini : " . $args{ergatis_cfg}->val('paths','workflow_log4j');
+        }
+        
+        my $child_pid = fork;
+        if ( $child_pid ){
+            while( ! ( -e $self->path ) ){
+                sleep 3;
+            }
+        } else {
+            ## these have to stay here or the separation doesn't work right
+            close STDOUT;
+            close STDERR;
+            close STDIN;
+
+            #  Fork again.  This helps separate the background process from
+            #  the httpd process.  If we're in the original child, $gpid will
+            #  hold the process id of the "grandchild", and if we're in the
+            #  grandchild it will be zero.
+            my $gpid = fork;
+            if (! $gpid){
+                ## open the debugging file if needed
+                open (my $debugfh, ">>$self->{debug_file}") if $self->{debug};
+                
+                ##debug
+                print $debugfh "debug init\n" if $self->{debug};
+                
+                chdir $run_dir;
+                use POSIX qw(setsid);
+                setsid() or die "Can't start a new session: $!";
+
+                ##debug
+                print $debugfh "got past the POSIX section\n" if $self->{debug};
+
+                $self->_setup_environment( ergatis_cfg => $args{ergatis_cfg} );
+
+                ##debug
+                print $debugfh "got past ENV setup section\n" if $self->{debug};
+
+
+                my $runstring = "$ENV{'WF_ROOT'}/RunWorkflow -i $self->{path} -l $self->{path}.log --logconf=" . $args{ergatis_cfg}->val('paths','workflow_log4j') . " >& $self->{path}.run.out";
+
+                if ( $self->{debug} ) {
+                    #use Data::Dumper;
+                    #print $debugfh Dumper(\%ENV);
+                    print $debugfh "preparing to run $runstring\n";
+                }
+
+                my $rc = 0xffff & system($runstring);
+
+                printf $debugfh "system(%s) returned %#04x: $rc" if $self->{debug};
+                if($rc == 0) {
+                    print $debugfh "ran with normal exit\n" if $self->{debug};
+                } elsif ( $rc == 0xff00 ) {
+                    print $debugfh "command failed: $!\n" if $self->{debug};
+                } elsif (($rc & 0xff) == 0) {
+                    $rc >>= 8;
+                    print $debugfh "ran with non-zero exit status $rc\n" if $self->{debug};
+                } else {
+                    print $debugfh "ran with " if $self->{debug};
+                    if($rc & 0x80){
+                        $rc &= ~0x80;
+                        print $debugfh "coredump from " if $self->{debug};
+                    }
+                    print $debugfh "signal $rc\n" if $self->{debug};
+                }
+
+                close $debugfh if $self->{debug};
+            }
+
+            exit;
+        }
     }
     
     ## accessors
@@ -90,6 +180,33 @@ umask(0000);
     ## modifiers
     sub set_id { $_[0]->{id} = $_[1] }
     sub set_path { $_[0]->{path} = $_[1] }
+    
+    sub _setup_environment {
+        my ($self, %args) = @_;
+
+        ## open the debugging file if needed
+        #open (my $debugfh, ">>$self->{debug_file}2") if $self->{debug};
+
+        ## remove the apache SERVER variables from the environment
+        for my $k (keys %ENV) {
+            if ($k =~ /^SERVER_/ ) {
+                delete $ENV{$k};
+            }
+        }
+
+        $ENV{SGE_ROOT} = $args{ergatis_cfg}->val('grid', 'sge_root');
+        $ENV{SGE_CELL} = $args{ergatis_cfg}->val('grid', 'sge_cell');
+        $ENV{SGE_QMASTER_PORT} = $args{ergatis_cfg}->val('grid', 'sge_qmaster_port');
+        $ENV{SGE_EXECD_PORT} = $args{ergatis_cfg}->val('grid', 'sge_execd_port');
+        $ENV{SGE_ARCH} = $args{ergatis_cfg}->val('grid', 'sge_arch');
+
+        $ENV{WF_ROOT} = $args{ergatis_cfg}->val('paths', 'workflow_root');
+        $ENV{WF_ROOT_INSTALL} = $args{ergatis_cfg}->val('paths', 'workflow_root');
+
+        $ENV{SYBASE} = '/usr/local/packages/sybase';
+        $ENV{PATH} = "$ENV{WF_ROOT}:$ENV{WF_ROOT}/bin:$ENV{WF_ROOT}/add-ons/bin:$ENV{PATH}";
+        $ENV{LD_LIBRARY_PATH} = '';
+    }
 }
 
 1==1;
