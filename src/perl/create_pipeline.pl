4b534bbfdc7e8dc5b7514fe49676080f99b4e20b
angiuoli <angiuoli@c60d6122-3b2e-0410-912d-af40fea696fe>
2006-12-05 15:46:27 +0000

    updated command line opts

--- src/perl/create_pipeline.pl
+++ src/perl/create_pipeline.pl
@@ -1,18 +1,37 @@
 #!/usr/local/bin/perl
 
 use lib (@INC,$ENV{"PERL_MOD_DIR"});
+use strict;
+use Getopt::Long;
 use Ergatis::SavedPipeline;
 use Ergatis::Pipeline;
 use Ergatis::Logger;
 
-my $logger = new Ergatis::Logger('LOG_FILE'=>"/tmp/create_pipeline.log",
-				 'LOG_LEVEL'=>5);
+my %options;
+my $results = GetOptions (\%options, 
+			  'template|s=s',
+			  'repository_root|r=s',
+                          'log=s',
+                          'debug=s', 
+                          'help|h' );
+
+my $logfile = $options{'log'} || Ergatis::Logger::get_default_logfilename();
+my $logger = new Ergatis::Logger('LOG_FILE'=>$logfile,
+				  'LOG_LEVEL'=>$options{'debug'});
 $logger = $logger->get_logger();
-$logger->debug("Test");
+
+if(!-e $options{'template'}){
+    $logger->logdie("Can't read template XML file: $options{'template'}");
+}
+if(!-d $options{'repository_root'}){
+    $logger->logdie("Can't read repository root: $options{'repository_root'}");
+}
+
 my $pipe = Ergatis::SavedPipeline->new( 
-					template => $ARGV[0],
-					pipeline_token => "create_pipeline_test"
+					template => $options{'template'},
+					pipeline_token => "none"
 					);
-my $pipeline = $pipe->write_pipeline( repository_root => $ARGV[1],
+my $pipeline = $pipe->write_pipeline( repository_root => $options{'repository_root'},
 				      id_repository => '/usr/local/devel/ANNOTATION/jorvis/global_id_repository/');
 print "Created pipeline ",$pipeline->path(),"\n";
+print "Invoke with RunWorkflow -i ",$pipeline->path(),"\n";
