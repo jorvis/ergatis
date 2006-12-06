9d3bf842395f9e38c135a23c7132f65cd4aa0b3a
angiuoli <angiuoli@c60d6122-3b2e-0410-912d-af40fea696fe>
2006-12-06 14:33:06 +0000

    *** empty log message ***

---
+++ src/perl/cat_files.pl
@@ -0,0 +1,30 @@
+#!/usr/local/bin/perl
+
+#!/usr/local/bin/perl
+
+use lib (@INC,$ENV{"PERL_MOD_DIR"});
+use strict;
+use Ergatis::Logger;
+use Getopt::Long;
+
+my %options;
+my $results = GetOptions (\%options, 
+			  'list_file|s=s',
+			  #opts for replacing keys in a single template file
+			  'extension|e=s',
+                          'log=s',
+                          'debug=s', 
+                          'help|h' );
+
+my $logfile = $options{'log'} || Ergatis::Logger::get_default_logfilename();
+my $logger = new Ergatis::Logger('LOG_FILE'=>$logfile,
+				  'LOG_LEVEL'=>$options{'debug'});
+$logger = $logger->get_logger();
+
+open FILE,"$options{'list_file'}" or $logger->logdie("Can't open file\n");
+while(my $line=<FILE>){
+    if($line =~ /$options{'extension'}$/){
+	chomp $line;
+	print `cat $line`;
+    }
+}
