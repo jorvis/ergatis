2e0b19c13a306e87fdf448efd22133ff804dab4f
jorvis <jorvis@c60d6122-3b2e-0410-912d-af40fea696fe>
2007-04-09 04:50:31 +0000

    added PERL5LIB modifications

--- install/perl2wrapper_ergatis.pl
+++ install/perl2wrapper_ergatis.pl
@@ -57,6 +57,8 @@ export LC_ALL
 PERL_MOD_DIR=$instdir/lib/5.8.8
 export PERL_MOD_DIR
 
+export PERL5LIB=$instdir/lib/5.8.8:$instdir/lib/perl5/site_perl/5.8.8:$PERL5LIB
+
 exec /usr/local/bin/perl $instdir/bin/$fname $shell_args    
 
 _END_WRAPPER_
