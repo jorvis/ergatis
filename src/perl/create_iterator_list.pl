7e6793a46a47aff33af35187b96e1f10a2a6ac49
jorvis <jorvis@c60d6122-3b2e-0410-912d-af40fea696fe>
2007-12-07 17:22:47 +0000

    changed Workflow::Logger to Ergatis::Logger

--- src/perl/create_iterator_list.pl
+++ src/perl/create_iterator_list.pl
@@ -25,18 +25,9 @@ B<--help,-h> This help message
 
 =cut
 
-#use lib (@INC,$ENV{"PERL_MOD_DIR"});
-no lib "$ENV{PERL_MOD_DIR}/i686-linux";
-no lib ".";
-
 use strict;
 use Getopt::Long qw(:config no_ignore_case no_auto_abbrev pass_through);
-
-
-BEGIN {
-    require '/usr/local/devel/ANNOTATION/cas/lib/site_perl/5.8.5/Workflow/Logger.pm';
-    import Workflow::Logger;
-}
+use Ergatis::Logger;
 use File::Basename;
 
 umask(0000);
@@ -52,10 +43,10 @@ my $results = GetOptions (\%options,
                           'debug=s', 
                           'help|h' ) || pod2usage();
 
-my $logfile = $options{'log'} || Workflow::Logger::get_default_logfilename();
-my $logger = new Workflow::Logger('LOG_FILE'=>$logfile,
+my $logfile = $options{'log'} || Ergatis::Logger::get_default_logfilename();
+my $logger = new Ergatis::Logger('LOG_FILE'=>$logfile,
 				  'LOG_LEVEL'=>$options{'debug'});
-$logger = Workflow::Logger::get_logger();
+$logger = Ergatis::Logger::get_logger();
 
 if (!defined($options{'input_list'})){
     $logger->logdie("input_list was not defined");
