b0613b2efbade8f049b1224e4ee25dde1acdadde
jorvis <jorvis@c60d6122-3b2e-0410-912d-af40fea696fe>
2008-03-17 12:32:54 +0000

    minor: removed INC trickery

--- src/perl/hmmpfam2bsml.pl
+++ src/perl/hmmpfam2bsml.pl
@@ -1,10 +1,5 @@
 #!/usr/bin/perl
 
-BEGIN{foreach (@INC) {s/\/usr\/local\/packages/\/local\/platform/}};
-use lib (@INC,$ENV{"PERL_MOD_DIR"});
-no lib "$ENV{PERL_MOD_DIR}/i686-linux";
-no lib ".";
-
 =head1  NAME 
 
 hmmpfam2bsml.pl - convert hmmpfam raw output to BSML
