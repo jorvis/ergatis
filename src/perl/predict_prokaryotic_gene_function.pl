4db2d4da2f3f4930b9efe3c02f5de5ceb13b687b
jorvis <jorvis@c60d6122-3b2e-0410-912d-af40fea696fe>
2008-07-10 20:42:32 +0000

    added more name processing rules

--- src/perl/predict_prokaryotic_gene_function.pl
+++ src/perl/predict_prokaryotic_gene_function.pl
@@ -412,80 +412,121 @@ sub check_gene_product_names {
     for my $feature (keys %$feats) {
         if ( defined $$feats{$feature}{product} ) {
             
+            my $new_product_name = $$feats{$feature}{product};
+            
             ## names with 'family, family' or 'family family' truncated to just 'family'
             #   ex: AcrB/AcrD/AcrF family family protein -> AcrB/AcrD/AcrF family protein
-            $$feats{$feature}{product} =~ s|family[, ]+family|family|ig;
+            $new_product_name =~ s|family[, ]+family|family|ig;
             
             ## names with 'domain, domain' or 'domain domain' truncated to just 'domain'
-            $$feats{$feature}{product} =~ s|domain[, ]+domain|domain|ig;
+            $new_product_name =~ s|domain[, ]+domain|domain|ig;
             
             ## Names with family and domain should just be domain protein:
             #   ex. PHP domain family protein -> PHP domain protein. 
-            $$feats{$feature}{product} =~ s|domain family protein|domain protein|ig;
+            $new_product_name =~ s|domain family protein|domain protein|ig;
             
             ## if 'putative' and 'family' are in the same name, remove the putative
             #   ex. putative methyltransferase family protein -> methyltransferase family protein
-            if ( $$feats{$feature}{product} =~ m|putative|i && $$feats{$feature}{product} =~ m|family|i ) {
-                $$feats{$feature}{product} =~ s|putative\s*||ig;
+            if ( $new_product_name =~ m|putative|i && $new_product_name =~ m|family|i ) {
+                $new_product_name =~ s|putative\s*||ig;
             }
             
             ## Remove The from the beginning of all names. 
             #   ex. The GLUG motif family protein -> GLUG motif family protein
-            $$feats{$feature}{product} =~ s|^the\s+||ig;
+            $new_product_name =~ s|^the\s+||ig;
             
             ## PAS domain proteins should just be called 'sensory box protein'
-            if ( $$feats{$feature}{product} =~ m|PAS domain|i ) {
-                $$feats{$feature}{product} = 'sensory box protein';
+            if ( $new_product_name =~ m|PAS domain|i ) {
+                $new_product_name = 'sensory box protein';
             }
             
             ## S-box domain proteins should just be called 'sensory box protein'
-            if ( $$feats{$feature}{product} =~ m|S\-box domain protein|i ) {
-                $$feats{$feature}{product} = 'sensory box protein';
+            if ( $new_product_name =~ m|S\-box domain protein|i ) {
+                $new_product_name = 'sensory box protein';
             }
 
             ## if 'response regulator' is anywhere within the name, that should be the entire name
-            if ( $$feats{$feature}{product} =~ m|response regulator|i ) {
-                $$feats{$feature}{product} = 'response regulator';
+            if ( $new_product_name =~ m|response regulator|i ) {
+                $new_product_name = 'response regulator';
             }            
             
             ## any name with conserved hypothetical, DUF family, or protein of unknown function should be 
             #   conserved hypothetical protein 
             #   exs conserved hypothetical family protein; Protein of unknown function (DUF454) family protein -> conserved hypothetical protein
-            if ( $$feats{$feature}{product} =~ m|conserved hypothetical|i || 
-                 $$feats{$feature}{product} =~ m|DUF.*protein| ||
-                 $$feats{$feature}{product} =~ m|protein.*unknown function|i   ) {
-                $$feats{$feature}{product} = 'conserved hypothetical protein';
+            if ( $new_product_name =~ m|conserved hypothetical|i || 
+                 $new_product_name =~ m|DUF.*protein| ||
+                 $new_product_name =~ m|protein.*unknown function|i   ) {
+                $new_product_name = 'conserved hypothetical protein';
             }
             
             ## anything with 'uncharacterized ACR' should be changed to conserved hypothetical
-            if ( $$feats{$feature}{product} =~ m|ncharacterized ACR| ) {
-                $$feats{$feature}{product} = 'conserved hypothetical protein';
+            if ( $new_product_name =~ m|ncharacterized ACR| ) {
+                $new_product_name = 'conserved hypothetical protein';
             }
             
             ## any gene symbol family starting with Y should be a conserved hypothetical
             #   ex. YfiH family COG1496 family protein -> conserved hypothetical protein
-            if ( $$feats{$feature}{product} =~ m|Y\S*[A-Z] family| ) {
-                $$feats{$feature}{product} = 'conserved hypothetical protein';
+            if ( $new_product_name =~ m|Y\S*[A-Z] family| ) {
+                $new_product_name = 'conserved hypothetical protein';
             }
             
             ## names with superfamily and family should just be family
             #   ex. PAP2 superfamily family protein -> PAP2 family protein
-            $$feats{$feature}{product} =~ s|superfamily family|family|ig;
+            $new_product_name =~ s|superfamily family|family|ig;
             
             ## Truncate "family protein" when if follows subunit. 
             #   ex. electron transport complex, RnfABCDGE type, D subunit family protein -> electron transport complex, RnfABCDGE type, D subunit
-            $$feats{$feature}{product} =~ s|subunit family protein|subunit|i;
+            $new_product_name =~ s|subunit family protein|subunit|i;
             
             ## anytime there's a protein something protein, take off the first instance of protein
-            $$feats{$feature}{product} =~ s|protein (\S+ protein)|$1|i;
+            $new_product_name =~ s|protein (\S+ protein)|$1|i;
             
             ## replace 'or' with /. 
             #   ex. succinate dehydrogenase or fumarate reductase, flavoprotein subunit family protein -> succinate dehydrogenase/fumarate reductase, flavoprotein subunit
-            $$feats{$feature}{product} =~ s| or |\/|i;
+            $new_product_name =~ s| or |\/|i;
             
             ## remove leading predicted from names
             #   ex. Predicted permease family protein -> permease family protein
-            $$feats{$feature}{product} =~ s|^predicted\s+||i;
+            $new_product_name =~ s|^predicted\s+||i;
+            
+            ## remove 'precursor' from the protein name
+            #   ex. halocyanin precursor -> halocyanin
+            $new_product_name =~ s|precursor||i;
+            
+            ## rename 'probable' to 'putative'
+            $new_product_name =~ s|probable|putative|gi;
+            
+            ## remove leading whitespace
+            if ( $new_product_name =~ /^\s*(.+)$/ ) {
+                $new_product_name = $1;
+            }
+            
+            ## remove trailing whitespace
+            if ( $new_product_name =~ /^(.+?)\s+$/ ) {
+                $new_product_name = $1;
+            }
+            
+            ## remove trailing periods
+            if ( $new_product_name =~ /^(.+?)\.$/ ) {
+                $new_product_name = $1;
+            }
+            
+            ## remove EC numbers from names. this will catch the following case-insensitive convention:
+            #   ex. Aspartate aminotransferase (EC 2.6.1.1) -> Aspartate aminotransferase
+            $new_product_name =~ s|\(ec .+?\)||i;
+            
+            ## The beginnings of all names should be lowercase unless it is an abbreviation. 
+            #   ex. Alkaline phosphatase family protein -- alkaline phosphatase family protein
+            if ( $new_product_name =~ /^([A-Z])([a-z].+)/ ) {
+                $new_product_name = lc($1) . $2;
+            }
+            
+            ## multiple whitespace should be collapsed
+            $new_product_name =~ s|\s{2,}| |g;
+            
+            _log("INFO: $feature: name before processing: $$feats{$feature}{product}");
+            _log("INFO: $feature: name after processing: $new_product_name");
+            $$feats{$feature}{product} = $new_product_name;
         }
     }
 }
