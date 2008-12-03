2b24ecd71af69245c2fb79e15031119c78accb9e
jorvis <jorvis@c60d6122-3b2e-0410-912d-af40fea696fe>
2008-12-03 20:32:01 +0000

    needs version attribute to produce BSML loadable by bsml2chado

--- src/perl/genbank2bsml.pl
+++ src/perl/genbank2bsml.pl
@@ -1,10 +1,5 @@
 #!/usr/bin/perl
 
-BEGIN{foreach (@INC) {s/\/usr\/local\/packages/\/local\/platform/}};
-use lib (@INC,$ENV{"PERL_MOD_DIR"});
-no lib "$ENV{PERL_MOD_DIR}/i686-linux";
-no lib ".";
-
 use strict;
 use warnings;
 use Getopt::Long qw(:config no_ignore_case no_auto_abbrev);
@@ -46,6 +41,7 @@ $doc->createAndAddAnalysis(
                             sourcename => $options{'output_bsml'},
                             algorithm => 'genbank2bsml',
                             program => 'genbank2bsml',
+                            version => 'current',
                           );
 # gzip output
 $doc->write($ofile, '', 1);
