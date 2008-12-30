e3d18a1f2fed2579d96d7ad9ff8807427b3f5bd2
agussman <agussman@c60d6122-3b2e-0410-912d-af40fea696fe>
2008-12-30 19:52:24 +0000

    Support for gene symbols

--- src/perl/gff3_to_annotab.pl
+++ src/perl/gff3_to_annotab.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 #./gff3_to_annotab.pl --type|t <featuretype> --input_file|i <file> --output_annotab|o <file> [--source|s <source>] [--id_attr <attr_field>]
-# ID will be taken from id_attr if one is specified.  If id_attr is specified and a feature of <featuretype> lacks it, the script will die.
+# seqID will be taken from id_attr if one is specified.  If id_attr is specified and a feature of <featuretype> lacks it, the script will die.
 # featuretype is stored in column 3 of gff3 
 # For example, this is a line with featuretype cds
 # nmpdr|158878.1.contig.NC_002758 NMPDR   cds ...
@@ -47,7 +47,7 @@ while(my $line=<$FIN>){
       if($elts[2] eq 'contig' || $elts[2] eq 'source' || $elts[2] eq 'supercontig') {
 
 	my(%attrs) = field_to_attributes($elts[8]);
-	$attrs{ID} = exists($attrs{ID}) ? $attrs{ID} : $elts[0];
+	$attrs{seqID} = exists($attrs{ID}) ? $attrs{ID} : $elts[0];
 
 #	my(%dbxrefs) = split(/[,:]/,$attrs{'Dbxref'});
 
@@ -60,19 +60,19 @@ while(my $line=<$FIN>){
 	  }
 	}
 	
-	exists($attrs{taxon}) || die "No taxon for $attrs{ID} in $opts{input_file}".Dumper(%attrs);
+	exists($attrs{taxon}) || die "No taxon for $attrs{seqID} in $opts{input_file}".Dumper(%attrs);
 
-	if ( (exists ($contig2taxon{$attrs{ID}})) && ($contig2taxon{$attrs{ID}} != $attrs{taxon})    ) {
+	if ( (exists ($contig2taxon{$attrs{seqID}})) && ($contig2taxon{$attrs{seqID}} != $attrs{taxon})    ) {
 	  # we will assume that this is for phage sequences and not really a problem
 	  #      warn "Redef of taxon for $attrs{ID} ($opts{input_file}) ".$contig2taxon{$attrs{ID}}." != $dbxrefs{taxon}";
-	  warn "Redef of taxon for $attrs{ID} ($opts{input_file}) ".$contig2taxon{$attrs{ID}}." != $attrs{taxon}";
+	  warn "Redef of taxon for $attrs{seqID} ($opts{input_file}) ".$contig2taxon{$attrs{seqID}}." != $attrs{taxon}";
 	}
 	else  {
-	  $contig2taxon{$attrs{ID}} = $attrs{taxon};
+	  $contig2taxon{$attrs{seqID}} = $attrs{taxon};
 	}
 
-#	exists ($contig2taxon{$attrs{ID}}) && die "Redef of taxon for $attrs{ID} ".$contig2taxon{$attrs{ID}};
-#	$contig2taxon{$attrs{ID}} = $dbxrefs{taxon};
+#	exists ($contig2taxon{$attrs{seqID}}) && die "Redef of taxon for $attrs{seqID} ".$contig2taxon{$attrs{seqID}};
+#	$contig2taxon{$attrs{seqID}} = $dbxrefs{taxon};
 
       }
 
@@ -82,13 +82,13 @@ while(my $line=<$FIN>){
 
 	defined( $contig2taxon{$elts[0]} ) || die "No taxon for $elts[0] ($opts{input_file})";
 
-	# retrieve ID from specific attribute field(s) if one was specified
+	# retrieve seqID from specific attribute field(s) if one was specified
 	if ( defined($opts{id_attr}) && !($opts{id_attr} eq '')) {
 	  my $found_id = 0;
 	  my @id_attr = split(",", $opts{id_attr});
 	  foreach my $id_attr (@id_attr) {
 	    if ( defined($attrs{$id_attr}) ) {
-	      $attrs{ID} = $attrs{$id_attr};
+	      $attrs{seqID} = $attrs{$id_attr};
 	      ++$found_id;
 	      last;
 	    }
@@ -98,32 +98,51 @@ while(my $line=<$FIN>){
 	  }
 	}
 	# the below is all largely redundant after comma-delimited method above
-	# assume that if an id_attr was specified, then for consistency's sake all IDs must come from that
+	# assume that if an id_attr was specified, then for consistency's sake all seqIDs must come from that
 	else {
-	# make sure an ID was defined
-	# can't go in field_to_attributes because it doesn't apply to contigs
-	unless ( defined( $attrs{ID} ) ) {
-	  # if there's no ID use locus_id or protein_id
-	  if (defined( $attrs{locus_tag} )) {
-	    $attrs{ID} = $attrs{locus_tag};
+	  # make sure an seqID was defined
+	  # can't go in field_to_attributes because it doesn't apply to contigs
+	  my $found_id = 0;
+	  foreach ('ID', 'locus_tag', 'protein_id') {
+	    if ( defined($attrs{$_}) ) {
+	      $attrs{seqID} = $attrs{$_};
+	      ++$found_id;
+	      last;
+	    }
 	  }
-	  elsif (defined( $attrs{protein_id} )) {
-	    $attrs{ID} = $attrs{protein_id};
-	  }
-	  else {
+	  unless ($found_id) {
+	    die "No specified id_attr $opts{id_attr} for row in file $opts{input_file}\n".Dumper(%attrs);
 	    # manually parse out protein_id
 	    if ($elts[8] =~ q{protein_id=([^;]+)[;$]} ) {
-	       $attrs{ID} = $1;
+	       $attrs{seqID} = $1;
 	    }
 	    else {
-	       die "No protein_id for $attrs{ID} ($opts{input_file})";
+	       die "Unable to populate seqID for line ($line) in $opts{input_file}";
 	       next;
 	    }
 	  }
-	}
+# 	unless ( defined( $attrs{ID} ) ) {
+# 	  # if there's no ID use locus_id or protein_id
+# 	  if (defined( $attrs{locus_tag} )) {
+# 	    $attrs{ID} = $attrs{locus_tag};
+# 	  }
+# 	  elsif (defined( $attrs{protein_id} )) {
+# 	    $attrs{ID} = $attrs{protein_id};
+# 	  }
+# 	  else {
+# 	    # manually parse out protein_id
+# 	    if ($elts[8] =~ q{protein_id=([^;]+)[;$]} ) {
+# 	       $attrs{ID} = $1;
+# 	    }
+# 	    else {
+# 	       die "No id for ($line) in $opts{input_file}";
+# 	       next;
+# 	    }
+# 	  }
+# 	}
         }
 
-        defined($attrs{ID}) || die "Missing 'ID' in attributes ( $opts{input_file} )";
+        defined($attrs{seqID}) || die "Unable to parse seqID from attributes ( $opts{input_file} )";
 
 	# unless a default source was passed in use column 2
 	my $source = (exists $opts{source}) ? $opts{source} : $elts[1];
@@ -134,22 +153,24 @@ while(my $line=<$FIN>){
        
 	# check that a description was provided
 	# if not use product
-	unless ( defined($attrs{description})) {
-	  if (defined( $attrs{product} )) {
-	    $attrs{description} = $attrs{product};
-	  }
-	  else {
-            # this really shouldn't be a die, as we know that not every cds will be annotated
-	    warn "Missing 'description' in attributes ( $opts{input_file} )";
-            $attrs{description} = '';
-	  }
+	if ( !( defined($attrs{description}) ) && (defined($attrs{product})) ) {
+	  $attrs{description} = $attrs{product};
 	}
-	print {$FOUT} join("\t", ($attrs{ID}, $source, $contig2taxon{$contig}, $contig, $attrs{description}));
+
+	if ( defined($attrs{description}) ) {
+	  # remove all unicode characters
+	  $attrs{description} =~ s/%([a-fA-F0-9]{2,2})/chr(hex($1))/eg;
+	}
+	else {
+	  # this really shouldn't be a die, as we know that not every cds will be annotated
+	  warn "Missing 'description' in attributes ( $opts{input_file} )";
+	  $attrs{description} = '';
+	}
+	print {$FOUT} join("\t", ($attrs{seqID}, $source, $contig2taxon{$contig}, $contig, $attrs{description}));
 
 	# optional fields
-
 	if ($opts{parse_GO}) {
-	  print {$FOUT} "\t".parse_GO(%attrs); # null or comma-delimited GO ids
+	  print {$FOUT} "\t".parse_GO($elts[8], %attrs); # null or comma-delimited GO ids
 	}
 
 	if ($opts{parse_EC}) {
@@ -182,30 +203,35 @@ sub parse_EC {
   if ( defined($attrs{EC}) ) {
     return "EC:".$attrs{EC};
   }
+  elsif ( $attrs{description} =~ /EC:[\.\-\d]+/ ) {
+    return join(",", sort ( $attrs{description} =~ /EC:[\.\-\d]+/g ) );
+  }
   elsif ( $field =~ /EC:[\.\-\d]+/ ) {
-    return join(",", ( $field =~ /EC:[\.\-\d]+/g ) );	      
+    return join(",", sort ( $field =~ /EC:[\.\-\d]+/g ) );	      
   }
 
-#   if (defined ($attrs{Dbxref})) {
-#     # assume comma delimited 
-#     # it looks like EC only matches once, but this is a bug if it matches > 1
-#     foreach my $full_dbxref (split(/[,]/,$attrs{'Dbxref'})) {
-#       if ($full_dbxref =~ /^EC:/) {
-# 	return $full_dbxref;
-#       }
-#     }
-#   }
   return 'null';
 }
 
 # Ways GO ids might be stored:
 # pathema:  Ontology_term=GO:0004239,GO:0006464
 sub parse_GO {
+  my $field = shift;
   my %attrs = @_;
 
   if (defined ($attrs{Ontology_term})) {
     return $attrs{Ontology_term};
-  } else {
+  } 
+  elsif (  defined($attrs{GO}) ) {
+    return "GO:".$attrs{GO};
+  }
+  elsif ( $attrs{description} =~ /GO:[\d]+/ ) {
+    return join(",", sort ( $attrs{description} =~ /GO:[\d]+/g ) );
+  }
+  elsif ( $field =~ /GO:[\d]+/ ) {
+    return join(",", sort ( $field =~ /GO:[\d]+/g ) );	      
+  }
+  else {
     return 'null';
   }
 }
@@ -217,7 +243,14 @@ sub parse_gene_symbol {
 
   if (defined ($attrs{gene_symbol})) {
     return $attrs{gene_symbol};
-  } else {
+  } 
+  # : separated portion of the ID, e.g.,
+  # ID=CP000082.1:dnaN:unknown_transcript_1;
+  elsif ( defined($attrs{ID}) && (my @id_parts = split(":", $attrs{ID})) > 1 ) {
+#    die Dumper(%attrs);    
+    return $id_parts[1];
+  }
+  else {
     return 'null';
   }
 }
@@ -268,20 +301,6 @@ sub field_to_attributes {
       }
     }
   }
-  
-#   while ((my $key = shift @split_atts) && (my $value = shift @split_atts)) {
-#     if ( ($key eq 'db_xref') || ($key eq 'Dbxref') ) {
-#       my @dbxrefs = split(/[:,]/,$value);
-#       while ((my $db = shift @dbxrefs) && (my $acc = shift @dbxrefs)) {
-# 	$attrs{$db} = $acc;
-#       }
-#     }
-#     else {
-# #      print "att:$key -> $value\n";
-#       $attrs{$key} = $value;
-#     }
-#   }
-
 
   return %attrs;
 
@@ -293,8 +312,17 @@ sub clean_gff3_line {
   # replace spaces
   $line =~ s/%20/ /g;
 
+  # replace (
+  $line =~ s/%28/\(/g;
+
+  # replace )
+  $line =~ s/%29/\)/g;
+
   # some files have them escaped, but just in the contig row, why??? :(
   $line =~ s/%7C/|/g;
+
+  # can't do this here because it'll decode ; (%3B) and screw up parsing
+#  $line =~ s/%([a-fA-F0-9]{2,2})/chr(hex($1))/eg;
 
   return $line;
 }
