b74f06659da9c401a487a5bab5c8ee0896bca296
jcrabtree <jcrabtree@c60d6122-3b2e-0410-912d-af40fea696fe>
2009-01-16 20:30:44 +0000

    Added --use_feature_ids_in_fasta.  Replaced prints with log statements.  Fixed discrepancy between two cut-and-pasted sections of code by refactoring.  Failure to find a polypeptide sequence is now a fatal error, not a silent one.

--- src/perl/CogProteinFasta.pl
+++ src/perl/CogProteinFasta.pl
@@ -6,11 +6,11 @@ no lib ".";
 
 use strict;
 use BSML::BsmlParserSerialSearch;
-
+use Ergatis::Logger;
 use Getopt::Long qw(:config no_ignore_case no_auto_abbrev);
 
 my %options = ();
-my $results = GetOptions( \%options, 'cogFile|c=s', 'bsmlModelList|m=s', 'outputDir|o=s', 'maxCogSeqCount|s=s', 'extension|e=s');
+my $results = GetOptions( \%options, 'cogFile|c=s', 'bsmlModelList|m=s', 'outputDir|o=s', 'maxCogSeqCount|s=s', 'extension|e=s', 'use_feature_ids_in_fasta=i', 'log|l=s', 'debug=s');
 
 my $cogFile = $options{'cogFile'};
 my $bsmlModelList = $options{'bsmlModelList'};
@@ -19,13 +19,28 @@ my $maxCogSeqCount = $options{'maxCogSeqCount'};
 if($options{'extension'} eq ''){
     $options{'extension'} = 'fsa';
 }
+my $use_feature_ids_in_fasta = defined($options{'use_feature_ids_in_fasta'}) && ($options{'use_feature_ids_in_fasta'} > 0);
 
+## logging
+my $logfile = $options{'log'} || Ergatis::Logger::get_default_logfilename();
+my $logger = new Ergatis::Logger('LOG_FILE'=>$logfile,
+                                  'LOG_LEVEL'=>$options{'debug'});
+$logger = Ergatis::Logger::get_logger();
+
+# mapping from Seq.id and/or Seq-data-import.identifier to polypeptide sequence
 my $Prot = {};
+# mapping from polypeptide Feature.id to Seq.id
+my $Feat = {};
 
-# set up a serial parser to parse sequence elements, bypassing feature tables for 
-# efficiency.
 
-my $seqParser = new BSML::BsmlParserSerialSearch( ReadFeatureTables => 0, SequenceCallBack => \&createPolypeptideLookup);
+my %seqParserArgs = ( ReadFeatureTables => 0, SequenceCallBack => \&createPolypeptideLookup );
+if ($use_feature_ids_in_fasta) {
+    $seqParserArgs{FeatureCallBack} = \&createFeatureLookup;
+    $seqParserArgs{ReadFeatureTables} = 1;
+}
+
+# set up a serial parser to parse sequence elements, bypassing feature tables for efficiency if possible
+my $seqParser = new BSML::BsmlParserSerialSearch(%seqParserArgs);
 
 #Get rid of trailing slashes in directory names
 
@@ -34,141 +49,151 @@ $outDir =~ s/\/+$//;
 
 if( !$bsmlModelList )
 {
-    die "no Bsml Directory specified\n";
+    $logger->logdie("no Bsml Directory specified");
 }
 else
 {
     if( ! -e $bsmlModelList )
     {
-	die "could not open list file: $bsmlModelList\n";
+        $logger->logdie("could not open list file: $bsmlModelList");
     }
 }
 
 if( !$cogFile )
 {
-    die "cog file not specified\n";
+    $logger->logdie("cog file not specified");
 }
 
 if( !$outDir )
 {
-    die "output directory not specified\n";
+    $logger->logdie("output directory not specified");
 }
 else
 {
     if( ! -d $outDir )
     {
-	mkdir( $outDir );
+        mkdir( $outDir );
     }
 }
 
-open FILE, $bsmlModelList or die "Can't open file $bsmlModelList";
+open FILE, $bsmlModelList or $logger->logdie("Can't open file $bsmlModelList");
 while(my $bsmlFile=<FILE>)
 {
     chomp $bsmlFile;
     if(-e $bsmlFile){
-	print STDOUT "Parsing $bsmlFile\n";
-	$seqParser->parse( $bsmlFile );
+        $logger->debug("parsing $bsmlFile");
+        $seqParser->parse( $bsmlFile );
     }
 }
 
-open( INPUTCOGS, "<$cogFile" ) or die "could not open $cogFile.\n";
+open( INPUTCOGS, "<$cogFile" ) or $logger->logdie("could not open $cogFile.");
 
-my $cog = '';
+my $cog = undef;
 my $list = [];
 
 while( my $line = <INPUTCOGS> )
 {
     if( $line =~ /^\t([\S]*)/ )
     {
-	# A new sequence has been found and added to the current cog.
-	print STDERR "Found line $line\n";
-	push( @{$list}, $1 );
+        # A new sequence has been found and added to the current cog.
+        $logger->debug("read line $line");
+        push( @{$list}, $1 );
     }
 
     if( $line =~ /^COG\s+=\s+([^,\s]+)/ )
     {
-	# A new cog has been encountered, flush the previous to disk if present
-	print STDERR "Found cog $line $cog\n";
-	if( $cog){
-	    print STDERR "Outputing cog $cog\n";
-	    if(scalar(@{$list})>1){
+        # A new cog has been encountered, flush the previous to disk if present
+        my $newcog = $1;
+        $logger->debug("read cog $line $newcog");
+        &outputCog($cog, $list) if (defined($cog));
+        $cog = $newcog;
+        $list = [];
+    }
+}
+outputCog($cog,$list) if (defined($cog));
+exit(0);
+
+## subroutines
+
+sub outputCog {
+    my($cog, $list) = @_;
+    $logger->debug("outputting cog $cog");
+    if(scalar(@{$list})>1){
 		if(@{$list} <= $maxCogSeqCount){
-		    open( OUTFILE, ">$outDir/$cog.$$.$options{'extension'}" ) or die "could not open $cog.$options{'extension'}\n";
+		    open( OUTFILE, ">$outDir/$cog.$$.$options{'extension'}" ) or $logger->logdie("could not open $cog.$options{'extension'}");
 		    foreach my $seq ( @{$list} )
 		    {
-			print OUTFILE ">$seq\n";
-			print OUTFILE $Prot->{$seq}."\n";
+                print OUTFILE ">$seq\n";
+                my $residues = undef;
+                if ($use_feature_ids_in_fasta) {
+                    my $seq_id = $Feat->{$seq};
+                    $logger->logdie("no sequence id found for feature with id=$seq") if (!defined($seq_id));
+                    $residues = $Prot->{$seq_id};
+                } else {
+                    $residues = $Prot->{$seq};
+                }
+                $logger->logdie("no sequence data found for seq=$seq") if (!defined($residues));
+                print OUTFILE $residues."\n";
 		    }
 		    close( OUTFILE );
 		}
 		else{
-		    open( OUTFILE, ">$outDir/$cog.$$.$options{'extension'}" ) or die "could not open $cog.$options{'extension'}\n";
+		    open( OUTFILE, ">$outDir/$cog.$$.$options{'extension'}" ) or $logger->logdie("could not open $cog.$options{'extension'}");
 		    foreach my $seq ( @{$list} )
 		    {
-			print OUTFILE ">$seq\n";
-			print OUTFILE "X\n";
+                print OUTFILE ">$seq\n";
+                print OUTFILE "X\n";
 		    }
 		    close( OUTFILE );
 		}
-	    }
-	}
-
-	$cog = $1;
-	$list = [];
-    }
-}
-if( $cog){
-    print STDERR "Outputing cog $cog\n";
-    if(scalar(@{$list})>1){
-	if(@{$list} <= $maxCogSeqCount){
-	    open( OUTFILE, ">$outDir/$cog.$$.$options{'extension'}" ) or die "could not open $cog.$options{'extension'}\n";
-	    foreach my $seq ( @{$list} )
-	    {
-		print OUTFILE ">$seq\n";
-			print OUTFILE $Prot->{$seq}."\n";
-	    }
-	    close( OUTFILE );
-	}
-	else{
-	    open( OUTFILE, ">$outDir/$cog.$$.$options{'extension'}" ) or die "could not open $cog.$options{'extension'}\n";
-	    foreach my $seq ( @{$list} )
-	    {
-		print OUTFILE ">$seq\n";
-		print OUTFILE "A\n";
-	    }
-	    close( OUTFILE );
-	}
     }
 }
 
 sub createPolypeptideLookup
 {
-    my $seqRef = shift;
+    my $seqRef = shift;    
 
     # We're only interested in polypeptide sequences for all-vs-all and pblast
 
     if( ($seqRef->returnattr( 'molecule' ) eq 'aa') || ($seqRef->returnattr('class') eq 'polypeptide'))
     {
-	my $seq = $seqRef->subSequence(-1,0,0);
-	
-	my $identifier;
-	if(defined $seqRef->{'BsmlSeqDataImport'}){
-	    $identifier = $seqRef->{'BsmlSeqDataImport'}->{'identifier'};
-	}
-	else{
-	    $identifier = $seqRef->returnattr( 'id' );
-	}
+        my $seq = $seqRef->subSequence(-1,0,0);
+        
+        my $identifier;
+        if((defined $seqRef->{'BsmlSeqDataImport'}) && (!$use_feature_ids_in_fasta)){
+            # don't think this is right, but trying to maintain reverse compatibility:
+            $identifier = $seqRef->{'BsmlSeqDataImport'}->{'identifier'};
+        }
+        else{
+            $identifier = $seqRef->returnattr( 'id' );
+        }
 
-	if( $identifier && $seq )
-	{
-	    $Prot->{$identifier} = $seq;
-	}
+        if( $identifier && $seq )
+        {
+            $Prot->{$identifier} = $seq;
+        }
     }
 }
 
+sub createFeatureLookup
+{
+    my $featRef = shift;
+    my $class = $featRef->returnattr('class');
+    return unless ($class eq 'polypeptide');
+    my $feat_id = $featRef->returnattr('id');
+    my $links = $featRef->returnBsmlLinkListR();
+    my @seqlinks = grep { $_->{'rel'} eq 'sequence' } @$links;
+    my $nsl = scalar(@seqlinks);
 
-
-
-
-
+    if ($nsl == 1) {
+        my $rel = $seqlinks[0]->{'rel'};
+        my $seq_id = $seqlinks[0]->{'href'};
+        $seq_id =~ s/^\#//;
+        if ($rel eq 'sequence') {
+            $Feat->{$feat_id} = $seq_id;
+        }
+    } elsif ($nsl > 1) {
+        $logger->logdie("feature $feat_id has $nsl sequence Links");
+    }
+}
 
