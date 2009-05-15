596fa22a7b9b53095edae3b1f57e78d9c026d5b0
daveriley <daveriley@c60d6122-3b2e-0410-912d-af40fea696fe>
2009-05-15 19:30:22 +0000

    pangenome_blast_parse: Added support for an organism_to_db_mapping file that will map uniquename prefixes to organisms.

--- src/perl/pangenome_blast_parse.pl
+++ src/perl/pangenome_blast_parse.pl
@@ -61,6 +61,7 @@ my $results = GetOptions (  \%options,
                             'input|i=s',
                             'output_path|o=s',
 #                           'filter|f=s',
+                            'organism_to_db_mapping|odb:s',
                             'db_list|dl:s',
                             'debug|d=s',
                             'command_id=s',       ## passed by workflow
@@ -70,10 +71,10 @@ my $results = GetOptions (  \%options,
                          ) || pod2usage();
 
 my @results = ();
-my @dups_temp = ();
+my $dups_temp = {};
 my %dups = ();
-my $filter;
-my $skip_filter = 1;
+my $db_to_org = {};
+my $db_filter = {};
 my $dbs = {};
 
 my $twig = XML::Twig->new(
@@ -85,22 +86,15 @@ my $twig = XML::Twig->new(
 if($options{'db_list'}) {
     &read_db_list();
 }
+if($options{'organism_to_db_mapping'}) {
+    &read_organism_to_db_mapping();
+}
 
 if (!-d $options{'output_path'}) {
     die "must specify output path as a directory";
 }
 
 $options{'output_path'} =~ s/\/$//;
-my $filter_dir = $options{'output_path'};
-$filter_dir =~ s/\/[^\/]+$//;
-
-if (-e "$filter_dir/pangenome.filter.stored") {
-    print STDERR "unserializing filter\n";
-    unless($filter = retrieve("$filter_dir/pangenome.filter.stored")) {
-        die "failed unserializing seq filter";
-    }
-    $skip_filter = 0;
-}
 
 my $input_prefix = basename($options{'input'},".bsml");
 
@@ -121,10 +115,11 @@ $twig->parse($ifh);
 
 close $ifh;
 
-@dups_temp = sort(@dups_temp);
 
-if (scalar(@dups_temp) > 1) {
-    $dups{join(" ", @dups_temp)} = 1;
+foreach my $key (keys %$dups_temp) {
+    if (scalar(@{$dups_temp->{$key}}) > 1) {
+        $dups{$key} = join(" ", @{$dups_temp->{$key}});
+    }
 }
 
 nstore([\@results,\%dups], $options{'output_path'}."/".$input_prefix.".blast.stored") || die "couldn't serialize results";
@@ -188,17 +183,23 @@ sub processSeqPairAlignment {
 	die "couldn't parse out db and sequence id from subject\n$subject_id\n";
     }
     
+    if(!$options{'db_list'} && !$options{'organism_to_db_mapping'}) {
+        $db_to_org->{$qdb} = $qdb;
+        $db_to_org->{$sdb} = $sdb;
+    }
+    
     if(!$options{'db_list'} || ($dbs->{$qdb} && $dbs->{$sdb})) {
-    if ($skip_filter || ($filter->{$qdb}->{$qprot} && $filter->{$sdb}->{$sprot})) {
+#    if ($skip_filter || ($filter->{$qdb}->{$qprot} && $filter->{$sdb}->{$sprot})) {
+    if ($db_filter || ($db_filter->{$db_to_org->{$qdb}}->{$qprot} && $db_filter->{$db_to_org->{$sdb}}->{$sprot})) {
     #   if ($all_seg_p_ident >= 50.0 && ($p_cov_ref >= 50.0 || $p_cov_comp >= 50.0)) {  ## switched from p_ident to p_sim
     #       push (@results, [$qdb,$qprot,$sdb,$sprot,$all_seg_p_ident,$p_cov_ref]);
         if ($all_seg_p_sim >= 50.0 && ($p_cov_ref >= 50.0 || $p_cov_comp >= 50.0)) {
-            push (@results, [$qdb,$qprot,$sdb,$sprot,$all_seg_p_sim,$p_cov_ref]);
+            push (@results, [$db_to_org->{$qdb},$qprot,$db_to_org->{$sdb},$sprot,$all_seg_p_sim,$p_cov_ref]);
         }
     #   if ($qdb eq $sdb && $qprot ne $sprot && $all_seg_p_indent == 100 && $p_cov_ref == 100 && $p_cov_comp == 100.0) {
         #   I won't exclude the self hit here because we do want the query id in the group of dups
-        if ($qdb eq $sdb && $all_seg_p_ident == 100 && $p_cov_ref == 100 && $p_cov_comp == 100) {
-            push(@dups_temp, $sprot);
+        if ($db_to_org->{$qdb} eq $db_to_org->{$sdb} && $all_seg_p_ident == 100 && $p_cov_ref == 100 && $p_cov_comp == 100) {
+            push(@{$dups_temp->{$db_to_org->{$sdb}}}, $sprot);
             print $sprot."\n";
             print $p_cov_ref." ".$p_cov_comp."\n";
         }
@@ -214,6 +215,20 @@ sub read_db_list {
     while(<IN>) {
         chomp;
         $dbs->{$_} = 1;
+        $db_to_org->{$_} = $_;
     }close IN;
     
+}
+
+sub read_organism_to_db_mapping {
+   open IN, "<".$options{'organism_to_db_mapping'} or die "couldn't open '$options{organism_to_db_mapping}' for reading: $!";
+    while(<IN>) {
+        chomp;
+        $_ =~ /^(.*)\s(\S+)$/;
+        my $org = $1;
+        my $db = $2;
+        $db_filter->{$db} = 1;
+        $db_to_org->{$db} = $org;
+    }close IN;
+  
 }
