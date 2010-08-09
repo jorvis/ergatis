f2cf4594082712df15d1ddefb76494af83d577be
kevingalens <kevingalens@c60d6122-3b2e-0410-912d-af40fea696fe>
2010-08-09 14:36:30 +0000

    will now create the output directory if it doesn't exist

--- src/perl/train_for_glimmer3_iteration.pl
+++ src/perl/train_for_glimmer3_iteration.pl
@@ -130,6 +130,15 @@ sub createPWM {
 
     my $startCodonUsage = &consolidateStartCodonUsages( $files );
 
+    #see if the directory where the PWM is written exists. If not, make it
+    my $dir = $1 if( $outputPWM =~ m|^(.*?)[^/]+$/| );
+    die("Could not parse directory from $outputPWM") unless( defined( $dir ) );
+    unless( -d $dir ) {
+        system('mkdir -p -m 777 $dir');
+        die("Something went wrong with mkdir command. Return value: $?")
+            unless( $? == 0 );
+    }
+
     my $elphOut = $outputPWM;
     my $elphCmd = "$elph $outFile LEN=6 | $glimmerDir/scripts/get-motif-counts.awk > $elphOut";
 
