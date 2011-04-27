0a0ce1bd1c13bea4d207a6a7cacc499e73dc7fb1
vangalamaheshh <vangalamaheshh@c60d6122-3b2e-0410-912d-af40fea696fe>
2011-04-27 13:33:04 +0000

    the read with longest match will be considered

--- src/perl/filter_blastx_output.pl
+++ src/perl/filter_blastx_output.pl
@@ -1,21 +1,33 @@
 #!/usr/bin/perl
+
+eval 'exec /usr/bin/perl  -S $0 ${1+"$@"}'
+    if 0; # not running under some shell
 use strict;
 use warnings;
 use Getopt::Long;
 use Pod::Usage;
 
 my $options = get_options( );
-open( OFH, ">$$options{'output_file'}" ) or die "Error in writing to the file, $$options{'output_file'}, $!\n";
 my $files = get_files( $$options{'blastx_file_list'} );
 my ( $reads_length, $reads_source ) = get_reads_info( $$options{ 'read_length_file' } );
 ## we need a hash to keep track of sub start and sub end for each query
 ## we need to hold on to these values through out all files
 my $filter = {};
+my $data = {};
 foreach my $file ( @$files ) {
 	parse_file( $file, $reads_length, $reads_source, $$options{ 'identity_percentage' }, $$options{ 'read_length_coverage' } );
 }
-close OFH;
+print_output( $data, $$options{ 'output_file' } );
 exit $?;
+
+sub print_output {
+	my( $data, $file ) = @_;
+	open( OFH, ">$file" ) or die "Error in writing to the file, $file, $!\n";
+	while( my( $key, $value ) = each %$data ) {
+		print OFH $key, "\t", join( "\t", @$value ), "\n";
+	}
+	close OFH;
+}
 
 sub parse_file {
 	my ( $file, $reads_length, $reads_source, $identity_cutoff, $coverage_cutoff ) = @_;
@@ -26,32 +38,24 @@ sub parse_file {
 		my ( $query_id, $sub_id, $identity, $coverage, $sub_start, $sub_end ) = ( @values[0 .. 3], @values[8 .. 9] );
 		if ( $identity >= $identity_cutoff && ( $coverage >= $coverage_cutoff * $$reads_length{ $sub_id } / 100 ) ) {
 			unless( exists $$filter{ $query_id } ) {
-				push @{ $$filter{ $query_id } }, $sub_start.", ".$sub_end;
-				print OFH join( "\t", ( $query_id, $sub_id, $identity, $coverage, $$reads_length{ $sub_id }, $sub_start, $sub_end, $$reads_source{ $sub_id } ) ), "\n";
+				$$filter{ $query_id }  = $sub_end - $sub_start;
+				$$data{$query_id} = [ $sub_id, $identity, $coverage, $$reads_length{ $sub_id }, $sub_start, $sub_end, $$reads_source{ $sub_id } ];
 			} elsif( is_valid( $sub_start, $sub_end, $$filter{ $query_id } ) ) {
-					push @{ $$filter{ $query_id } }, $sub_start.", ".$sub_end;
-					print OFH join( "\t", ( $query_id, $sub_id, $identity, $coverage, $$reads_length{ $sub_id }, $sub_start, $sub_end, $$reads_source{ $sub_id } ) ), "\n";
+					$$filter{ $query_id } = $sub_end - $sub_start;
+					$$data{$query_id} = [ $sub_id, $identity, $coverage, $$reads_length{ $sub_id }, $sub_start, $sub_end, $$reads_source{ $sub_id } ];
 			}
 		}
 	}
-	close FH;
 }
 
 sub is_valid {
-	my ( $sub_start, $sub_end, $ref_array ) = @_;
-	foreach my $pair ( @$ref_array ) {
-		my ( $start, $end ) = split( ", ", $pair );
-		if ( ( $sub_start >= $start && $sub_start <= $end ) ||
-		     ( $sub_end >= $start && $sub_end <= $end ) || ( $sub_start <= $start && $sub_end >= $end ) ) {
-			return 0;
-		}
-	}
-	return 1;
+	my ( $sub_start, $sub_end, $length ) = @_;
+	return (( $sub_end - $sub_start ) > $length ) ? 1 : 0;
 }
 
 
 sub get_options {
-	my %options;
+	my ( %options );
 	GetOptions( \%options, 'read_length_file|r=s', 'blastx_file_list|b=s', 'output_file|o=s',
 			'identity_percentage|i=i', 'read_length_coverage|c=i', 'help|h' ) || pod2usage( );
 	if( $options{'help'} ) {
