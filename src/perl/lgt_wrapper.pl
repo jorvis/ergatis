f460ce39a4fbdd6fae6631d921cdb709d78566a1
daveriley <daveriley@c60d6122-3b2e-0410-912d-af40fea696fe>
2011-10-21 23:12:18 +0000

    Adding the lgt_wrapper.pl script.

---
+++ src/perl/lgt_wrapper.pl
@@ -0,0 +1,423 @@
+#!/usr/bin/perl
+
+use strict;
+use Getopt::Long qw(:config no_ignore_case no_auto_abbrev);
+use Pod::Usage;
+use File::Basename;
+use JSON;
+
+my %options = ();
+my $results = GetOptions (\%options,
+              'input_list|i=s',
+              'tags_to_upload|ttu=s',
+              'output_directory|o=s',
+              'data_output_directory|do=s',
+              'credential|c=s',
+              'user|u=s',
+              'key|k=s',
+              'host|s=s',
+              'decrypt_script|d=s',
+              'skip_transfer|st=s',
+              'child_config|cc=s',
+              'child_config_params|ccp=s',
+              'pipeline_parent|pp=s',
+              'num_retries|nr=s',
+              'input_param_name|ipn=s',
+              'wait_time|wt=s',
+              'tags_to_download|ttd=s',
+              'max_pipelines|mp=s',
+              'help|h');
+
+my $pipe_tasks = [];
+my $input_lists = undef;
+my $finished = 0;
+my @tags_to_upload = split(/\,/, $options{tags_to_upload});
+$options{output_directory} =~ s/\/$//;
+my $task_file = $options{output_directory}."/pipeline_tasks.txt";
+my $wait = $options{wait_time} ? $options{wait_time} : 100;
+my $max_pipes = $options{max_pipelines} ? $options{max_pipelines} : 50;
+my $resize_interval = 3600;
+my $time_since_resize = $resize_interval;
+
+my $num_running = 0;
+
+while(!$finished) {
+
+    # Read in the pipeline tasks if we haven't already
+    if(-e $task_file && !(@$pipe_tasks)) {
+        print "LOG: Loading task file\n";
+        $pipe_tasks = &load_state($task_file);
+    }
+    
+    # Load the input list of lists
+    if(not defined $input_lists) {
+        print "LOG: Loading Input Lists\n";
+        &load_input_lists();
+    }
+    
+    # Check if any tasks have finished and download the data if they have
+    $finished = &check_tasks($pipe_tasks);
+    
+    # Submit the next task
+    if(@$input_lists) {
+        &submit_pipeline(shift(@$input_lists));
+    }
+    
+    # Check that all clusters are the right size (avoids losing all execs)
+    $time_since_resize -= $wait;
+    if($time_since_resize <= 0) {
+        &resize_clusters();
+        $time_since_resize = $resize_interval;
+    }
+    
+    print "LOG: Finished with an iteration, waiting $wait seconds\n";
+    sleep $wait;
+}
+
+print "LOG: Finished!\n";
+
+exit(0);
+
+sub submit_pipeline {
+    my $input = shift;
+    my $task = shift;
+    my $fname = fileparse($input,qr/\.[^.]*$/);
+    
+    if(!$task && $num_running >= $max_pipes) {
+        return;
+    }
+    
+    # Create an output directory for the temp files for this subgroup
+    my $output_dir = "$options{output_directory}/$fname";
+    if(! -d $output_dir) {
+        my $cmd = "mkdir $output_dir";
+       `$cmd`;
+        if($?) {
+            die "FAILURE: Failed running:\n$cmd\n$?";
+        }
+    }
+    else {
+        print "WARNING: output directory $output_dir already exists\n";
+    }
+    
+    # Start new cluster
+    my $cmd = "vp-start-cluster --cluster=$fname\_cluster --num-exec=0 --cred=$options{credential} -t";
+   `$cmd`;
+    if($?) {
+       die "FAILURE: Failed running:\n$cmd\n$?";
+    }
+    
+    # Copy files and decrypt files (if necessary)
+    my $localfl = "$output_dir/$fname.list";
+    print "LOG: Local file list will be $localfl\n";
+    &copy_files($input,$localfl);
+    
+    # Create tag
+    print "LOG: Tagging $fname\n";
+    &create_tag($fname,$localfl);
+    
+    # Check cluster status, wait till it's up to continue
+    print "LOG: Waiting for cluster $fname\_cluster to come up\n";
+    my $cmd = "vp-start-cluster --cluster=$fname\_cluster --num-exec=0 --cred=$options{credential}";
+   `$cmd`;
+    if($?) {
+       die "FAILURE: Failed running:\n$cmd\n$?";
+    }
+    print "LOG: Cluster $fname\_cluster came up\n";
+    
+    # Add instances
+    my $num_lines = `wc -l $localfl`;
+    my $num_execs = int($num_lines/2+1);
+    print "LOG: Resizing cluster $fname\_cluster with $num_execs execs\n";
+    my $cmd = "vp-run-metrics -c cluster.CLUSTER_NAME=$fname\_cluster -c pipeline.EXEC_WANT_INSTANCES=$num_execs resize-cluster";
+   `$cmd`;
+    if($?) {
+       die "FAILURE: Failed running:\n$cmd\n$?";
+    }
+    # Upload tags
+    print "LOG: Uploading several tags to $fname\_cluster\n";
+    &upload_tags([$fname,@tags_to_upload],"$fname\_cluster");
+    # Write configuration
+    
+    print "LOG: Writing config options to $output_dir/$fname\_pipeline.config\n";
+    my $ipn = $options{input_param_name} ? $options{input_param_name} : 'input.INPUT_TAG';
+    my $cmd = "vp-describe-protocols --config-from-protocol=$options{child_config} -c $ipn=$fname -c pipeline.PIPELINE_WRAPPER_NAME=$fname $options{child_config_params} > $output_dir/$fname\_pipeline.config";
+    `$cmd`;
+    if($?) {
+       die "FAILURE: Failed running:\n$cmd\n$?";
+    }
+    # Run Pipeline
+    print "LOG: Running pipeline with $output_dir/$fname\_pipeline.config on $fname\_cluster\n";
+
+    my $cmd = "vp-run-pipeline --pipeline-config=$output_dir/$fname\_pipeline.config --bare --cluster=$fname\_cluster -t --overwrite";
+    if($options{pipeline_parent}) {
+        $cmd .= " --pipeline-parent=$options{pipeline_parent}";
+    }
+    my $tid = `$cmd`;
+    chomp $tid;
+    if($?) {
+       die "FAILURE: Failed running:\n$cmd\n$?";
+    }    
+
+    my $downloads = [];
+    map{
+        push(@$downloads,"$fname\_$_");
+    } split(/,/,$options{tags_to_download});
+    my $newtask = {
+            input_list => $input,
+            task_id => $tid,
+            completed => 0,
+            num_retries => 2,
+            cluster => "$fname\_cluster",
+            download_tags => $downloads,
+            num_execs => $num_execs
+        };
+    # If a task was passed in then we are resubmitting    
+    if(!$task) {
+        # Create pipeline task entry and write out pipeline tasks
+        my $nr = $options{num_retries} ? $options{num_retries} : 2;
+        push(@$pipe_tasks,$newtask);
+    }
+    else {
+        $newtask->{num_retries} = $task->{num_retries};
+        $task = $newtask;
+    }
+    &dump_tasks();    
+}
+
+sub load_input_lists {
+    my $submitted_lists = {};
+    map {$submitted_lists->{$_->{input_list}} = 1}@$pipe_tasks;
+    $input_lists = [];
+    open IN, "<$options{input_list}" or die "Unable to open $options{input_list}\n";
+    while(<IN>) {
+        chomp;
+        if(!$submitted_lists->{$_}) {
+            print "LOG: Need to process $_\n";
+            push(@$input_lists, $_);
+        }
+    }
+}
+
+sub resize_clusters {
+
+    print "LOG: Checking Cluster sizes\n";
+    foreach my $task (@{$pipe_tasks}) {
+        if(!$task->{completed}) {
+            my $cmd = "vp-run-metrics -c cluster.CLUSTER_NAME=$task->{cluster} -c pipeline.EXEC_WANT_INSTANCES=$task->{num_execs} resize-cluster";
+           `$cmd`;
+            if($?) {
+               print "FAILURE: Failed running:\n$cmd\n$?";
+            }
+        }
+    }
+}
+
+sub load_state {
+    my $file = shift;
+    
+    open IN, "<$file" or die "Unable to open $file\n";
+    my $text = <IN>;
+    close IN;
+    return decode_json($text);
+}
+
+sub check_tasks {
+    my $tasks = shift;
+    my $num_complete = 0;
+    my $finished = 0;
+    foreach my $task (@$tasks) {
+    
+        # If we have completed a task, harvest the data
+        # and update the task list. If the task failed then
+        # mark it complete so we can finish
+        if(!$task->{completed}) {
+            my $complete = &check_task_complete($task);
+            
+            if($complete) {
+                if($complete ne 'failed') {
+                    print "$task->{task_id} complete on cluster $task->{cluster}\n";
+                    print "Harvesting data from $task->{cluster}\n";
+                    &harvest_data($task);
+                }
+                $task->{completed} = 1;
+                &dump_tasks();
+            }
+        }
+        if($task->{completed}) {
+            $num_complete++;
+        }
+    }
+    if($num_complete == @$tasks && scalar @$input_lists ==0) {
+        $finished = 1;
+    }
+    return $finished;
+}
+
+sub dump_tasks {
+    open OUT, ">$task_file" or die "Unable to open $task_file\n";
+    print OUT to_json($pipe_tasks);
+    close OUT;
+}
+
+sub terminate_cluster {
+    my $cluster = shift;
+    
+    print "LOG: Terminating cluster $cluster\n";
+    my $cmd = "vp-terminate-cluster --cluster=$cluster";
+    my $ret = `$cmd`;
+    if($?) {
+        print "WARNING: Failed to terminate cluster $cluster, will move on anyway\n$?";
+    }
+}
+
+sub check_task_complete {
+    my $task = shift;
+    my $tid = $task->{task_id};
+    my $cluster = $task->{cluster};
+    my $complete = 0;
+    print "LOG: Checking status of task $tid running on $cluster\n";
+    my $cmd = "vp-describe-task $tid --name=$cluster";
+    my $ret = `$cmd`;
+    if($?) {
+        print "FAILURE: Failed to get info about task $tid on $cluster:\n$cmd\n$?";
+        if($task->{num_retries} > 0) {
+            $task->{num_retries}--;
+            &terminate_cluster($task->{cluster});
+            print "LOG: Retrying $tid on $cluster $task->{num_retries} remain\n";
+            &submit_pipeline($task->{input_list},$task);
+        }
+        else {
+            $complete = 'failed';
+        }
+    }
+
+    
+    if($ret =~ /State: complete/) {
+        $complete = 1;
+    }
+    elsif($ret =~ /State: failed/) {
+        if($task->{num_retries} > 0) {
+            $task->{num_retries}--;
+            print "LOG: Retrying $tid on $cluster $task->{num_retries} remain\n";
+            &submit_pipeline($task->{input_list},$task);
+        }
+        else {
+            $complete = 'failed';
+        }
+    }
+    return $complete;
+}
+
+sub harvest_data {
+    my $task = shift;
+    foreach my $tag (@{$task->{download_tags}}) {
+        # First step is transfering the dataset back to the local node.
+        print "LOG: transfering $tag from $task->{cluster}\n";
+        my $cmd = "vp-transfer-dataset --src-cluster=$task->{cluster} --dst-cluster=local --cluster=local --block --tag-name=$tag";
+        `$cmd`;
+        if($?) {
+            die "FAILURE: Failed running:\n$cmd\n$?";
+        }
+        my @files = `vp-describe-dataset --tag-name=$tag|grep FILE|cut -f2`;
+        foreach my $file (@files) {
+            chomp $file;
+            print "LOG: Transfering file $file to $options{user}\@$options{host}:$options{data_output_directory}/$task->{cluster}/\n";
+            &transfer_file({
+                src => $file,
+                dst => "$options{user}\@$options{host}:$options{data_output_directory}/$tag/"
+            });
+        }
+    }
+}
+
+sub transfer_file {
+    my $config = shift;
+
+    my $cmd = "rsync -rlptD -e \"ssh -o PasswordAuthentication=no -o ConnectTimeout=30 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -o UserKnownHostsFile=/dev/null -i $options{key}\" $config->{src} $config->{dst}";
+
+    print "LOG: Running:\n$cmd\n";
+    `$cmd`;
+
+    if($?) {
+       die "FAILURE: Failed running:\n$cmd\n$?";
+    }
+
+}
+
+sub copy_files {
+    my $file_list = shift;
+    my $output_list = shift;
+    if(! -d "$options{output_directory}/decrypt") {
+        print "LOG: Making temporary decrypt directory in $options{output_directory}\n";
+        `mkdir $options{output_directory}/decrypt`;
+    }
+    open IN, "<$file_list" or die "unable to open list\n";
+    open OUT, ">$output_list" or die "unable to open output list\n";
+    while(<IN>) {
+        chomp;
+        if(!$options{skip_transfer}) {
+            print "Copying $_\n";
+            &transfer_file({
+                src => "$options{user}\@$options{host}:$_",
+                dst => "$options{output_directory}",
+                key => $options{key}
+            });
+        }
+        
+        my $fname = basename($_);
+        my $decrypt_fname = $fname;
+
+        if($options{decrypt_script}) {
+            print "Decrypting $options{output_directory}/$fname\n";
+            my $cmd = "$options{decrypt_script} $options{output_directory}/$fname -out-dir $options{output_directory}/decrypt -remove-encrypted";
+            print "Running:\n $cmd\n";
+            my $res = `$cmd`; 
+            print $res;
+            if($res !~ /File validation is skipped/ && $?) {
+                die "$?\n";
+            }
+            my $dfname = `ls $options{output_directory}/decrypt/`;
+        	chomp $dfname;
+            my $cmd = "mv $options{output_directory}/decrypt/$dfname $options{output_directory}";
+            print "Running $cmd\n";
+            my $res = `$cmd`;
+            if( $?) {
+                die "$?\n";
+            }
+            $decrypt_fname = basename $dfname;
+        }
+        elsif(! -e "$options{output_directory}/$decrypt_fname") {
+            my $fname = basename($decrypt_fname,'.ncbi_enc');
+            $decrypt_fname = $fname;
+        }
+        $options{output_dir} =~ s/\/$//;
+        print OUT "$options{output_directory}/$decrypt_fname\n";
+    }
+    close OUT;
+}
+
+sub create_tag {
+    my $tag_name = shift;
+    my $filelist = shift;
+    
+    print "LOG: Tagging $filelist as $tag_name\n";
+    my $cmd = "cat $filelist | vp-add-dataset --tag-name=$tag_name -o --stdin";
+    `$cmd`;
+    if( $?) {
+       die "FAILURE: Failed running:\n$cmd\n$?";
+    }
+}
+
+sub upload_tags {
+    my $tags = shift;
+    my $dest = shift;
+    foreach my $tag (@$tags) {
+        print "LOG: Transfering $tag to $dest\n";
+        my $cmd = "vp-transfer-dataset --tag-name=$tag --dst-cluster=$dest --block --expand";
+        `$cmd`;
+        if( $?) {
+            die "FAILURE: Failed running:\n$cmd\n$?";
+        }        
+    }
+}
