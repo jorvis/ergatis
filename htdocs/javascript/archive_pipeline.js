9ad7c1951f03cb1bbfadbe41cd983bf3ea0008f2
jorvis <jorvis@c60d6122-3b2e-0410-912d-af40fea696fe>
2012-04-09 20:58:19 +0000

    fixed bug when deleting a pipeline that would cause a redirect loop

--- htdocs/javascript/archive_pipeline.js
+++ htdocs/javascript/archive_pipeline.js
@@ -4,13 +4,14 @@ $(document).ready(function() {
     // Our pipeline list will be stored in a hidden input
     var pipelineListURL = $("#pipeline_list_input").val();
 
-    window.setInterval(function() {
+    var wintimer = window.setInterval(function() {
 	    var timeCounter = $("b[id=redirect_count]").html();
 	    var updateTime = eval(timeCounter)- eval(1);
 	    $("b[id=redirect_count]").html(updateTime);
 
 	    if(updateTime <= 0){
-		window.location = (pipelineListURL);
+            window.clearInterval(wintimer);
+            window.location = pipelineListURL;
 	    }
     }, 1000);
 });
