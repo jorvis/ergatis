3604cacc239a646ca269852d2a85d09bac312f1d
kshefchek <kshefchek@c60d6122-3b2e-0410-912d-af40fea696fe>
2012-05-30 14:26:55 +0000

    Adding new component kSNP

---
+++ src/perl/ksnp_merge_reference.pl
@@ -0,0 +1,178 @@
+#!/usr/bin/perl
+
+=head1 NAME
+
+ksnp_merge_reference.pl 
+
+B<--input,-in,-i>
+    single fasta file or list of fasta files
+
+=head1  DESCRIPTION
+
+Merge multiple reference genomes into one multifasta file for kSNP component.  Add "NN" if reference is made up of contigs/scaffolds
+
+=head1  INPUT
+
+fsa or list of fasta files
+
+=head1  OUTPUT
+
+A multi-fasta file that will be used as a reference genome in kSNP
+    
+=head1  CONTACT
+
+    Kent Shefchek
+    kshefchek@som.umaryland.edu
+
+=cut
+
+use strict;
+use File::Temp;
+use Getopt::Long qw(:config no_ignore_case no_auto_abbrev pass_through);
+
+my $input;
+my %options;
+my $bool;
+my $bool2;
+
+my $results = GetOptions(\%options,
+			"input|in|i=s" => \$input
+						 );
+
+die "Must enter input file" if $input eq undef;
+
+open (my $fh, "<$input") || die "Cannot open input file";
+
+if ($input =~ /.*\.[fnsa]{2,3}/) #Single fasta file
+{
+
+	my @list = <$fh>;
+
+	my $tmp = File::Temp->new(TEMPLATE => "tempXXXX",
+			          DIR => "./",
+			          SUFFIX => ".fsa"
+			          );
+
+	open (my $fh3, ">$tmp") || die "Cannot open temp file";	
+
+	$bool = "true";
+	foreach my $a (@list) #Iterate and join contigs if needed
+		{
+
+
+			if (($a =~ /^>/)&&($bool eq 'true'))
+			{
+				print $fh3 "$a";
+				$bool = 'false';
+			}
+		
+			elsif (($a =~ /^>/)&&($bool eq 'false'))
+			{
+				print $fh3 "NN";
+				$bool2 = 'true';
+			}
+			else
+			{
+				$a =~ s/\n//;
+				print $fh3 $a;
+			}
+		}
+		print $fh3 "\n";
+		close $fh3;
+		#Now print with 60 bases per line
+
+		open (my $fh4, "<$tmp") || die "Cannot open temp file";	
+
+		$input =~ s/\///; 	
+		$input =~ s/\w+\///g;
+		$input =~ s/\..+//g;
+		$input =~ s/.{60}/$1 /g;   #kSNP will not work if a header contains 60 straight characters after the >
+
+		while (<$fh4>)
+		{
+			$_ =~ s/^>.*/>$input merged/ if ($bool2 eq 'true');
+			$_ =~ s/(\w{60})/$&\n/g;
+			$_ =~ s/^\s+$//g;
+			print $_;
+		}
+
+
+}
+
+elsif ($input =~ /.*\.list/)
+{
+
+	my @list = <$fh>;
+
+	foreach my $i (@list)
+        {
+
+		my $tmp = File::Temp->new(TEMPLATE => "tempXXXX",
+			          DIR => "./",
+			          SUFFIX => ".fsa"
+			          );
+
+		open (my $fh3, ">$tmp") || die "Cannot open temp file";		
+	
+		$i =~ s/\n//;
+
+		open (my $fh2, "<$i") || die "Cannot open input in list file";
+
+		my @fasta = <$fh2>;
+
+		$bool = "true";
+		foreach my $a (@fasta) #Iterate and join contigs if needed
+		{
+
+
+			if (($a =~ /^>/)&&($bool eq 'true'))
+			{
+				print $fh3 "$a";
+				$bool = 'false';
+			}
+		
+			elsif (($a =~ /^>/)&&($bool eq 'false'))
+			{
+				print $fh3 "NN";
+				$bool2 = 'true';
+			}
+			else
+			{
+				$a =~ s/\n//;
+				print $fh3 $a;
+			}
+		}
+		print $fh3 "\n";
+		close $fh3;
+		#Now print with 60 bases per line
+
+		open (my $fh4, "<$tmp") || die "Cannot open temp file";	
+
+		$i =~ s/\///;
+		$i =~ s/\w+\///g; 
+		$i =~ s/\..+//g;
+		$i =~ s/.{60}/$1 /g; #kSNP will not work if a header contains 60 straight characters after the >
+			
+		while (<$fh4>)
+		{
+			$_ =~ s/^>.*/>$i merged/ if ($bool2 eq 'true');
+			$_ =~ s/(\w{60})/$&\n/g;
+			$_ =~ s/^\s+$//g;
+			print $_;
+		}
+
+	}
+
+}
+
+else {die "incorrect file format";}
+
+
+
+
+
+
+
+
+
+
