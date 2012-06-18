c9b45e8785c5c1bdd79dd7e82ae2acc1a59e8d1c
kevingalens <kevingalens@c60d6122-3b2e-0410-912d-af40fea696fe>
2012-06-18 13:39:42 +0000

    Cleaned up code a little bit (closing filehandles, updated perldoc, removed extraneous print lines, removed commented out debug statements, refactored small sections)

--- src/perl/fetch_genbank.pl
+++ src/perl/fetch_genbank.pl
@@ -1,16 +1,5 @@
 #!/usr/bin/perl 
 
-eval 'exec /usr/bin/perl  -S $0 ${1+"$@"}'
-    if 0; # not running under some shell
-
-eval 'exec /usr/bin/perl  -S $0 ${1+"$@"}'
-if 0; # not running under some shell
-
-BEGIN{foreach (@INC) {s/\/usr\/local\/packages/\/local\/platform/}};
-use lib (@INC,$ENV{"PERL_MOD_DIR"});
-no lib "$ENV{PERL_MOD_DIR}/i686-linux";
-no lib ".";
-
 =head1 NAME
 
 fetch_genbank.pl - Fetches data from GenBank for the provided accession numbers list 
@@ -18,26 +7,23 @@ fetch_genbank.pl - Fetches data from GenBank for the provided accession numbers 
 =head1 SYNOPSIS
 
 USAGE: fetch_genbank.pl 
---database=name of the database to be searched
---query=comma-separated accession number list
---format=output format
---output_dir=/path/to/somedir	
+    --database=name of the database to be searched
+    --query=comma-separated accession number list
+    --format=output format
+    --output_dir=/path/to/somedir	
 =head1 OPTIONS
 
 B<--database,-d>
 The name of the database to be searched 
 
 B<--output_dir,-o>
-The directory to which the output file will be written.
+The directory to which the output files will be written.
 
 B<--query,-q>
 The comma-separated list of accession numbers from the genBank to be fetched.
 
 B<--format,-f>
-The format in which output is required like FASTA
-
-B<--output_file,-s>
-The name of the output file containing the searched, reference sequence
+The format in which output is required. Examples fasta, gb, asn. If format given is not recognized, eutils will return asn.
 
 B<--debug,-d> 
 Debug level.  Use a large number to turn on verbose debugging. 
@@ -52,8 +38,6 @@ This help message
 
 This script is used to fetch a text query given a UID in a given database. Its mainly used for downloading reference genomes in FASTA format from genBank
 given genBank accesion ids. 
-
-=head1  INPUT
 
 =head1  CONTACT
 
@@ -70,9 +54,7 @@ use LWP::Simple;
 use Getopt::Long qw(:config no_ignore_case no_auto_abbrev pass_through);
 use Pod::Usage;
 use POSIX;
-BEGIN {
-	use Ergatis::Logger;
-}
+use Ergatis::Logger;
 
 
 my %options = ();
@@ -99,57 +81,53 @@ if( $options{'help'} ){
 
 ## make sure everything passed was correct
 &check_parameters(\%options);
-my $report = $options{format};
-my $db = $options{database};
-my @ids = split(/,/,$options{query}); 
-foreach my $id (@ids) {
+
+foreach my $id ( split(/,/, $options{query}) ) {
 	chomp($id);
 	next if ($id =~ /\/|\\/g);
-## open the output file for writing 
-	my $fh;
-	my $filename = $options{output_dir}."/reference_".$id.".".$options{format}; 
 
-	open($fh, ">$filename") || $logger->logdie("Could not open $filename file for writing");
+	## open the output file for writing 
+	my $filename = $options{'output_dir'}."/".$id.".".$options{'format'}; 
 
-# ---------------------------------------------------------------------------
-# This part of the code is taken from efetch.pl from the NCBI Efetch utilities
-# $esearch contains the PATH & parameters for the ESearch call
-# $esearch_result containts the result of the ESearch call
-# the results are displayed Ánd parsed into variables 
-# $Count, $QueryKey, and $WebEnv for later use
+	open(my $fh, ">$filename") || $logger->logdie("Could not open $filename file for writing");
+
+	# ---------------------------------------------------------------------------
+	# This part of the code is taken from efetch.pl from the NCBI Efetch utilities
+	# $esearch contains the PATH & parameters for the ESearch call
+	# $esearch_result containts the result of the ESearch call
+	# the results are displayed Ánd parsed into variables 
+	# $Count, $QueryKey, and $WebEnv for later use
 
 	my $esearch = "$utils/esearch.fcgi?" .
-		"db=$db&retmax=1&usehistory=y&term=";
+		"db=$options{'database'}&retmax=1&usehistory=y&term=";
 
 	my $esearch_result = get($esearch . $id);
 
-	print "\nESEARCH RESULT: $esearch_result\n";
-
-	$esearch_result =~ 
-#		m|<Count>(\d+)</Count>.*<QueryKey>(\d+)</QueryKey>.*<WebEnv>(\S+)</WebEnv>|s;
-		m|<Count>(\d+)</Count>.*<Id>(\d+)</Id>|s;
-	my $Count    = $1;
-	my $Id = $2;
-#	my $QueryKey = $2;
-#	my $WebEnv   = $3;
-
-#	print "Count = $Count; QueryKey = $QueryKey; WebEnv = $WebEnv\n";
-#	print "Count = $Count; Id = $Id\n";
-	my $retstart;
+	my ($Count, $Id);
 	my $retmax=3;
-	for($retstart = 0; $retstart < $Count; $retstart += $retmax) {
-		my $efetch = "$utils/efetch.fcgi?" .
-			"rettype=$report&retmode=text&retstart=$retstart&retmax=$retmax&" .
-#			"db=$db&query_key=$QueryKey&WebEnv=$WebEnv";
-			"db=$db&id=$Id";
-#		print "\nEF_QUERY=$efetch\n";     
-
-		my $efetch_result = get($efetch);
-
-		sleep 2;
-
-		print $fh $efetch_result;
+	if( $esearch_result =~ m|<Count>(\d+)</Count>.*<Id>(\d+)</Id>|s ) {
+	    $Count = $1;
+	    $Id = $2;
+	} else {
+	    die("Had issues getting esearch_result");
 	}
+
+	for(my $retstart = 0; $retstart < $Count; $retstart += $retmax) {
+	    if( $retstart > 0 ) {
+		# sleep in between consecutive requests
+		# but not the first time
+		sleep 2;
+	    }
+
+	    my $efetch = "$utils/efetch.fcgi?" .
+		"rettype=$options{'format'}&retmode=text&retstart=$retstart&retmax=$retmax&" .
+		"db=$options{'database'}&id=$Id";  
+
+	    my $efetch_result = get($efetch);
+	    print $fh $efetch_result;
+	}
+
+	close($fh);
 
 }
 # -------------------------------------------------------------------------------
@@ -158,14 +136,14 @@ foreach my $id (@ids) {
 sub check_parameters {
 	my $options = shift;
 
-## make sure output_dir, database, query and format were passed
-	unless ( $options{database} && $options{output_dir} && $options{format} && $options{query}) {
-		$logger->logdie("All the manadatory parameters should be passed");
+	## make sure output_dir, database, query and format were passed
+	my @missing;
+	foreach my $req( qw(database output_dir format query) ) {
+	    push(@missing, $req) unless( exists( $options->{$req) ) );
 	}
+	$logger->logdie("Missing mandatory parameters: ".join(", ", @missing)) unless( @missing == 0 );
 
-## make sure the output_dir exists
-	if (! -e "$options{output_dir}") {
-		$logger->logdie("The output directory passed could not be read or does not exist");
-	}
+	## make sure the output_dir exists, otherwise make it.
+	system("mkdir -p $options{'output_dir'}") unless( -d $options{'output_dir'} );
 }
 
