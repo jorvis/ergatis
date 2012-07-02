b3940ad3c9da459ad3b0b081a6784263e1c947a3
kevingalens <kevingalens@c60d6122-3b2e-0410-912d-af40fea696fe>
2012-07-02 18:19:22 +0000

    Create map file can now take a comma separated list of files from the command line

--- src/perl/create_map_file.pl
+++ src/perl/create_map_file.pl
@@ -8,52 +8,58 @@ my $file = "";
 my $outputFile = "";
 
 GetOptions('input_file_list=s' => \$files,
-			'input_file=s' => \$file,
-			'output_file=s' => \$outputFile);
-unless($outputFile) {
-	die "Must provide an output file\n";
+	   'input_file=s' => \$file,
+	   'output_file=s' => \$outputFile);
+
+die("Must provide an output file") unless($outputFile);
+
+
+my @files;
+if( $files ) {
+    foreach my $list ( split(/[,\s]+/, $files) ) {
+	open(IN, "< $list") or die("Could not open list file $list: $!");
+	chomp( my @tmp = <IN> );
+	close(IN);
+	push( @files, @tmp );
+    }
 }
-open(OFH,">$outputFile") or die "Error in writing to the output file, $outputFile, $!\n";
-if($files) {
-	open(TAG,"<$files") or die "Error in opening the file, $files, $!\n";
-	while(my $filePath = <TAG>) {
-		chomp $filePath;
-		if(trim($filePath) =~ /(\S+)/) {
-			printMapInfo($1);
-		}
-	}
-	close TAG;	
-} elsif($file) {
-	printMapInfo(trim($file));
-} else {
-	die "Usage: create_pangenome_map_file.pl --input_file_list or --input_file and --output_file\n";
+
+if( $file ) {
+    my @tmp = split(/[,\s]+/, $file);
+    push(@files, @tmp);
 }
-close OFH;
-exit(0);
+
+die("Either --input_file_list or --input_file is required") unless( @files );
+
+open(my $OFH,">$outputFile") or die "Error in writing to the output file, $outputFile, $!\n";
+foreach my $f ( @files ) {
+    printMapInfo(trim($f), $OFH);
+}
+close $OFH;
 
 sub trim {
-	my ($param) = @_;
-	$param =~ s/^\s+//;
-	$param =~ s/\s+$//;
-	return $param;
+    my ($param) = @_;
+    $param =~ s/^\s+//;
+    $param =~ s/\s+$//;
+    return $param;
 }
 
 sub printMapInfo {
-	my ($genbankFile) = @_;
-	open (FH, "<$genbankFile") or die "Error in opening the file, $genbankFile, $!\n";
-	my $locus;
-	while(my $line = <FH>) {
-		if(trim($line) =~ /^LOCUS\s+(\S+)\s+/) {
-			$locus = $1;
-		}
-		elsif(trim($line) =~ /^ORGANISM\s+(.+)\s*$/) {
-			unless($locus) {
-				die "No locus found in the genbank file, $genbankFile, $!\n";
-			}
-			print OFH $1." ".$locus."\n";
-			last;
-		}
+    my ($genbankFile, $ofh) = @_;
+    open (FH, "<$genbankFile") or die "Error in opening the file, $genbankFile, $!\n";
+    my $locus;
+    while(my $line = <FH>) {
+	if(trim($line) =~ /^LOCUS\s+(\S+)\s+/) {
+	    $locus = $1;
 	}
-	close FH;
+	elsif(trim($line) =~ /^ORGANISM\s+(.+)\s*$/) {
+	    unless($locus) {
+		die "No locus found in the genbank file, $genbankFile, $!\n";
+	    }
+	    print $ofh $1." ".$locus."\n";
+	    last;
+	}
+    }
+    close FH;
 }
 
