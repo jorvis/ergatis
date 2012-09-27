7b77c298ddd0461fd10158a7d25cccf9c3d2c7dc
kabolude <kabolude@c60d6122-3b2e-0410-912d-af40fea696fe>
2012-09-27 20:24:27 +0000

    Added components: soapdenovo and extract_assembly_stats

---
+++ src/perl/extract_assembly_stats.pl
@@ -0,0 +1,235 @@
+#!/usr/bin/perl
+
+eval 'exec /usr/bin/perl  -S $0 ${1+"$@"}'
+    if 0; # not running under some shell
+
+eval 'exec /usr/bin/perl  -S $0 ${1+"$@"}'
+    if 0; # not running under some shell
+
+BEGIN{foreach (@INC) {s/\/usr\/local\/packages/\/local\/platform/}};
+use lib (@INC,$ENV{"PERL_MOD_DIR"});
+no lib "$ENV{PERL_MOD_DIR}/i686-linux";
+no lib ".";
+
+=head1 NAME
+
+extract_assembly_stats.pl - Extracts statistics from an assembly file
+
+=head1 SYNOPSIS
+
+USAGE: extract_assembly_stats.pl 
+            --input=/path/to/input_file.fa 
+            --output=/path/to/output_file.fa
+          [ --limit=integer 
+            --no50base
+	    --help
+	    --log
+          ]
+
+=head1 OPTIONS
+
+B<--input,-i>
+    Required.Input assembly file to be analyzed. Fasta format.
+    Files compressed with gzip or bzip2 are automatically decompressed.
+
+B<--output,-o>
+    Required. Output file with statistics. Tab-delimited format.
+
+B<--limit,-m>
+    Optional. Script ignores contigs smaller than the integer assigned. 
+
+B<--no50base,-n> 
+    Optional. Script will use this number as the base genome size for computing the N50 value.
+
+B<--log,-l> 
+    Log file
+
+B<--help,-h>
+    This help message
+
+
+=head1  DESCRIPTION
+
+This script extracts statistics from an assembly (fasta) file - contigs or scaffolds.
+
+=head1  INPUT
+
+Input format is fasta. Files compressed with gzip or bzip2 are automatically decompressed.
+
+=head1  OUTPUT
+
+Output is a tab-delimited file with the following information:
+File: name of input file
+Number: total number of contigs
+Total Size: total size of contigs
+Min Size: minimum contig size
+Max Size: maximum contig size
+Average Size: average contig size
+N50: size of contig c, such that 50% of the total assembly size is contained in contigs larger than c
+Size @ 1Mbp: same as N50 but assuming that genome size is 2Mbp
+Number @ 1Mbp: smallest number of contigs that add up to 1Mbp
+Size & Number @ 2Mbp, 4Mbp, 10Mbp - same as above but for more values 
+
+
+=head1  CONTACT
+
+    Kemi Abolude
+    kabolude@som.umaryland.edu
+    Script adapted from http://www.cbcb.umd.edu/software/metamos/statistics.pl
+
+=cut
+
+
+use Bio::SeqIO;
+use Getopt::Long;
+use Statistics::Descriptive;
+use strict;
+use Ergatis::Logger;
+
+
+my %options = ();
+my $results = GetOptions (\%options,
+			  'input|i=s',
+			  'output|o=s',
+ 			  'limit|m:i',
+			  'n50base|n:i',
+			  'log|l=s',
+			  'debug=s',
+			  'help|h') || pod2usage();
+
+my $ifile = $options{'input'};
+my $ofile = $options{'output'};
+my $limit = $options{'limit'};
+my $N50Base = $options{'n50base'};
+
+my $logfile = $options{'log'} || Ergatis::Logger::get_default_logfilename();
+my $logger = new Ergatis::Logger('LOG_FILE'=>$logfile,
+                                  'LOG_LEVEL'=>$options{'debug'});
+$logger = $logger->get_logger();
+
+
+## display documentation
+if( $options{'help'} ){
+    pod2usage( {-exitval => 0, -verbose => 2, -output => \*STDERR} );
+}
+
+## make sure everything passed was peachy
+&check_parameters(\%options);
+
+###code
+open(OUT, ">$ofile");
+
+print OUT "File:\t$ifile\n";
+    
+if ($ifile =~ /\.gz$/){ # gzipped file
+    	open(IN, "gzip -dc $ifile |") || $logger->logdie ("Cannot open $ifile\n");
+   }elsif ($ifile =~ /\.bz2$/) { #bzipped file
+   	open(IN, "bzip2 -dc $ifile |") || $logger->logdie ("Cannot open $ifile\n");
+   } else {
+   	open(IN, "$ifile") || $logger->logdie ("Cannot open $ifile\n");
+   }
+
+my $fr = Bio::SeqIO->new(-fh=>\*IN, -format=> 'fasta');
+ 
+if (! defined $fr){ die ("Cannot parse file\n");}
+
+my @sizes = ();
+my $stats = new Statistics::Descriptive::Full;
+
+while (my $seq = $fr->next_seq){
+      if (defined $limit && length($seq->seq()) < $limit) { next;}
+	push @sizes, length($seq->seq());
+	$stats->add_data(length($seq->seq()));
+}
+
+close(IN);
+
+print OUT "Number:\t", $stats->count(), "\n"; # count 
+print OUT "Total size:\t", $stats->sum(), "\n"; # total size
+print OUT "Min size:\t", $stats->min(), "\n"; # min size
+print OUT "Max size:\t", $stats->max(), "\n"; # max size
+print OUT "Mean size:\t", sprintf("%.2f\n",$stats->mean()); # mean size
+print OUT "Median size:\t", $stats->median(), "\n"; # median size
+    
+# now for the N* statistics
+if (! defined $N50Base){
+	$N50Base = $stats->sum() / 2;
+}
+
+my $total = 0;
+my @sizes = sort {$b <=> $a} @sizes;
+
+my $n50 = undef;
+my $n1m = undef;
+my $n2m = undef;
+my $n4m = undef;
+my $n10m = undef;
+my $s1m = undef;
+my $s2m = undef;
+my $s4m = undef;
+my $s10m = undef;
+
+    for (my $i = 0; $i <= $#sizes; $i++){
+	$total += $sizes[$i];
+	if ($total >= $N50Base && ! defined $n50){
+	    $n50 = $sizes[$i];
+	}
+	if ($total >= 1000000 && ! defined $n1m){
+	    $n1m = $i + 1;
+	    $s1m = $sizes[$i];
+	}
+	if ($total >= 2000000 && ! defined $n2m){
+	    $n2m = $i + 1;
+	    $s2m = $sizes[$i];
+	}
+	if ($total >= 4000000 && ! defined $n4m){
+	    $n4m = $i + 1;
+	    $s4m = $sizes[$i];
+	}
+	if ($total >= 10000000 && ! defined $n10m){
+	    $n10m = $i + 1;
+	    $s10m = $sizes[$i];
+	}
+    }
+
+print OUT  "N50:\t$n50\nSize @ 1Mbp:\t$s1m\nNumber @ 1Mbp:\t$n1m\nSize @ 2Mbp:\t$s2m\nNumber @ 2Mbp:\t$n2m\nSize @ 4Mbp:\t$s4m\nNumber @ 4Mbp:\t$n4m\nSize @ 10Mbp:\t$s10m\nNumber @ 10Mbp:\t$n10m\n\n";
+
+print OUT "File: name of input file
+Number: total number of contigs
+Total Size: total size of contigs
+Min Size: minimum contig size
+Max Size: maximum contig size
+Average Size: average contig size
+N50: size of contig c, such that 50% of the total assembly size is contained in contigs larger than c
+Size @ 1Mbp: same as N50 but assuming that genome size is 2Mbp
+Number @ 1Mbp: smallest number of contigs that add up to 1Mbp
+Size & Number @ 2Mbp, 4Mbp, 10Mbp - same as above but for more values\n"; 
+
+close(OUT);
+
+exit(0);
+
+#Subroutines
+sub check_parameters {
+    my $options = shift;
+    
+    ## make sure required arguments were passed
+    my @required = qw( input output );
+    for my $option ( @required ) {
+        unless  ( defined $$options{$option} ) {
+            $logger->logdie ("--$option is a required option");
+        }
+    }
+
+    ## make sure input file exists
+    if (! -e $options{input} ) {
+        $logger->logdie("the input file passed ($options{input}) cannot be read or does not exist");
+    }
+}
+
+sub _pod {
+# Used to display the perldoc help.
+    pod2usage( {-exitval => 0, -verbose => 2} );
+}
+
+ 
---
+++ src/perl/soapdenovo.pl
@@ -0,0 +1,151 @@
+#!/usr/bin/perl
+
+eval 'exec /usr/bin/perl  -S $0 ${1+"$@"}'
+    if 0; # not running under some shell
+
+eval 'exec /usr/bin/perl  -S $0 ${1+"$@"}'
+    if 0; # not running under some shell
+BEGIN{foreach (@INC) {s/\/usr\/local\/packages/\/local\/platform/}};
+use lib (@INC,$ENV{"PERL_MOD_DIR"});
+no lib "$ENV{PERL_MOD_DIR}/i686-linux";
+no lib ".";
+
+=head1 NAME
+
+soapdenovo.pl - Wrapper to facilitate the execution of SOAPdenovo through ergatis.
+
+=head1 SYNOPSIS
+
+USAGE ./soapdenovo.pl --soapdenovo_exe=/path/to/soapdenovo --process=soapdenovo_process --config_file=/path/to/config --args=<SOAPDENOVO ARGS>
+
+=head1 OPTIONS
+
+B<--soapdenovo_exe, -s>
+        Path to the soapdenovo executable.
+
+B<--process, -p> 
+        Choose soapdenovo process to run.
+	Options include all, pregraph, contig, map, scaff
+        
+B<--config_file, -c>
+        Config file with info location of input files, library info, etc
+
+B<--args, -a> 
+        Any command-line arguments that should be passed to soapdenovo
+
+B<--log, -l>
+        OPTIONAL. Log file
+        
+=head1 DESCRIPTION
+
+The script executes the SOAPdenovo script from the SOAPdenovo software package (version 1.04).
+
+=head1 INPUT
+
+The main input for this script is the config file which has the following format:
+	#maximal read length
+	max_rd_len=75
+	[LIB]
+	avg_ins = <LIBSZ> # average insert size
+	reverse_seq = 0 # forward/reverse library
+	asm_flags = 3 # reads used for contigging and scaffolding
+	pair_num_cutoff = 2 # num of mates needed to scaffold across a gap
+	map_len = 60 # minimum length of read mapping to a contig
+	q1 = <prefix>.1.fastq # read1 in fastq
+	q2 = <prefix>.2.fastq # read2 in fastq
+	q=<prefix>.singleton.fastq
+
+more details: http://soap.genomics.org.cn/soapdenovo.html
+
+=head1 CONTACT
+
+        Kemi Abolude
+        kabolude@som.umaryland.edu
+
+=cut
+
+use strict;
+use Getopt::Long qw(:config no_ignore_case no_auto_abbrev);
+use Ergatis::Logger;
+use File::Copy;
+use File::Basename;
+
+my %options = ();
+my $results = GetOptions (\%options,
+			'soapdenovo_exec|s=s',
+			'process|p=s',
+                        'config_file|c=s',
+                        'args|a=s',
+                        'log|l=s',
+                        'help|h') || pod2usage();
+                                                   
+if ( $options{'help'} ) {
+        pod2usage( {-exitval => 0, -verbose => 2, -output => \*STDERR} );
+}                                                  
+                                                                                        
+my $logfile = $options{'log'} || Ergatis::Logger::get_default_logfilename();
+my $logger = new Ergatis::Logger( 'LOG_FILE'    =>      $logfile,
+                                  'LOG_LEVEL'   =>      $options{'debug'} );                    
+                                          
+$logger = Ergatis::Logger::get_logger();
+               
+
+my $process;
+my $soapdenovo_exec;
+my $config_file;
+my $args;
+
+
+## make sure everything passed was peachy
+check_parameters(\%options);
+
+
+## Check args being passed into mothur
+#my $cmd = "$options{'soapdenovo_exe'} $options{'process'} -s $options{'config_file'} \"$options{'args'}\""; 
+my $cmd = "$soapdenovo_exec $process -s $config_file $args";
+
+##run command
+run_system_cmd($cmd);
+
+
+
+##Subroutines
+
+sub run_system_cmd {
+        my $cmd = shift;
+        my $res = system($cmd);
+        $res = $res >> 8;
+        
+    unless ($res == 0) {
+#        unlink($temp_input);
+                $logger->logdie("Could not run $cmd");
+        }
+        
+        return $res;
+}
+
+sub check_parameters {
+   my $opts = shift;
+   my $logfh;
+   my $ofh;
+
+   if( $opts->{'help'} ) {
+       &_pod;
+   }
+
+   if( $opts->{'log'} ) {
+       open( $logfh, "> $opts->{'log'}") or die("Can't open log file ($!)");
+   }
+
+   foreach my $req ( qw(soapdenovo_exec process config_file args) ) {
+       die("ERROR: Option $req is required") unless( $opts->{$req} );
+   }
+   $soapdenovo_exec = $opts->{'soapdenovo_exec'};
+   $process = $opts->{'process'};
+   $config_file = $opts->{'config_file'};
+   $args = $opts->{'args'} ;
+
+}
+
+
+                    
