a1ae6c800726492aff12667c8d53f29e862aae8f
kabolude <kabolude@c60d6122-3b2e-0410-912d-af40fea696fe>
2013-04-30 14:27:25 +0000

    adding scripts for diginorm pipeline

---
+++ src/python/filter-abund.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python
+"""
+Trim sequences at k-mers of the given abundance, based on the given counting
+hash table.  Output sequences will be placed in 'infile.abundfilt'.
+
+% python scripts/filter-abund.py <counting.kh> <data1> [ <data2> <...> ]
+
+Use '-h' for parameter help.
+"""
+import sys, screed.fasta, os
+import khmer
+from khmer.thread_utils import ThreadedSequenceProcessor, verbose_loader
+
+from khmer.counting_args import build_counting_multifile_args
+
+###
+
+DEFAULT_CUTOFF=2
+
+def main():
+    parser = build_counting_multifile_args()
+    parser.add_argument('--cutoff', '-C', dest='cutoff',
+                        default=DEFAULT_CUTOFF, type=int,
+                        help="Trim at k-mers below this abundance.")
+    parser.add_argument('-o', '--outputpath', dest='outputpath', default='.')
+    args = parser.parse_args()
+
+    counting_ht = args.input_table
+    infiles = args.input_filenames
+    outpath = args.outputpath    
+
+    print 'file with ht: %s' % counting_ht
+
+    print 'loading hashtable'
+    ht = khmer.load_counting_hash(counting_ht)
+    K = ht.ksize()
+
+    print "K:", K
+
+    ### the filtering function.
+    def process_fn(record):
+        name = record['name']
+        seq = record['sequence']
+        if 'N' in seq:
+            return None, None
+
+        trim_seq, trim_at = ht.trim_on_abundance(seq, args.cutoff)
+
+        if trim_at >= K:
+            return name, trim_seq
+
+        return None, None
+
+
+    ### the filtering loop
+    for infile in infiles:
+       print 'filtering', infile
+       outfile = outpath + '/' + os.path.basename(infile) + '.abundfilt'
+       outfp = open(outfile, 'w')
+
+       tsp = ThreadedSequenceProcessor(process_fn)
+       tsp.start(verbose_loader(infile), outfp)
+
+       print 'output in', outfile
+
+if __name__ == '__main__':
+    main()
