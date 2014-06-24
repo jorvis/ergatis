cb0e8f8c32836c05305384a06f757224864b71c7
Shaun Adkins <sadkins@sadkins-lx.igs.umaryland.edu>
2014-06-24 11:15:36 -0400

    modified 'fix praze output' workflow step to use polypeptide hits and uniref IDs both as keys for acquiring correct p-values

--- src/perl/add_pvalue_to_praze_output.pl
+++ src/perl/add_pvalue_to_praze_output.pl
@@ -8,14 +8,14 @@ use IO::File;
 use Blast::BlastHitDataType;
 
 my %p_values	=	();
+my %map_hash = ();
 my $praze_btab	=	*STDIN;
 my $out		=	*STDOUT;
 
 &parse_options;
 &add_pvalues;
 
-sub print_usage
-{
+sub print_usage {
 	die << "END";
 usage: add_pvalue_to_praze_output.pl -blast_btab|b <blast_btab_data>
 	-praze_btab|p <praze_brab_data> [-output|o <output>]
@@ -23,13 +23,16 @@ usage: add_pvalue_to_praze_output.pl -blast_btab|b <blast_btab_data>
 END
 }
 
-sub parse_options
-{
+sub parse_options {
 	my %opts = ();
-	GetOptions(\%opts, "blast_btab|b=s", "praze_btab|p=s",
+	GetOptions(\%opts, "blast_btab|b=s", "praze_btab|p=s", "mapping_file|m=s",
 		   "output|o=s", "help|h");
 	print_usage if $opts{help};
-	initialize_pvalues($opts{blast_btab}) if $opts{blast_btab};
+	
+	if ($opts{mapping_list}) {
+		map_polypeptide_to_CDS($opts{mapping_list});
+	}
+	initialize_pvalues($opts{blast_btab}, \%map_hash) if ($opts{blast_btab});
     
     ## if the input file doesn't exist, touch it.  This is a dirty hack used
     ##  only because praze doesn't create an empty output file if there are no
@@ -58,30 +61,59 @@ sub parse_options
 		if $opts{output};
 }
 
-sub initialize_pvalues
-{
+sub map_polypeptide_to_CDS {
+	my $map_list = shift;
+	open MAP, $map_list or die "Cannot open mapping list for reading: $!\n";
+	while (<MAP>) {
+		chomp;
+		my $line = $_;
+		my @l = split(/\t/, $line);
+		$map_hash{$l[0]} = $l[1];
+	}
+	close MAP
+}
+
+sub initialize_pvalues {
 	my $fname = shift;
+	my $map_h = shift;
+	my $name;
 	my $fh = new IO::File($fname) or
 		die "Error reading BLAST btab data $fname: $!";
 	while (my $line = <$fh>) {
 		chomp $line;
 		my $hit = new Blast::BlastHitDataType($line);
-        my $subject_name = $hit->GetSubjectName;
+		my $query_name = $hit->GetQueryName;	#get polypeptide ID
+        my $subject_name = $hit->GetSubjectName;	#get UNIREF ID
         $subject_name =~ s/^lcl\|//;
-        print "Subject Name = $subject_name\n";
-		$p_values{$subject_name} = $hit->GetPValue;
+        print "Subject Name = $subject_name, Polypeptide Name = $query_name\n";
+
+        # if we used a mapping list, assign key as CDS, otherwise assign as polypeptide.
+        if (scalar keys %{$map_h} > 0) {
+        	$name = $map_h->{$query_name};
+		} else {
+			$name = $query_name;
+		}
+		$p_values{$name}{$subject_name} = $hit->GetPValue;
 	}
 }
 
-sub add_pvalues
-{
+sub add_pvalues {
+	#Add dummy p-value to praze results that did not have BLAST hits to ensure it never ranks among the best results
+	my $dummy = "1.00e+100";	
+
 	while (my $line = <$praze_btab>) {
 		chomp $line;
 		my @tokens = split /\t/, $line;
+		my $query_name = $tokens[0];
 		my $subject_name = $tokens[5];
         $subject_name =~ s/^lcl\|//;
-		my $p_value = $p_values{$subject_name} or
-			die "No p-value found for $subject_name";
+        my $p_value;
+        if (exists $p_values{$query_name}{$subject_name}){
+			$p_value = $p_values{$query_name}{$subject_name}
+		} else {
+			$p_value = $dummy;
+			#print STDERR "No p-value found for $subject_name on query $query_name.  Inserting dummy p-value\n";
+		}
 		print $out "$line\t$p_value\n";
 	}
 }
