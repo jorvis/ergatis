a4dbc3f55332d8f6e86eb58f1fb181086f067056
Shaun Adkins <sadkins@som.umaryland.edu>
2014-10-24 10:25:01 -0400

    small typo fix

--- util/create_prok_pipeline_config.pl
+++ util/create_prok_pipeline_config.pl
@@ -325,7 +325,7 @@ if ($included_subpipelines{'genecalls'}){
 	$config{'p_func default'}->{'$;SKIP_WF_COMMAND$;'} = '';
 	$config{'p_func default'}->{'$;FEATURE_RELATIONSHIP_FILE$;'} = '$;REPOSITORY_ROOT$;/output_repository/'.
 		'bsml2feature_relationships/$;PIPELINEID$;_pre_overlap_analysis/bsml2featurerelationships.mapping.list';
-	$config{'p_func default'}->{'$MULTIFASTA_FILE$;'} = '$;REPOSITORY_ROOT$;/output_repository/'.
+	$config{'p_func default'}->{'$;MULTIFASTA_FILE$;'} = '$;REPOSITORY_ROOT$;/output_repository/'.
 		'clean_fasta/$;PIPELINEID$;_genecalls/i1/g1/final_clean/genecalls.fasta';
 }
 
