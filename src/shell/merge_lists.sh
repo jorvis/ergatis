302cc24d574f1406d2bdf44a1c465069e340c04f
Shaun Adkins <sadkins@som.umaryland.edu>
2015-01-13 14:06:46 -0500

    added workflow step to concatentate blast bsml files before pangenome_blast_parse runs

---
+++ src/shell/merge_lists.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Merge_lists.sh- This will merge comma-separated list files together using `cat`
+# Author: Shaun Adkins
+
+if [ $# -lt 1 ]
+then 
+    printf "%b" "Error: No list files provided.\n" >&2
+fi
+    
+while getopts "i:o:" opt
+do
+	case $opt in
+		i) list_string=$OPTARG;;
+		o) output=$OPTARG;;
+	esac
+done    
+    
+# Take comma-separated string of list files and replace all commas with spaces
+temp_string=$(echo $list_string | sed "s/,/ /g")
+
+#Open every list file and cat contents into output file
+cat $temp_string > $output
