540ab1dd1bd3e021c54a542a9c8596b92d5fb81e
Shaun Adkins <sadkins@som.umaryland.edu>
2016-02-29 13:15:20 -0500

    fixing syntax errors and various bugs

--- src/shell/prep_MiSeq_for_qiime.sh
+++ src/shell/prep_MiSeq_for_qiime.sh
@@ -16,32 +16,29 @@
 
 #### Originally was a script written by Bing Ma, but I modified to work in the CloVR 16S pipeline in Ergatis
 
-while (( $# > 0 ))
+while getopts 'b:f:m:o:' opt
 do
     case $opt in
-		-b) shift;
-				BIN_DIR=$1
-				;;
-        -f) shift;  #shift out option flag so option value is next in line
-                FASTQ_LIST=$1
-                ;;
-        -m) shift;
-                MAPPING_FILE=$1
-                ;;
-		-o) shift;
-				OUTPUT_DIR=$1
-				;;
-        *)  printf 'usage %s -f /path/to/fastq.list -m /path/to/mapping.txt -o /path/to/output/dir/ \n' $(basename $0) >&2
-                exit 1
-                ;;
+        b)	BIN_DIR=$OPTARG
+            ;;
+        f)	FASTQ_LIST=$OPTARG
+            ;;
+        m)	MAPPING_FILE=$OPTARG
+            ;;
+        o)	OUTPUT_DIR=$OPTARG
+            ;;
+        ?)	printf ' usage: %s -f /path/to/fastq.list -m /path/to/mapping.txt -o /path/to/output/dir/ -b /dir/to/misc/scripts/ \n' $(basename $0) >&2
+            exit 2
+            ;;
     esac
-    shift   #shift out already processed options
 done
+shift $((OPTIND - 1))	# Shift out all processed options to handle any existing non-option args
 
 # Verify FastQ list input is exactly 2 files
-line_count=`wc -l $FASTQ_LIST`
-if [ line_count -ne 2 ]; then
-	echo "FASTQ list must have exactly 2 sequences in it";
+echo "Verifying contents of FASTQ and mapping files..."
+line_count=$(wc -l $FASTQ_LIST | cut -f1 -d' ')
+if [ $line_count -ne 2 ]; then
+	echo "FASTQ list must have exactly 2 sequences in it.  This file had $line_count" >&2
 	exit 1
 fi
 
@@ -51,7 +48,7 @@ R2=`tail -n 1 $FASTQ_LIST`
 
 #### Verify 3 files do exist
 if [ ! -f $R1 ] || [ ! -f $R2 ] || [ ! -f $MAPPING_FILE ]; then
-	echo "Required files not exist!! Abort"; 
+	echo "Required files not exist!! Abort" >&2
 	exit; 
 fi
 
@@ -61,11 +58,12 @@ cutoffLength=187 ### ie, 250 x 0.75
 
 prep_dir=$OUTPUT_DIR/prep
 
-if [! -d $prep_dir ]; then
+if [ ! -d $prep_dir ]; then
 	mkdir -m 777 $prep_dir;
 fi
 
 #trim barcode, form a new barcode file 
+echo "Extract barcodes from FASTQ input to create barcode FASTQ files..."
 fastx_trimmer -i $R1 -f 1 -l 12 -Q 33 -o $prep_dir/R1_barcode.fq
 fastx_trimmer -i $R2 -f 1 -l 12 -Q 33 -o $prep_dir/R2_barcode.fq
 cat $prep_dir/R1_barcode.fq | $BIN_DIR/fq_mergelines.pl > $prep_dir/R1_barcode_temp
@@ -74,27 +72,32 @@ paste $prep_dir/R1_barcode_temp $prep_dir/R2_barcode_temp | awk -F"\t" '{print $
 cat $prep_dir/R1R2_barcode.fastq | $BIN_DIR/fq_mergelines.pl > $prep_dir/R1R2_barcode_temp
 
 #trim sequences (12 bp of barcode) off the sequence
+echo "Running Seqtk to further trim barcodes..." 
 seqtk trimfq -b 12 $prep_dir/R1.fq > $prep_dir/R1_trimmed_seq.fastq
 seqtk trimfq -b 12 $prep_dir/R2.fq > $prep_dir/R2_trimmed_seq.fastq
 
 #trim off low quality bases 
+echo "Trim off low quality bases..."
 seqtk trimfq $prep_dir/R1_trimmed_seq.fastq | $BIN_DIR/fq_mergelines.pl > $prep_dir/R1_seqtk_trimmed_seq_temp  
 seqtk trimfq $prep_dir/R2_trimmed_seq.fastq | $BIN_DIR/fq_mergelines.pl > $prep_dir/R2_seqtk_trimmed_seq_temp 
 
 #create new fastq file to concatenate barcode (24bp) with sequences
+echo "Create new FASTQ inputs with modified barcodes..."
 paste $prep_dir/R1R2_barcode_temp $prep_dir/R2_seqtk_trimmed_seq_temp | awk -F"\t" '{print $1"\t"$2$6"\t"$7"\t"$4$8}' | $BIN_DIR/fq_splitlines.pl | sed 's/2:N:0:/3:N:0:/' > $prep_dir/R3N.fq
 paste $prep_dir/R1R2_barcode_temp $prep_dir/R1_seqtk_trimmed_seq_temp | awk -F"\t" '{print $1"\t"$2$6"\t"$7"\t"$4$8}' | $BIN_DIR/fq_splitlines.pl | sed 's/2:N:0:/1:N:0:/' > $prep_dir/R1N.fq
 
 
 ####based on the length, define the cutoff of the concat length, default is half of the original 250bp
+echo "Running prinseq-lite to filter out bad reads..."
 prinseq-lite -fastq $prep_dir/R3N.fq -out_format 3 -out_good $prep_dir/R3N_${cutoffLength} -no_qual_header -line_width 0 -min_len $cutoffLength
 prinseq-lite -fastq $prep_dir/R1N.fq -out_format 3 -out_good $prep_dir/R1N_${cutoffLength} -no_qual_header -line_width 0 -min_len $cutoffLength
 
 ###in this case, the total and total_filter files have no difference, only the barcode file will change. The purpose is to make the trimmed seq file and barcode file match.
-
+echo "Match the trimmed sequence file and the barcode file..."
 $BIN_DIR/fq_getPairAndOrphan1.8.py $prep_dir/R3N_${cutoffLength}.fastq $prep_dir/R1N_${cutoffLength}.fastq $OUTPUT_DIR/R3N_PE.fq $OUTPUT_DIR/R1N_PE.fq $prep_dir/orphan_temp.fq 
 $BIN_DIR/fq_getPairAndOrphan1.8.py $OUTPUT_DIR/R3N_PE.fq $prep_dir/R1R2_barcode.fastq $prep_dir/PE_temp.fq $OUTPUT_DIR/barcode.fq $prep_dir/orphan_temp.fq 
 
+echo "Removing temporary files..."
 rm *_bad_*
 rm *_temp*
 #rm -rf $prep_dir
@@ -102,5 +105,8 @@ rm *_temp*
 
 ####make sure the Mapping file is correct, warning is ok, just no errors
 # Note:  validate_mapping_file.py is part of QIIME and QIIME's bin dir should be set in $PATH
+echo "Validate provided mapping file using QIIME..."
 validate_mapping_file.py -m $MAPPING_FILE > $OUTPUT_DIR/mapping.corrected.txt	## for QIIME 1.8
 
+echo "Done!"
+exit 0
