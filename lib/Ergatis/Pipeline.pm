8472c2a097bc1e32a16744861d11e846c77b736c
Shaun Adkins <sadkins@sadkinslt-osx.igs.umaryland.edu>
2016-11-17 15:52:13 -0500

    splitting web ergatis run code from non-web code.  Will refactor later

--- lib/Ergatis/Pipeline.pm
+++ lib/Ergatis/Pipeline.pm
@@ -79,6 +79,12 @@ umask(0000);
     sub run {
         my ( $self, %args ) = @_;
 
+        # If the 'block' argument was passed, run the code without the use of forking
+        if ($args{block}) {
+            run_non_web($self, %args);
+            return;
+        }
+
         ## path must be defined and exist to run
         if ( !defined $_[0]->{path} ) {
             croak("failed to run pipeline: no path set yet.");
@@ -129,6 +135,8 @@ umask(0000);
               . $args{ergatis_cfg}->val( 'paths', 'workflow_log4j' );
         }
 
+
+        # If the 'web-based' code is being run, fork processes
         my $child_pid = fork;
         if ($child_pid) {
             while ( !( -e $self->path ) ) {
@@ -252,7 +260,7 @@ umask(0000);
                 $runstring .=
                     " --logconf="
                   . $args{ergatis_cfg}->val( 'paths', 'workflow_log4j' )
-                  . " >& $self->{path}.run.out";
+                  . " &> $self->{path}.run.out";
 
                 ## write all this to a file
                 my $pipeline_script =
@@ -326,22 +334,253 @@ umask(0000);
 
                 close $debugfh if $self->{debug};
             }
-
-         # If 'block' is set to 1, wait for pipeline to return non-running state
-            my $p_state = '';
-            if ( $args{block} ) {
-                do {
-                    $p_state = $self->pipeline_state;
-					sleep 60;
-                } while ( $p_state =~ /(running|pending|waiting|incomplete)/i );
-
-                # If end-state is complete, return 1.  Otherwise return 0
-                return 1 if ($p_state eq 'complete');
-                return 0;
-            }
-
             exit;
         }
+    }
+
+    sub run_non_web {
+        my ($self, %args) = @_;
+
+        ## path must be defined and exist to run
+        if ( !defined $_[0]->{path} ) {
+            croak("failed to run pipeline: no path set yet.");
+        } elsif ( !-f $_[0]->{path} ) {
+            croak(
+                "failed to run pipeline: pipeline file $_[0]->{path} doesn't exist"
+            );
+        }
+
+        ## ergatis cfg is required
+        if ( !defined $args{ergatis_cfg} ) {
+            croak("ergatis_cfg is a required option");
+        }
+        my $run_dir = $args{ergatis_cfg}->val( 'paths', 'workflow_run_dir' )
+          || croak "workflow_run_dir not found in ergatis.ini";
+        my $pipeline_scripts_dir = "$run_dir/scripts";
+
+        my $current_user = $args{run_as} || '';
+
+        ## create a directory from which to run this pipeline
+        if ( -d $run_dir ) {
+            ## make sure the scripts directory exists.  this is where the pipeline execution shell
+            #   files are written
+            if ( !-d $pipeline_scripts_dir ) {
+                ( mkdir $pipeline_scripts_dir )
+                  || croak
+                  "filed to create pipeline scripts directory: $pipeline_scripts_dir : $!";
+            }
+
+            # make a subdirectory for this pipelineid if doing data placement
+            if ( !$args{ergatis_cfg}->val( 'grid', 'vappio_data_placement' ) ) {
+                $run_dir .= '/' . $self->id;
+
+                if ( !-d $run_dir ) {
+                    ( mkdir $run_dir )
+                      || croak
+                      "failed to create workflow_run_dir: $run_dir : $!";
+                }
+            }
+
+        } else {
+            croak
+              "Invalid workflow_run_dir (doesn't exist) in ergatis.ini: $run_dir";
+        }
+
+        if ( !-e $args{ergatis_cfg}->val( 'paths', 'workflow_log4j' ) ) {
+            croak "Invalid workflow_log4j in ergatis.ini : "
+              . $args{ergatis_cfg}->val( 'paths', 'workflow_log4j' );
+        }
+
+        ## open the debugging file if needed
+        open( my $debugfh, ">>$self->{debug_file}" ) if $self->{debug};
+
+        ##debug
+        print $debugfh "debug init\n" if $self->{debug};
+
+        print $debugfh "attempting to chdir to $run_dir\n"
+          if $self->{debug};
+
+        chdir $run_dir
+          || croak "Can't change to running directory $run_dir\n";
+        use POSIX qw(setsid);
+        setsid() or die "Can't start a new session: $!";
+
+        print $debugfh "got past the POSIX section\n" if $self->{debug};
+        $self->_setup_environment( ergatis_cfg => $args{ergatis_cfg} );
+        print $debugfh "got past ENV setup section\n" if $self->{debug};
+
+        my $marshal_interval_opt = '';
+        if ( $args{ergatis_cfg}
+            ->val( 'workflow_settings', 'marshal_interval' ) )
+        {
+            $marshal_interval_opt = "-m "
+              . $args{ergatis_cfg}
+              ->val( 'workflow_settings', 'marshal_interval' );
+        }
+
+        my $init_heap =
+          $args{ergatis_cfg}->val( 'workflow_settings', 'init_heap' )
+          || '100m';
+        my $max_heap =
+          $args{ergatis_cfg}->val( 'workflow_settings', 'max_heap' )
+          || '1024m';
+
+        my $sudo_prefix = '';
+        my $runprefix   = '';
+        my $runstring   = '';
+
+        ## should we sudo to a different user?
+        if ( $args{run_as} ) {
+            print $debugfh "INFO: run_as parameter was set\n"
+              if $self->{debug};
+            $sudo_prefix = "sudo -E -u $current_user";
+        } else {
+            print $debugfh "INFO: run_as parameter not set\n"
+              if $self->{debug};
+        }
+
+        print $debugfh "so far runsprefix: $runprefix\n";
+
+        ## are we submitting the workflow as a job?  (CURRENTLY TIED TO SGE)
+        if ( $args{ergatis_cfg}
+            ->val( 'workflow_settings', 'submit_pipelines_as_jobs' ) )
+        {
+            $runprefix = $args{ergatis_cfg}->val( 'grid', 'sge_qsub' )
+              . " -V -wd $run_dir -b y";
+
+            my $job_name = "pipeline_" . $self->id;
+            $runprefix .= " -N $job_name";
+
+            my $pipe_submission_queue = $args{ergatis_cfg}
+              ->val( 'workflow_settings', 'pipeline_submission_queue' );
+            my $pipe_submission_queue_memory = $args{ergatis_cfg}
+              ->val( 'workflow_settings', 'submit_queue_memory' );
+
+            if ($pipe_submission_queue) {
+                $runprefix .= " -q $pipe_submission_queue";
+            }
+
+            # If the memory has been specified add that line
+            if ($pipe_submission_queue_memory) {
+                $runprefix .=
+                  " -l mem_free=$pipe_submission_queue_memory";
+            }
+
+            my $pipe_submission_project =
+              $args{ergatis_cfg}->val( 'workflow_settings',
+                'pipeline_submission_project' );
+
+            if ($pipe_submission_project) {
+                $runprefix .= " -P $pipe_submission_project";
+            }
+        }
+
+        $runstring =
+          "$ENV{'WF_ROOT'}/RunWorkflow -i $self->{path} $marshal_interval_opt "
+          . "--init-heap=$init_heap --max-heap=$max_heap ";
+
+        ## If email notification is toggled
+        if ( $args{email_user} ) {
+            $runstring .= " --notify $args{'email_user'} ";
+        }
+
+        ## Support observer scripts
+        if ( $args{ergatis_cfg}
+            ->val( 'workflow_settings', 'observer_scripts' ) )
+        {
+            $runstring .=
+              " --scripts="
+              . $args{ergatis_cfg}
+              ->val( 'workflow_settings', 'observer_scripts' );
+        }
+
+        $runstring .=
+            " --logconf="
+          . $args{ergatis_cfg}->val( 'paths', 'workflow_log4j' )
+          . " &> $self->{path}.run.out";
+
+        ## write all this to a file
+        my $pipeline_script =
+          "$pipeline_scripts_dir/pipeline." . $self->id . ".run.sh";
+        print $debugfh "Pipeline script: $pipeline_script\n";
+        open( my $pipeline_fh, ">$pipeline_script" )
+          || die "can't write pipeline shell file $pipeline_script: $!";
+        print $debugfh "Wrote to pipeline script\n";
+
+        print $pipeline_fh '#!/bin/bash', "\n\n";
+
+        for my $env ( keys %ENV ) {
+            ## We don't want HTTP_* or REMOTE_* env variables to
+            ## be pushed to SGE.
+            if ( $env =~ /(^HTTP_|^REMOTE_)/ ) {
+                print $debugfh "UNSETTING ENV $env\n" if $self->{debug};
+                print $pipeline_fh "unset $env\n";
+                next;
+            }
+
+            ## don't do HTTP_ variables
+            next if $env =~ /^HTTP_/;
+
+            print $pipeline_fh "export $env=\"$ENV{$env}\"\n";
+            print $debugfh "ENV $env=\"$ENV{$env}\"\n"
+              if $self->{debug};
+        }
+        print $pipeline_fh "\n$sudo_prefix $runprefix $runstring";
+
+        close $pipeline_fh;
+        print $debugfh
+          "Wrote runstring to $pipeline_script: $runstring\n";
+
+        ## the script needs to be executable
+        chmod 0777, $pipeline_script;
+
+## create a marker file showing that the pipeline has been started (or attempted to start)
+#   we can't rely completely on XML here, since a pipeline submitted as a job won't have any
+#   xml changes until the job starts running.  this allows us to show a 'pending' state
+#   on the pipeline itself.
+        my $final_run_command = "$pipeline_script";
+
+        print $debugfh "preparing to run $final_run_command\n"
+          if $self->{debug};
+
+        my $rc = 0xffff & system($final_run_command);
+
+        printf $debugfh
+          "system(%s) returned %#04x: $rc for command $final_run_command\n"
+          if $self->{debug};
+        if ( $rc == 0 ) {
+            print $debugfh "ran with normal exit\n" if $self->{debug};
+        } elsif ( $rc == 0xff00 ) {
+            print $debugfh "command failed: $!\n" if $self->{debug};
+            croak
+              "Unable to run workflow command $final_run_command failed : $!\n";
+        } elsif ( ( $rc & 0xff ) == 0 ) {
+            $rc >>= 8;
+            print $debugfh "ran with non-zero exit status $rc\n"
+              if $self->{debug};
+            croak
+              "Unable to run workflow command $final_run_command failed : $!\n";
+        } else {
+            print $debugfh "ran with " if $self->{debug};
+            if ( $rc & 0x80 ) {
+                $rc &= ~0x80;
+                print $debugfh "coredump from " if $self->{debug};
+            }
+            print $debugfh "signal $rc\n" if $self->{debug};
+        }
+
+        close $debugfh if $self->{debug};
+
+     # If 'block' is set to 1, wait for pipeline to return non-running state
+        my $p_state = '';
+        do {
+            $p_state = $self->pipeline_state;
+			sleep 60;
+        } while ( $p_state =~ /(running|pending|waiting|incomplete)/i );
+
+        # If end-state is complete, return 1.  Otherwise return 0
+        return 1 if ($p_state eq 'complete');
+        return 0;
     }
 
     ## accessors
@@ -437,4 +676,3 @@ umask(0000);
 
 }
 1 == 1;
-
