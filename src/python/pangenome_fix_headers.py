bf6b811082b8effd083ddf375fa475d6402bdf42
Shaun Adkins <sadkins@som.umaryland.edu>
2017-01-17 11:57:14 -0500

    adding translate sequence component to pangenome pipeline

--- src/python/pangenome_fix_headers.py
+++ src/python/pangenome_fix_headers.py
@@ -14,10 +14,12 @@ from Bio import SeqIO
 
 # Read in a fasta file as a list of SeqRecords
 def read_in_fasta(fasta_in):
+	#print "reading in fasta file\n"
 	return list( SeqIO.parse(fasta_in, "fasta") )
 
 # Parse mapping file and keep specific fields
 def parse_map_file(mapping_in):
+	#print "parsing mapping file\n"
 	lines = []
 	with open (mapping_in, 'r') as mapping_fh:
 		for line in mapping_fh:
@@ -30,16 +32,24 @@ def parse_map_file(mapping_in):
 
 # Replace the current fasta header with a properly named one
 def replace_fasta_headers(records, replace_vals):
+	#print "replacing fasta headers\n"
+	updated_records = []
 	for record in records:
 		header = record.id
+		#print record.id + "\n";
 # Split on first space
 # NOTE: This is under the assumption that the bsml2fasta component for assemblies has USE_SEQUENCE_IDS_IN_FASTA set to 1 in the config file.  The polypeptide version does not need this
-		seq_id, rest = header.split(' ', 1)
+		fields = header.split(' ', 1)
+		seq_id = fields[0]
 # Search for seq_id amongst our kept mapping values
 		for line in replace_vals:
-# Handle the 'assembly' or 'polypeptide' cases
-			if seq_id == line[0] or seq_id == line[1]:
-				record.id = 'gnl|' + line[3] + '|' + line[2]
+# Handle the 'assembly' cases or the 'polypeptide' cases
+			if seq_id == line[0]:	# Assembly
+				record.id = 'gnl|' + line[3] + '|' + line[0]	# gnl|org_strain|genome/contig
+				updated_records.append(record)
+				break
+			elif seq_id == line[1]:	# Polypeptide
+				record.id = 'gnl|' + line[3] + '|' + line[2]	# gnl|org_strain|locus_tag
 				updated_records.append(record)
 				break
 	return updated_records
@@ -57,7 +67,7 @@ def main():
 	description = "Format pangenome headers so that genomes and genes are easily identifiable and parseable"
 	parser = ArgumentParser(description=description)
 	parser.add_argument("--mapping_file", "-m", help="Path to the mapping file created by mugsyprep.pl", metavar="/path/to/mugsymap_complete.txt", required=True)
-	parser.add_argument("--input_file", "-m", help="Path to a fasta file.  File should have information that can make it easy to map the genome and gene using the mugsymap file", metavar="/path/to/genome.fa", required=True)
+	parser.add_argument("--input_file", "-f", help="Path to a fasta file.  File should have information that can make it easy to map the genome and gene using the mugsymap file", metavar="/path/to/genome.fa", required=True)
 	parser.add_argument("--output_file", "-o", help="Path to write the output file", metavar="/path/to/output.txt", required=True)
 	args = parser.parse_args()
 
---
+++ src/shell/pangenome_fix_headers.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+if [ -z "$SCHEMA_DOCS_DIR" ]
+then
+    SCHEMA_DOCS_DIR=
+export SCHEMA_DOCS_DIR
+fi
+if [ -z "$WORKFLOW_WRAPPERS_DIR" ]
+then
+    WORKFLOW_WRAPPERS_DIR=/usr/local/projects/ergatis/package-nightly//bin
+export WORKFLOW_WRAPPERS_DIR
+fi
+if [ -z "$WORKFLOW_DOCS_DIR" ]
+then
+    WORKFLOW_DOCS_DIR=
+export WORKFLOW_DOCS_DIR
+fi
+
+umask 0000
+
+unset LD_LIBRARY_PATH
+
+LANG=C
+export LANG
+LC_ALL=C
+export LC_ALL
+
+while [[ $# -ge 1 ]]
+do
+i="$1"
+arg=$(echo $i | cut -f1 -d "=")
+val=$(echo $i | cut -f2 -d "=")
+
+case $arg in
+    --input_file)
+    input_file="$val"
+    ;;
+    --output_file)
+    output_file="$val"
+    ;;
+    --mapping_file)
+    mapping_file="$val"
+    ;;
+    ;;
+    --python_exec)
+    python_exec="$val"
+    ;;
+	--script_bin)
+	script_bin="$val"
+	;;
+esac
+shift
+done
+
+# This script needs to run in Python 2.7 or later but I'd like to keep a default python path in case
+if [ -z "$python_exec" ]; then
+	python_exec="/usr/bin/python"
+fi
+
+cmd="$python_exec ${script_bin}/pangenome_fix_headers.py --input_file=${input_file} --output_file=${output_file} --mapping_file=${mapping_file}"
+
+echo "$cmd"
+$cmd
+
+exit 0
