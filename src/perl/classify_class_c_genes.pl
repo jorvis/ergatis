56b146e5ab624117e813056d00b9ad8593acf2f1
Shaun Adkins <sadkins@som.umaryland.edu>
2017-05-15 14:56:44 -0400

    renamed LGT_phylogenetic components

--- src/perl/classify_class_c_genes.pl
+++ src/perl/classify_class_c_genes.pl
@@ -7,8 +7,8 @@ classify_class_c_genes.pl - Calculate h-score of two blast files and filter out 
 =head1 SYNOPSIS
 
  USAGE: classify_class_c_genes.pl
-       --only_file=/path/to/only/blast.m8
-       --exclude_file=/path/to/restricted/blast.m8
+       --sister_file=/path/to/sister/blast.m8
+       --outside_file=/path/to/outside/blast.m8
        --output_dir=/path/to/class_c.tsv
      [ --bitscore_thresh=100
 	   --hscore_thresh=30
@@ -19,11 +19,11 @@ classify_class_c_genes.pl - Calculate h-score of two blast files and filter out 
 
 =head1 OPTIONS
 
-B<--only_file>
+B<--sister_file>
 	The m8-formatted BLAST output where the subject database was restricted to only the GI accessions in question
 	File should only have the best hit for each query gene
 
-B<--exclude_file>
+B<--outside_file>
 	The m8-formatted BLAST output where the subject database excluded those GI accessions from --only_file
 	File should only have the best hit for each query gene
 
@@ -84,8 +84,8 @@ exit(0);
 
 sub main {
     my $results = GetOptions (\%options,
-                         "only_file=s",
-                         "exclude_file=s",
+                         "sister_file=s",
+                         "outside_file=s",
                          "output_dir|o=s",
 						 "bitscore_thresh:100",
 						 "hscore_thresh:30",
@@ -95,36 +95,36 @@ sub main {
                           );
 
     &check_options(\%options);
-	parse_m8(\%genes, $options{'only_file'}, $options{'exclude_file'});
+	parse_m8(\%genes, $options{'sister_file'}, $options{'outside_file'});
 	calculate_h_score(\%genes);
 	print_class_c_genes(\%genes, \%options);
 }
 
 sub parse_m8 {
 	my ($gene_h, $only_f, $exclude_f) = @_;
-	open ONLY, $only_f || die("Cannot open $only_f for reading: $!");
-	while (<ONLY>) {
+	open SISTER, $only_f || die("Cannot open $only_f for reading: $!");
+	while (<SISTER>) {
 		chomp;
 		my @hit = split(/\t/);
-		$gene_h->{$hit[0]}->{'only_subj'} = $hit[1];
-		$gene_h->{$hit[0]}->{'only_bit'} = $hit[11];
+		$gene_h->{$hit[0]}->{'sister_subj'} = $hit[1];
+		$gene_h->{$hit[0]}->{'sister_bit'} = $hit[11];
 	}
-	close ONLY;
-	open EXCLUDE, $exclude_f || die("Cannot open $exclude_f for reading: $!");
-	while (<EXCLUDE>) {
+	close SISTER;
+	open OUTSIDE, $exclude_f || die("Cannot open $exclude_f for reading: $!");
+	while (<OUTSIDE>) {
 		chomp;
 		my @hit = split(/\t/);
-		$gene_h->{$hit[0]}->{'exclude_subj'} = $hit[1];
-		$gene_h->{$hit[0]}->{'exclude_bit'} = $hit[11];
+		$gene_h->{$hit[0]}->{'outside_subj'} = $hit[1];
+		$gene_h->{$hit[0]}->{'outside_bit'} = $hit[11];
 	}
-	close EXCLUDE;
+	close OUTSIDE;
 }
 
 sub calculate_h_score {
 	my $gene_h = shift;
 	foreach my $hit (keys %$gene_h) {
-		my $only = $gene_h->{$hit}->{'only_bit'};
-		my $exclude = $gene_h->{$hit}->{'exclude_bit'};
+		my $only = $gene_h->{$hit}->{'sister_bit'};
+		my $exclude = $gene_h->{$hit}->{'outside_bit'};
 		if (defined $only && defined $exclude) {
 			$gene_h->{$hit}->{'h_score'} = $exclude - $only;
 		} else {
@@ -139,13 +139,13 @@ sub print_class_c_genes {
 	my ($gene_h, $opts) = @_;
 	my $outdir = $opts->{'output_dir'};
 	my $class_c_file = $outdir."/class_c.tsv";
-	my $all_file = $outdir."/all_genes.tsv";
+	#my $all_file = $outdir."/all_genes.tsv";
 	my $b_thresh = $opts->{'bitscore_thresh'};
 	my $h_thresh = $opts->{'hscore_thresh'};
 	open CFH, ">".$class_c_file || die("Cannot open $class_c_file for writing: $!");
-	open AFH, ">".$all_file || die("Cannot open $all_file for writing: $!");
-	print CFH "query\tonly_hit\texclude_hit\tonly_bit\texclude_bit\th_score\n";
-	print AFH "query\tonly_hit\texclude_hit\tonly_bit\texclude_bit\th_score\thighest_lgt_class\n";
+	#open AFH, ">".$all_file || die("Cannot open $all_file for writing: $!");
+	print CFH "query\tsister_hit\toutside_hit\tsister_bit\texclude_bit\th_score\n";
+	#print AFH "query\tonly_hit\texclude_hit\tonly_bit\texclude_bit\th_score\thighest_lgt_class\n";
 	foreach my $hit (keys %$gene_h) {
 		my $gene_str = "$hit\t" . $gene_h->{$hit}->{'only_subj'} . "\t" . $gene_h->{$hit}->{'exclude_subj'} . "\t" . $gene_h->{$hit}->{'only_bit'} . "\t" . $gene_h->{$hit}->{'exclude_bit'} . "\t" . $gene_h->{$hit}->{'h_score'};
 		print AFH $gene_str;
@@ -153,13 +153,13 @@ sub print_class_c_genes {
 		# If gene meets class C thresholds print to that file, and mark it is a class C gene in "all genes" file
 		if ($gene_h->{$hit}->{'h_score'} >= $h_thresh && $gene_h->{$hit}->{'exclude_bit'} >= $b_thresh){
 			print CFH $gene_str . "\n";
-			print AFH "\tC\n";
+			#print AFH "\tC\n";
 		} else {
-			print AFH "\t-\n";
+			#print AFH "\t-\n";
 		}
 	}
 	close CFH;
-	close AFH;
+	#close AFH;
 }
 
 sub check_options {
@@ -174,7 +174,7 @@ sub check_options {
 
     $debug = $opts->{'debug'} if( $opts->{'debug'} );
 
-    foreach my $req ( qw(only_file exclude_file output_dir) ) {
+    foreach my $req ( qw(sister_file outside_file output_dir) ) {
         &_log($ERROR, "Option $req is required") unless( $opts->{$req} );
     }
 }
